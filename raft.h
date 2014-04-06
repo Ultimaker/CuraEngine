b539f0d2b07ff6bc00309003fd97c4cb461d14fb
Skeen <skeen@cs.au.dk>
2014-04-06 19:00:13 +0200

    Updated gitignore
    Ignore vim comment files, and the build folder (for out of source
    builds)

--- raft.h
+++
@@ -1,9 +0,0 @@
-/** Copyright (C) 2013 David Braam - Released under terms of the AGPLv3 License */
-#ifndef RAFT_H
-#define RAFT_H
-
-#include "sliceDataStorage.h"
-
-void generateRaft(SliceDataStorage& storage, int distance);
-
-#endif//RAFT_H
--- skirt.h
+++
@@ -1,9 +0,0 @@
-/** Copyright (C) 2013 David Braam - Released under terms of the AGPLv3 License */
-#ifndef SKIRT_H
-#define SKIRT_H
-
-#include "sliceDataStorage.h"
-
-void generateSkirt(SliceDataStorage& storage, int distance, int extrusionWidth, int count, int minLength, int initialLayerHeight);
-
-#endif//SKIRT_H
