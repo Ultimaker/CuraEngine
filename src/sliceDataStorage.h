951f8f4e4b96c105029dc03347d668ff6423746d
daid <daid303@gmail.com>
2014-11-26 14:52:11 +0100

    Added some documentation to the sliceDataStorage.

--- src/sliceDataStorage.h
+++ src/sliceDataStorage.h
@@ -8,25 +8,32 @@
 #include "gcodePlanner.h"
 
 namespace cura {
-
+/*!
+    The SliceLayerPart is a single enclosed printable area for a single layer. (Also known as islands)
+    It's filled during the fffProcessor.processSliceData, where each step uses data from the previous steps.
+    Finally it's used in the fffProcessor.writeGCode to generate the final gcode.
+ */
 class SliceLayerPart
 {
 public:
-    AABB boundaryBox;
-    Polygons outline;
-    Polygons combBoundery;
-    std::vector<Polygons> insets;    //insets[n] is the inset (n * line_width + line_width/2) offset
-    Polygons skinOutline;
-    std::vector<Polygons> sparse_outline; //sparse_outline[n] is sparse outline of (n+1) layer thick. 
+    AABB boundaryBox;       //!< The boundaryBox is an axis-aligned bounardy box which is used to quickly check for possible collision between different parts on different layers. It's an optimalization used during skin calculations.
+    Polygons outline;       //!< The outline is the first member that is filled, and it's filled with polygons that math a cross section of the 3D model.
+    Polygons combBoundery;  //!< The combBoundery is generated from the online. It's the area in which the nozzle tries to stay during traveling.
+    std::vector<Polygons> insets;       //!< The insets are generated with: an offset of (index * line_width + line_width/2) compared to the outline. The insets are also known as perimeters, and printed inside out.
+    Polygons skinOutline;               //!< The skinOutline is the area which needs to be 100% filled to generate a proper top&bottom filling. It's filled by the "skin" module.
+    std::vector<Polygons> sparse_outline; //!< The sparse_outline are the areas which need to be filled with sparse (0-99%) infill. The sparse_outline is an array to support thicker layers of sparse infill. sparse_outline[n] is sparse outline of (n+1) layers thick. 
 };
 
+/*!
+    The SlicerLayer contains all the data for a single cross section of the 3D model.
+ */
 class SliceLayer
 {
 public:
-    int sliceZ;
-    int printZ;
-    std::vector<SliceLayerPart> parts;
-    Polygons openLines;
+    int sliceZ;     //!< The height at which the 3D model was cut.
+    int printZ;     //!< The height at which this layer needs to be printed. Can differ from sliceZ due to the raft.
+    std::vector<SliceLayerPart> parts;  //!< An array of LayerParts which contain the actual data. The parts are printed one at a time to minimize travel outside of the 3D model.
+    Polygons openLines; //!< A list of lines which where never hooked up into a 2D polygon. (Currently unused in normal operation)
 };
 
 /******************/
