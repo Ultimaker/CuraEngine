faaf5737f21eca6575b7fc51415f2c9dd7f464ed
Tim Kuipers <t.kuipers@ultimaker.com>
2015-03-02 13:28:59 +0100

    first zigzag support infill approximation

--- src/infill.cpp
+++ src/infill.cpp
@@ -34,41 +34,120 @@ int compare_int64_t(const void* a, const void* b)
     return 0;
 }
 
+/*!
+ * generate lines within the area of [in_outline], at regular intervals of [lineSpacing]
+ * idea:
+ * intersect a regular grid of 'scanlines' with the area inside [in_outline]
+ * sigzag:
+ * include pieces of boundary, connecting the lines, forming an accordion like zigzag instead of separate lines    |_|^|_|
+ * 
+ * we call the areas between two consecutive scanlines a 'scansegment'
+ * 
+ * algorithm:
+ * 1) for each line segment of each polygon:
+ *      store the intersections of that line segment with all scanlines in a mapping (vector of vectors) from scanline to intersections
+ *      (zigzag): add boundary segments to result
+ * 2) for each scanline:
+ *      sort the associated intersections 
+ *      and connect them using the even-odd rule
+ * 
+ * zigzag algorithm:
+ * while walking around (each) polygon
+ *  if polygon intersects with even scanline
+ *      start boundary segment (add each following segment to the [result])
+ *  when polygon intersects with a scanline again
+ *      stop boundary segment (stop adding segments to the [result])
+ *      if polygon intersects with even scanline again (instead of odd)
+ *          dont add the last line segment to the boundary
+ */
 void generateLineInfill(const Polygons& in_outline, Polygons& result, int extrusionWidth, int lineSpacing, int infillOverlap, double rotation)
 {
+    bool zigzag = true;
+
+    
     Polygons outline = in_outline.offset(extrusionWidth * infillOverlap / 100);
     PointMatrix matrix(rotation);
     
     outline.applyMatrix(matrix);
     
+    auto addLine = [&](Point from, Point to)
+    {            
+        PolygonRef p = result.newPoly();
+        p.add(matrix.unapply(from));
+        p.add(matrix.unapply(to));
+    };
+        
     AABB boundary(outline);
     
     boundary.min.X = ((boundary.min.X / lineSpacing) - 1) * lineSpacing;
     int lineCount = (boundary.max.X - boundary.min.X + (lineSpacing - 1)) / lineSpacing;
-    std::vector<std::vector<int64_t> > cutList;
+    
+    std::vector<std::vector<int64_t> > cutList; // mapping from scanline to all intersections with polygon segments
+    
     for(int n=0; n<lineCount; n++)
         cutList.push_back(std::vector<int64_t>());
-
     for(unsigned int polyNr=0; polyNr < outline.size(); polyNr++)
     {
-        Point p1 = outline[polyNr][outline[polyNr].size()-1];
+        std::vector<Point> firstBoundarySegment;
+        
+        bool isFirstBoundarySegment = true;
+        bool firstBoundarySegmentEndsInEven;
+        
+        bool isEvenScanSegment = false; 
+        
+        
+        Point p0 = outline[polyNr][outline[polyNr].size()-1];
+        Point lastPoint = p0;
+        firstBoundarySegment.push_back(p0);
         for(unsigned int i=0; i < outline[polyNr].size(); i++)
         {
-            Point p0 = outline[polyNr][i];
-            int idx0 = (p0.X - boundary.min.X) / lineSpacing;
-            int idx1 = (p1.X - boundary.min.X) / lineSpacing;
-            int64_t xMin = p0.X, xMax = p1.X;
-            if (p0.X > p1.X) { xMin = p1.X; xMax = p0.X; }
-            if (idx0 > idx1) { int tmp = idx0; idx0 = idx1; idx1 = tmp; }
-            for(int idx = idx0; idx<=idx1; idx++)
+            Point p1 = outline[polyNr][i];
+            int64_t xMin = p1.X, xMax = p0.X;
+            if (xMin > xMax) { xMin = p0.X; xMax = p1.X; }
+            
+            int idx0 = (p0.X - boundary.min.X) / lineSpacing; // scanline index 0
+            int idx1 = (p1.X - boundary.min.X) / lineSpacing; // scanline index 1
+            int direction = 1;
+            if (idx0 > idx1) { direction = -1; }
+            
+            if (isFirstBoundarySegment) firstBoundarySegment.push_back(p1);
+            for(int idx = idx0; idx!=idx1+direction; idx+=direction)
             {
                 int x = (idx * lineSpacing) + boundary.min.X + lineSpacing / 2;
                 if (x < xMin) continue;
                 if (x >= xMax) continue;
-                int y = p0.Y + (p1.Y - p0.Y) * (x - p0.X) / (p1.X - p0.X);
+                int y = p1.Y + (p0.Y - p1.Y) * (x - p1.X) / (p0.X - p1.X);
                 cutList[idx].push_back(y);
+                
+                
+                bool last_isEvenScanSegment = isEvenScanSegment;
+                if (idx % 2 == 0) isEvenScanSegment = true;
+                else isEvenScanSegment = false;
+                
+                if (last_isEvenScanSegment && !isEvenScanSegment)
+                    addLine(lastPoint, Point(x,y));
+                lastPoint = Point(x,y);
+
+                if (isFirstBoundarySegment) 
+                {
+                    firstBoundarySegment.push_back(lastPoint);
+                    isFirstBoundarySegment = false;
+                    firstBoundarySegmentEndsInEven = isEvenScanSegment;
+                }
             }
-            p1 = p0;
+            if (isEvenScanSegment)
+                addLine(lastPoint, p1);
+            lastPoint = p1;
+            p0 = p1;
+        }
+        
+        if (isEvenScanSegment)
+        {
+            for (int i = 1; i < firstBoundarySegment.size(); i++)
+            {
+                if (i < firstBoundarySegment.size() - 1 | !firstBoundarySegmentEndsInEven)
+                    addLine(firstBoundarySegment[i-1], firstBoundarySegment[i]);
+            }   
         }
     }
     
@@ -80,9 +159,7 @@ void generateLineInfill(const Polygons& in_outline, Polygons& result, int extrus
         {
             if (cutList[idx][i+1] - cutList[idx][i] < extrusionWidth / 5)
                 continue;
-            PolygonRef p = result.newPoly();
-            p.add(matrix.unapply(Point(x, cutList[idx][i])));
-            p.add(matrix.unapply(Point(x, cutList[idx][i+1])));
+            addLine(Point(x, cutList[idx][i]), Point(x, cutList[idx][i+1]));
         }
         idx += 1;
     }
