5f470237592d1344005d916e62957e9c73b02e19
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-04-20 17:54:07 +0200

    Add print material amount and simplify print time message

--- src/commandSocket.h
+++ src/commandSocket.h
@@ -25,7 +25,7 @@ public:
     void sendLayerInfo(int layer_nr, int32_t z, int32_t height);
     void sendPolygons(cura::PolygonType type, int layer_nr, cura::Polygons& polygons);
     void sendProgress(float amount);
-    void sendPrintTimeForObject(int index, float print_time);
+    void sendPrintTime();
     void sendPrintMaterialForObject(int index, int extruder_nr, float material_amount);
 
     void beginSendSlicedObject();
