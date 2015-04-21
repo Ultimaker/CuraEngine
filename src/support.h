bb7d6f4cdc491b0621927461072a1da530268b0f
Tim Kuipers <t.kuipers@ultimaker.com>
2015-04-21 16:14:44 +0200

    some refactoring and extra commenting

--- src/support.h
+++ src/support.h
@@ -9,20 +9,50 @@ namespace cura {
 
 class AreaSupport {
 public:
+            
+    /*!
+    * Generate support polygons over all layers.
+    * 
+    * This function also handles small overhang areas (creates towers with larger diameter than just the overhang area) and single walls which could otherwise fall over.
+    * 
+    * \param storage data storage containing the input layer outline data and containing the output support storage per layer
+    * \param object The object for which to generate support areas
+    * \param layer_count total number of layers
+    */
+    void generateSupportAreas(SliceDataStorage& storage, SliceMeshStorage* object, int layer_count);
+    
+private:
+    
     /*!
      * Joins the layerpart outlines of all meshes and collects the overhang points (small areas).
+     * \param storage input layer outline information
+     * \param overhang_points stores overhang_points along with the layer index at which the overhang point occurs
+     * \param layer_count total number of layers
+     * \param supportMinAreaSqrt diameter of the minimal area which can be supported without a specialized strut
+     * \param extrusionWidth extrusionWidth
      */
     static void joinMeshesAndDetectOverhangPoints(
-    SliceDataStorage& storage,
-    std::vector<Polygons>& joinedLayers,
-    std::vector<std::pair<int, std::vector<Polygons>>>& overhang_points, // stores overhang_points along with the layer index at which the overhang point occurs)
-    int layer_count,
-    int supportMinAreaSqrt,
-    int extrusionWidth
-                  );
+        SliceDataStorage& storage,
+        std::vector<Polygons>& joinedLayers,
+        std::vector<std::pair<int, std::vector<Polygons>>>& overhang_points, 
+        int layer_count,
+        int supportMinAreaSqrt,
+        int extrusionWidth
+    );
+    
     /*!
      * Adds tower pieces to the current support layer.
      * From below the roof, the towers are added to the normal support layer and handled as normal support area.
+     * \param supportLayer_this The support areas in the layer for which we are creating towers/struts
+     * \param towerRoofs The parts of roofs which need to expand downward until they have the required diameter
+     * \param overhang_points stores overhang_points along with the layer index at which the overhang point occurs
+     * \param overhang_points_pos Index into \p overhang_points for the overhang points in the next layer
+     * \param layer_idx The index of the layer at which to handle towers
+     * \param towerRoofExpansionDistance The offset distance which determines the angle of the tower roof tops
+     * \param supportTowerDiameter The diameter of the eventual tower, below the roof
+     * \param supportMinAreaSqrt diameter of the minimal area which can be supported without a specialized strut
+     * \param layer_count total number of layers
+     * \param z_layer_distance_tower The number of layers between an overhang point and the top of a support tower
      */
     static void handleTowers(
         Polygons& supportLayer_this,
@@ -39,6 +69,9 @@ public:
     
     /*!
      * Adds struts (towers against a wall) to the current layer.
+     * \param supportLayer_this The areas of the layer for which to handle the wall struts.
+     * \param supportMinAreaSqrt The minimal diameter of a wall which doesn't need a strut for reinforcement
+     * \param suportTowerDiameter The diameter of the strut
      */
     static void handleWallStruts(
         Polygons& supportLayer_this,
@@ -46,22 +79,6 @@ public:
         int supportTowerDiameter
     );
 };
-    
-/*!
- * Generate support polygons over all layers.
- * Algorithm:
- * From top layer to bottom layer:
- * - find overhang by looking at the difference between two consucutive layers
- * - join with support areas from layer above
- * - subtract current layer
- * - use the result for the next lower support layer (without doing XY-distance and Z bottom distance, so that a single support beam may move around the model a bit => more stability)
- * - perform inset using X/Y-distance and bottom Z distance
- * 
- * for support buildplate only: purge all support not connected to buildplate
- * 
- * This function also handles small overhang areas (creates towers with larger diameter than just the overhang area) and single walls which could otherwise fall over.
- */
-void generateSupportAreas(SliceDataStorage& storage, SliceMeshStorage* object, int layer_count);
 
 }//namespace cura
 
