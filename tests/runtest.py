51214f9dfd14dcd88a759a13eb752e01bf3e35a3
daid <daid303@gmail.com>
2015-04-23 09:52:57 +0200

    Disable error catching in debug.

--- tests/runtest.py
+++ tests/runtest.py
@@ -3,16 +3,22 @@
 import sys
 import subprocess
 import os
+import time
+import stat
 
 def main(engine, model_path):
-	for filename in os.listdir(model_path):
+	for filename in sorted(os.listdir(model_path), key=lambda filename: os.stat(os.path.join(model_path, filename)).st_size):
 		if filename.lower().endswith('.stl'):
 			print("Slicing: %s" % (filename))
-			p = subprocess.Popen([engine, os.path.join(model_path, filename)], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			t = time.time()
+			p = subprocess.Popen([engine, '-vv', os.path.join(model_path, filename)], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 			stdout, stderr = p.communicate()
 			if p.wait() != 0:
 				print ("Engine failed to report success on test object: %s" % (filename))
+				print(stderr.decode('utf-8', 'replace').split('\n')[-5:])
 				sys.exit(1)
+			else:
+				print("Slicing took: %f" % (time.time() - t))
 
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
