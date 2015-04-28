20fa95def89a83c35a4f1bd146be5074238fc33d
Tim Kuipers <t.kuipers@ultimaker.com>
2015-04-28 14:25:22 +0200

    2D hash grid class for finding nearby items

---
+++ src/utils/BucketGrid2D.h
@@ -0,0 +1,195 @@
+/** Copyright (C) 2015 Tim Kuipers- Released under terms of the AGPLv3 License */
+#ifndef BUCKET_GRID_2D_H
+#define BUCKET_GRID_2D_H
+#include "logoutput.h"
+
+namespace cura
+{
+
+/*!
+ * Container for items with location for which the lookup for nearby items is optimized.
+ * 
+ * It functions by hashing the items location and lookuping up based on the hash of that location and the hashes of nearby locations.
+ */
+template<typename T>
+class BucketGrid2D
+{
+private:
+
+    /*!
+     * Returns a point for which the hash is at a grid position of \p relativeHash relative to \p p.
+     * 
+     * \param p The point for which to get the relative point to hash
+     * \param relativeHash The relative position - in grid terms - of the relative point. 
+     * \return A point for which the hash is at a grid position of \p relativeHash relative to \p p.
+     */
+    inline Point getRelativeForHash(const Point& p, const Point& relativeHash)
+    {
+        return p + relativeHash*squareSize;
+    }
+
+
+    /*!
+     * A hash class representing the hash function object.
+     */
+    struct PointHasher
+    {
+
+        /*!
+         * The basic hash function for a 2D grid position.
+         * \param p The grid location to hash
+         * \return the hash
+         */
+        inline uint32_t pointHash_simple(const Point& p) const
+        {
+            return p.X ^ (p.Y << 8);
+            //return (p.X / 20000) ^ (p.Y / 20000) << 8;
+        }
+
+        /*!
+         * The hash function for a 2D position.
+         * \param point The location to hash
+         * \return the hash
+         */
+        inline uint32_t pointHash(const Point& point) const
+        {
+            Point p = point/squareSize;
+            return pointHash_simple(p);
+        }
+/*
+        inline uint32_t pointHash(const Point& point, const Point& relativeHash) const
+        {
+            Point p = p/squareSize + relativeHash;
+            return pointHash_simple(p);
+        }*/
+
+        /*!
+         * The horizontal and vertical size of a cell in the grid; the width and height of a bucket.
+         */
+        int squareSize;
+        
+        /*!
+         * Basic constructor.
+         * \param squareSize The horizontal and vertical size of a cell in the grid; the width and height of a bucket.
+         */
+        PointHasher(int squareSize) : squareSize(squareSize) {};
+        
+        /*!
+         * See PointHasher::pointHash
+         */
+        uint32_t operator()(const Point& p) const { return pointHash(p); };
+
+    };
+
+    /*!
+     * A helper predicate object which allways returns false when comparing two objects.
+     * 
+     * This is used for mapping each point to a unique object, even when two objects have the same point associated with it.
+     */
+    struct NeverEqual
+    {
+        template<typename S>
+        bool operator()(const S& p1, const S& p2) const { return false; };
+    };
+
+
+private:
+    /*!
+     * Basic constructor.
+     * \param squareSize The horizontal and vertical size of a cell in the grid; the width and height of a bucket.
+     */
+    int squareSize;
+    /*!
+     * The map type used to associate points with their objects.
+     */
+    typedef typename std::unordered_map<Point, T, PointHasher, NeverEqual> Map;
+    
+    /*!
+     * The map used to associate points with their objects.
+     */
+    Map point2object;
+
+
+public:
+    /*!
+     * The constructor for a bucket grid.
+     * 
+     * \param squareSize The horizontal and vertical size of a cell in the grid; the width and height of a bucket.
+     * \param initial_map_size The minimal number of initial buckets 
+     */
+    BucketGrid2D(int squareSize, unsigned int initial_map_size = 4) : squareSize(squareSize), point2object(initial_map_size, PointHasher(squareSize)) {};
+
+    /*!
+     * Find all objects with a point in a grid cell at a distance of one cell from the cell of \p p.
+     * 
+     * \param p The point for which to find close points.
+     * \param ret Ouput parameter: all objects close to \p p.
+     */
+    void findNearbyObjects(Point& p, std::vector<T>& ret)
+    {
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                int i = point2object.bucket(getRelativeForHash(p, Point(x,y)));
+                for ( auto local_it = point2object.begin(i); local_it!= point2object.end(i); ++local_it )
+                    ret.push_back(local_it->second);
+            }
+        }
+    };
+
+    /*!
+     * Find the nearest object to a given lcoation \p p, if there is any in a neighboring cell in the grid.
+     * 
+     * \param p The point for which to find the nearest object.
+     * \param nearby Output parameter: the nearest object, if any
+     * \return Whether an object has been found.
+     */
+    bool findNearestObject(Point& p, T& nearby)
+    {
+        bool found = false;
+        
+        int32_t bestDist2 = squareSize*9; // 9 > sqrt(2*2 + 2*2)^2  which is the square of the largest distance of a point to a point in a neighboring cell
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                int i = point2object.bucket(getRelativeForHash(p, Point(x,y)));
+                for ( auto local_it = point2object.begin(i); local_it!= point2object.end(i); ++local_it )
+                {
+                    found = true;
+                    int32_t dist2 = vSize2(local_it->first - p);
+                    if (dist2 < bestDist2)
+                    {
+                        nearby = local_it->second;
+                        bestDist2 = dist2;
+                    }
+                }
+            }
+        }
+        return found;
+    };
+
+    
+    /*!
+     * Insert a new point into the bucket grid.
+     * 
+     * \param p The location associated with \p t.
+     * \param t The object to insert in the grid cell for position \p p.
+     */
+    void insert(Point& p, T& t)
+    {
+        typedef typename Map::iterator iter;
+        std::pair<iter, bool> emplaced = point2object.emplace(p, t);
+        if (! emplaced.second)
+            logError("Error! BucketGrid2D couldn't insert object!");
+    };
+
+
+
+
+
+};
+
+} // namespace cura
+#endif//BUCKET_GRID_2D_H
