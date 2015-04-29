c23701d7f12f5b353662a18d4248839aae13bd85
Tim Kuipers <t.kuipers@ultimaker.com>
2015-04-29 15:12:45 +0200

    made the comb boundary larger, so that (barely) connected parts still use the comb boundary

--- src/inset.cpp
+++ src/inset.cpp
@@ -6,7 +6,8 @@ namespace cura {
 
 void generateInsets(SliceLayerPart* part, int extrusionWidth, int insetCount, bool avoidOverlappingPerimeters)
 {
-    part->combBoundery = part->outline.offset(-extrusionWidth);
+    int combBoundaryInset = extrusionWidth/2; // hard coded value
+    part->combBoundery = part->outline.offset(-combBoundaryInset);
     if (insetCount == 0)
     {
         part->insets.push_back(part->outline);
