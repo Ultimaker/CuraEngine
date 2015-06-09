9fa902ad6d977a179db8124c1134866d74506602
Tim Kuipers <t.kuipers@ultimaker.com>
2015-06-09 13:09:12 +0200

    license correction

--- src/utils/polygon.cpp
+++ src/utils/polygon.cpp
@@ -1,4 +1,4 @@
-/** Copyright (C) 2015 Tim Kuipers - Released under terms of the AGPLv3 License */
+/** Copyright (C) 2015 Ultimaker - Released under terms of the AGPLv3 License */
 #include "polygon.h"
 
 #include "linearAlg2D.h" // pointLiesOnTheRightOfLine
