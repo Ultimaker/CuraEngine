Ultimaker/CuraEngine

commit 1070d367e10cef973c55373b6b0740521db78827

Re-nable layer count using a consistent tag name
Signed-off-by: Olliver Schinagl <o.schinagl@ultimaker.com>

src/FffGcodeWriter.cpp
@@ -36,7 +36,7 @@ void FffGcodeWriter::writeGCode(SliceDataStorage& storage, TimeKeeper& time_keep
     meshgroup_number++;
 
     unsigned int total_layers = storage.meshes[0].layers.size();
-    //gcode.writeComment("Layer count: %d", totalLayers);
+    gcode.writeComment("LAYER_COUNT: %d", totalLayers);
 
     bool has_raft = getSettingAsPlatformAdhesion("adhesion_type") == EPlatformAdhesion::RAFT;
     if (has_raft)

commit a2881b83f7f8f8a3ea9b0d9f2695635af48703ed

GCodeExport: (re-)Add layer_count tag
Commit 1070d36 broke the build by enabeling code that was not kept up to
date, this patch fixes it properly by adding the
writeLayerCountComment() method.

Signed-off-by: Olliver Schinagl <o.schinagl@ultimaker.com>

src/FffGcodeWriter.cpp
@@ -36,7 +36,7 @@ void FffGcodeWriter::writeGCode(SliceDataStorage& storage, TimeKeeper& time_keep
     meshgroup_number++;
 
     unsigned int total_layers = storage.meshes[0].layers.size();
-    gcode.writeComment("LAYER_COUNT: %d", totalLayers);
+    gcode.writeLayerCountComment(total_layers);
 
     bool has_raft = getSettingAsPlatformAdhesion("adhesion_type") == EPlatformAdhesion::RAFT;
     if (has_raft)

src/gcodeExport.cpp
@@ -189,6 +189,11 @@ void GCodeExport::writeLayerComment(int layer_nr)
     *output_stream << ";LAYER:" << layer_nr << "\n";
 }
 
+void GCodeExport::writeLayerCountComment(int layer_count)
+{
+    *output_stream << ";LAYER_COUNT:" << layer_count << "\n";
+}
+
 void GCodeExport::writeLine(const char* line)
 {
     *output_stream << line << "\n";

src/gcodeExport.h
@@ -216,6 +216,7 @@ public:
     void writeComment(std::string comment);
     void writeTypeComment(const char* type);
     void writeLayerComment(int layer_nr);
+    void writeLayerCountComment(int layer_count);
     
     void writeLine(const char* line);
     