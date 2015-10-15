951eac4c084ca57ada90c8c8bffb64c51166d94d
Tim Kuipers <t.kuipers@ultimaker.com>
2015-10-15 18:05:02 +0200

    config per extruder

--- src/Preheat.h
+++ src/Preheat.h
@@ -1,6 +1,8 @@
 #ifndef PREHEAT_H
 #define PREHEAT_H
 
+#include <cassert>
+
 #include "utils/logoutput.h"
 
 namespace cura 
@@ -15,9 +17,14 @@ public:
         double temp;
     };
     std::vector<Datum> data;
-    
+
     double getTemp(double flow)
     {
+        assert(data.size() > 0);
+        if (data.size() == 1)
+        {
+            return data.front().temp;
+        }
         if (flow < data.front().flow)
         {
             logError("flow too low"); // TODO
@@ -28,7 +35,7 @@ public:
             logError("flow too high"); // TODO
             return data.back().temp;
         }
-        Datum* last_datum = data.front();
+        Datum* last_datum = &data.front();
         for (unsigned int datum_idx = 1; datum_idx < data.size(); datum_idx++)
         {
             Datum& datum = data[datum_idx];
@@ -38,7 +45,6 @@ public:
             }
             last_datum = &datum;
         }
-        
     };
 };
 
@@ -47,15 +53,22 @@ public:
  */
 class Preheat 
 {
-    double time_to_heatup_1_degree = 0.6; // TODO per extruder
-    double time_to_cooldown_1_degree = 0.4; // TODO
-    
-    double idle_temp = 150; // TODO
-    double printing_temp = 230; // TODO
-    
-    double timeToHeatFromIdleToPrintTemp(unsigned int extruder)
+    class Config
     {
-        return (printing_temp - idle_temp) * time_to_heatup_1_degree; 
+    public:
+        double time_to_heatup_1_degree = 0.6; // TODO 
+        double time_to_cooldown_1_degree = 0.4; // TODO
+
+        double idle_temp = 150; // TODO
+
+        FlowTempGraph flow_temp_graph;
+    };
+
+    std::vector<Config> config_per_extruder;
+
+    double timeToHeatFromIdleToPrintTemp(unsigned int extruder, double flow)
+    {
+        return (config_per_extruder[extruder].flow_temp_graph.getTemp(flow) - config_per_extruder[extruder].idle_temp) * config_per_extruder[extruder].time_to_heatup_1_degree; 
     }
 
     /*!
@@ -64,7 +77,7 @@ class Preheat
      */
     double timeRatioCooldownHeatup(unsigned int extruder)
     {
-        return time_to_cooldown_1_degree / time_to_heatup_1_degree; 
+        return config_per_extruder[extruder].time_to_cooldown_1_degree / config_per_extruder[extruder].time_to_heatup_1_degree; 
     }
 
 public:
@@ -73,17 +86,18 @@ public:
      * \param window_time The time window within which the cooldown and heat up must take place.
      * \param extruder The extruder used
      */
-    double timeBeforeEndToInsertPreheatCommand(double time_window, unsigned int extruder)
+    double timeBeforeEndToInsertPreheatCommand(double time_window, unsigned int extruder, double flow)
     {
         double time_ratio_cooldown_heatup = timeRatioCooldownHeatup(extruder);
-        double time_needed_to_reach_idle_temp = timeToHeatFromIdleToPrintTemp(extruder) * (1.0 + time_ratio_cooldown_heatup);
+        double time_to_heat_from_idle_to_print_temp = timeToHeatFromIdleToPrintTemp(extruder, flow);
+        double time_needed_to_reach_idle_temp = timeToHeatFromIdleToPrintTemp(extruder, flow) * (1.0 + time_ratio_cooldown_heatup);
         if (time_needed_to_reach_idle_temp < time_window)
         {
-            return timeToHeatFromIdleToPrintTemp(extruder);
+            return time_to_heat_from_idle_to_print_temp;
         }
         else 
         {
-            return time_window * time_to_heatup_1_degree / (time_to_cooldown_1_degree + time_to_heatup_1_degree);
+            return time_window * config_per_extruder[extruder].time_to_heatup_1_degree / (config_per_extruder[extruder].time_to_cooldown_1_degree + config_per_extruder[extruder].time_to_heatup_1_degree);
         }
     }
 };
