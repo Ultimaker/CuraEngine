0781a8f4efb55786484bcd6097c9d7b6df8d8bcf
Ghostkeeper <ghost_keeper+github@hotmail.com>
2015-10-30 17:41:48 +0100

    Initial setup of CppUnit in CMake
    I've setup a basic test but it doesn't actually do anything.

---
+++ tests/LinearAlg2DTest.cpp
@@ -0,0 +1,20 @@
+//Copyright (c) 2015 Ultimaker B.V.
+//UltiScanTastic is released under the terms of the AGPLv3 or higher.
+
+#include "LinearAlg2DTest.h"
+#include <iostream>
+
+int main(int arg_count,char** args)
+{
+    std::cout << "Test succeeded!" << std::endl;
+    return 0;
+}
+
+namespace cura
+{
+
+LinearAlg2DTest::LinearAlg2DTest()
+{
+}
+
+}
---
+++ tests/LinearAlg2DTest.h
@@ -0,0 +1,21 @@
+//Copyright (c) 2015 Ultimaker B.V.
+//UltiScanTastic is released under the terms of the AGPLv3 or higher.
+
+#ifndef LINEARALG2DTEST_H
+#define	LINEARALG2DTEST_H
+
+namespace cura
+{
+
+class LinearAlg2DTest
+{
+public:
+    LinearAlg2DTest();
+private:
+
+};
+
+}
+
+#endif //LINEARALG2DTEST_H
+
