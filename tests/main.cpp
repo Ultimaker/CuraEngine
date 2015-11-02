7d707681782907a7eb47b1f12925a9402665d8b5
Ghostkeeper <ghost_keeper+github@hotmail.com>
2015-11-02 15:08:28 +0100

    Tell CppUnit to output XML
    The standard XmlOutputter seems to output JUnit-style XML.

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,6 +3,7 @@
 
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/XmlOutputter.h>
 
 /*!
  * \brief Runs the test cases.
@@ -10,9 +11,15 @@
 int main(int argc,char** argv)
 {
     CppUnit::TextUi::TestRunner runner;
+    
+    //Set the output type to be JUnit-style XML.
+    std::ofstream output("output.xml");
+    CppUnit::XmlOutputter* outputter = new CppUnit::XmlOutputter(&runner.result(), output);
+    runner.setOutputter(outputter);
+    
     CppUnit::TestFactoryRegistry& registry = CppUnit::TestFactoryRegistry::getRegistry();
-    runner.addTest(registry.makeTest());
-    bool success = runner.run("", false);
+    runner.addTest(registry.makeTest()); //Makes a test suite from all test cases that are registered with CPPUNIT_TEST_SUITE_REGISTRATION().
+    bool success = runner.run("", false); //Run the tests!
     return success ? 0 : 1;
 }
 
