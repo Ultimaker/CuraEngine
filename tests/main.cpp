9c201e33969be9e67029eeb8067b78c3387259d6
Ghostkeeper <ghost_keeper+github@hotmail.com>
2015-11-02 14:50:57 +0100

    Remove old try for return code
    Directly returning the boolean didn't work.

--- tests/main.cpp
+++ tests/main.cpp
@@ -12,7 +12,6 @@ int main(int argc,char** argv)
     CppUnit::TextUi::TestRunner runner;
     CppUnit::TestFactoryRegistry& registry = CppUnit::TestFactoryRegistry::getRegistry();
     runner.addTest(registry.makeTest());
-    //return runner.run("",false);
     bool success = runner.run("", false);
     return success ? 0 : 1;
 }
