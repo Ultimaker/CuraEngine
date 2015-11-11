4460b9a1d02c7795a0066a3f1afefb7a8a3ceffa
Ghostkeeper <ghost_keeper+github@hotmail.com>
2015-11-11 13:39:25 +0100

    Fix license header
    I don't know how I keep getting this wrong.

--- tests/LinearAlg2DTest.h
+++ tests/LinearAlg2DTest.h
@@ -1,5 +1,5 @@
 //Copyright (c) 2015 Ultimaker B.V.
-//UltiScanTastic is released under the terms of the AGPLv3 or higher.
+//CuraEngine is released under the terms of the AGPLv3 or higher.
 
 #ifndef LINEARALG2DTEST_H
 #define	LINEARALG2DTEST_H
