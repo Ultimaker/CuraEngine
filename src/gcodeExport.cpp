ac8b0e149a81d08ee64c0ba85e47cc7bb45bd211
Tim Kuipers <t.kuipers@ultimaker.com>
2015-11-17 14:25:34 +0100

    bugfix: retraction dist used as if it was volume (CURA-415)

--- src/gcodeExport.cpp
+++ src/gcodeExport.cpp
@@ -475,7 +475,7 @@ void GCodeExport::writeRetraction_extruderSwitch()
     }
 //     resetExtrusionValue(); // TODO: why would we do this?
 
-    double retraction_e_amount = extruder_attr[current_extruder].extruder_switch_retraction_distance;
+    double retraction_e_amount = extruder_attr[current_extruder].extruder_switch_retraction_distance * ((is_volumatric)? extruder_attr[current_extruder].filament_area : 1.0);
     if (extruder_attr[current_extruder].retraction_e_amount_current == retraction_e_amount)
     {
         return; 
