6d2ecc78118ac4d7869f8847d1259ff13b3bc46d
Ghostkeeper <ghost_keeper+github@hotmail.com>
2015-11-27 13:51:34 +0100

    Add "-" operator for TimeMaterialEstimates
    This allows you to get the estimates of segments of the g-code, such as what I want to do when making unit tests.

--- src/gcodePlanner.cpp
+++ src/gcodePlanner.cpp
@@ -7,6 +7,20 @@
 
 namespace cura {
 
+TimeMaterialEstimates TimeMaterialEstimates::operator-(const TimeMaterialEstimates& other)
+{
+    return TimeMaterialEstimates(extrude_time - other.extrude_time,unretracted_travel_time - other.unretracted_travel_time,retracted_travel_time - other.retracted_travel_time,material - other.material);
+}
+
+TimeMaterialEstimates& TimeMaterialEstimates::operator-=(const TimeMaterialEstimates& other)
+{
+    extrude_time -= other.extrude_time;
+    unretracted_travel_time -= other.unretracted_travel_time;
+    retracted_travel_time -= other.retracted_travel_time;
+    material -= other.material;
+    return *this;
+}
+
 GCodePath* GCodePlanner::getLatestPathWithConfig(GCodePathConfig* config, float flow)
 {
     std::vector<GCodePath>& paths = extruder_plans.back().paths;
