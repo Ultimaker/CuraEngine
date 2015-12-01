345c599062dae64f41ab3c6a4998433062c1270e
Tim Kuipers <t.kuipers@ultimaker.com>
2015-12-01 13:39:44 +0100

    bugfix: retraction_count_max ignored when < 0, retractions not occuring when ==0 (CURA-414)

--- src/gcodeExport.cpp
+++ src/gcodeExport.cpp
@@ -411,16 +411,21 @@ void GCodeExport::writeRetraction(RetractionConfig* config, bool force)
     {
         return;
     }
-    
+
+    { // handle retraction limitation
     double current_extruded_volume = getCurrentExtrudedVolume();
     std::deque<double>& extruded_volume_at_previous_n_retractions = extruder_attr[current_extruder].extruded_volume_at_previous_n_retractions;
-    while (int(extruded_volume_at_previous_n_retractions.size()) >= config->retraction_count_max) 
+    while (int(extruded_volume_at_previous_n_retractions.size()) >= config->retraction_count_max && !extruded_volume_at_previous_n_retractions.empty()) 
     {
         // extruder switch could have introduced data which falls outside the retraction window
         // also the retraction_count_max could have changed between the last retraction and this
         extruded_volume_at_previous_n_retractions.pop_back();
     }
-    if (!force && config->retraction_count_max > 0 && int(extruded_volume_at_previous_n_retractions.size()) == config->retraction_count_max - 1 
+    if (!force && config->retraction_count_max <= 0)
+    {
+        return;
+    }
+    if (!force && int(extruded_volume_at_previous_n_retractions.size()) == config->retraction_count_max - 1 
         && current_extruded_volume < extruded_volume_at_previous_n_retractions.back() + config->retraction_extrusion_window * extruder_attr[current_extruder].filament_area) 
     {
         return;
@@ -430,7 +435,8 @@ void GCodeExport::writeRetraction(RetractionConfig* config, bool force)
     {
         extruded_volume_at_previous_n_retractions.pop_back();
     }
-
+    }
+    
     extruder_attr[current_extruder].last_retraction_prime_speed = config->primeSpeed;
     
     double retraction_e_amount = config->distance * ((is_volumatric)? extruder_attr[current_extruder].filament_area : 1.0);
