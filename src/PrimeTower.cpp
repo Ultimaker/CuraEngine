321a9c11e05e2d29777a441009d020633ff8a838
Tim Kuipers <t.kuipers@ultimaker.com>
2015-12-01 15:03:21 +0100

    bugfix: dynamically sized array initialized as if constant (CURA-499)

--- src/PrimeTower.cpp
+++ src/PrimeTower.cpp
@@ -50,7 +50,8 @@ void PrimeTower::computePrimeTowerMax(SliceDataStorage& storage)
         
     extruder_count = storage.getSettingAsCount("machine_extruder_count");
     
-    int max_object_height_per_extruder[extruder_count] = { -1 }; // unitialize all as -1
+    int max_object_height_per_extruder[extruder_count]; 
+    std::fill_n(max_object_height_per_extruder, extruder_count, -1); // unitialize all as -1
     { // compute max_object_height_per_extruder
         for (SliceMeshStorage& mesh : storage.meshes)
         {
