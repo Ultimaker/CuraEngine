73152d84c7ed000e88e76e0122becbc0f7235902
Tim Kuipers <t.kuipers@ultimaker.com>
2015-12-02 14:03:18 +0100

    getLayerSecondOrInnermostWalls functions (CURA-522)

--- src/sliceDataStorage.cpp
+++ src/sliceDataStorage.cpp
@@ -27,6 +27,35 @@ void SliceLayer::getOutlines(Polygons& result, bool external_polys_only)
     }
 }
 
+Polygons SliceLayer::getSecondOrInnermostWalls()
+{
+    Polygons ret;
+    getSecondOrInnermostWalls(ret);
+    return ret;
+}
+
+void SliceLayer::getSecondOrInnermostWalls(Polygons& layer_walls)
+{
+    for (SliceLayerPart& part : parts)
+    {
+        // we want the 2nd inner walls
+        if (part.insets.size() >= 2) {
+            layer_walls.add(part.insets[1]);
+            continue;
+        }
+        // but we'll also take the inner wall if the 2nd doesn't exist
+        if (part.insets.size() == 1) {
+            layer_walls.add(part.insets[0]);
+            continue;
+        }
+        // offset_from_outlines was so large that it completely destroyed our isle,
+        // so we'll just use the regular outline
+        layer_walls.add(part.outline);
+        continue;
+    }
+}
+
+
 SliceDataStorage::SliceDataStorage(MeshGroup* meshgroup) : SettingsMessenger(meshgroup),
     meshgroup(meshgroup != nullptr ? meshgroup : new MeshGroup(FffProcessor::getInstance())), //If no mesh group is provided, we roll our own.
     retraction_config_per_extruder(initializeRetractionConfigs()),
@@ -92,6 +121,44 @@ Polygons SliceDataStorage::getLayerOutlines(int layer_nr, bool include_helper_pa
     }
 }
 
+Polygons SliceDataStorage::getLayerSecondOrInnermostWalls(int layer_nr, bool include_helper_parts)
+{
+    if (layer_nr < 0)
+    { // when processing raft
+        if (include_helper_parts)
+        {
+            return raftOutline;
+        }
+        else 
+        {
+            return Polygons();
+        }
+    }
+    else 
+    {
+        Polygons total;
+        for (SliceMeshStorage& mesh : meshes)
+        {
+            SliceLayer& layer = mesh.layers[layer_nr];
+            layer.getSecondOrInnermostWalls(total);
+            if (mesh.getSettingAsSurfaceMode("magic_mesh_surface_mode") != ESurfaceMode::NORMAL)
+            {
+                total = total.unionPolygons(layer.openPolyLines.offsetPolyLine(100));
+            }
+        }
+        if (include_helper_parts)
+        {
+            if (support.generated) 
+            {
+                total.add(support.supportLayers[layer_nr].supportAreas);
+                total.add(support.supportLayers[layer_nr].roofs);
+            }
+            total.add(primeTower.ground_poly);
+        }
+        return total;
+    }
+
+}
 
 
 
