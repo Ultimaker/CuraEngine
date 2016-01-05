73de288f44cf20ee7f25b37197c7f1fa3ffbd87f
Tim Kuipers <t.kuipers@ultimaker.com>
2016-01-05 11:30:43 +0100

    lil cleanup of unused commented out code (CURA-581)

--- src/PrintFeature.h
+++ src/PrintFeature.h
@@ -4,20 +4,6 @@
 namespace cura
 {
 
-/*
-enum class EPrintFeature : unsigned int // unused!!
-{ // TODO: use in gcodePathConfigs ?
-    OUTER_WALL,
-    INNER_WALLS,
-    INFILL,
-    SKIN,
-    HELPERS,
-    UNCLASSIFIED,
-    ENUM_COUNT
-};
-*/
-
-
 enum class PrintFeatureType
 {
     OuterWall,
