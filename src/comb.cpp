38c8941b59e4bd6832105eff8a4518f7681565cf
Tim Kuipers <t.kuipers@ultimaker.com>
2016-02-15 17:18:34 +0100

    Revert "fix: no move outside if it's a detour (CURA-579)"
    This reverts commit f28cf53651b907b9ea767a20faa35887b3c050bf.

--- src/comb.cpp
+++ src/comb.cpp
@@ -133,11 +133,6 @@ bool Comb::calc(Point startPoint, Point endPoint, CombPaths& combPaths, bool sta
                 outside_middle_to = endPoint;
                 inside_middle_to = endPoint;
             }
-
-            if (vSize(inside_middle_from - outside_middle_from) + vSize(inside_middle_to - outside_middle_to) > vSize(inside_middle_from - inside_middle_to))
-            { // via outside moves more over in_between than directly going to the end piece
-                avoid_other_parts = false;
-            }
         }
         
         if (startInside)
