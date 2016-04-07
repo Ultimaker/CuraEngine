13fe966db33c5a948935d849e6836e7bae394ce6
Tim Kuipers <t.kuipers@ultimaker.com>
2016-04-08 01:59:24 +0200

    feat: retrieve color from material (CURA-1371)

--- src/Material.cpp
+++ src/Material.cpp
@@ -1,5 +1,7 @@
 /** Copyright (C) 2016 Tim Kuipers - Released under terms of the AGPLv3 License */
 
+#include <limits> // numeric limits
+
 #include "Material.h"
 
 
@@ -17,5 +19,12 @@ void Material::setWidthHeight(int width, int height)
     this->height = height;
 }
 
+float Material::getColor(float x, float y)
+{
+    int w_idx = std::max(0, std::min(x * width, width - 1));
+    int h_idx = std::max(0, std::min(y * height, height - 1));
+    unsigned char r = data[(h_idx * width + w_idx) * 3];
+    return (float) r / std::numeric_limits<unsigned char>::max();
+}
 
 } // namespace cura
