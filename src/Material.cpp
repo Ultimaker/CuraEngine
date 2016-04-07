e04714ae40bedaeda5cf2a104038a269a8687b3e
Tim Kuipers <t.kuipers@ultimaker.com>
2016-04-08 01:03:48 +0200

    feat: loading of mtl (obj) (CURA-1371)

---
+++ src/Material.cpp
@@ -0,0 +1,10 @@
+/** Copyright (C) 2016 Tim Kuipers - Released under terms of the AGPLv3 License */
+
+#include "Material.h"
+
+
+namespace cura
+{
+
+
+} // namespace cura
---
+++ src/Material.h
@@ -0,0 +1,17 @@
+/** Copyright (C) 2016 Tim Kuipers - Released under terms of the AGPLv3 License */
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+namespace cura
+{
+
+class Material
+{
+public:
+protected:
+    
+};
+
+} // namespace cura
+
+#endif // MATERIAL_H
