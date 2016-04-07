07d7edaf1fd742cc40996f8a312532a516f7eeca
Tim Kuipers <t.kuipers@ultimaker.com>
2016-04-08 01:59:24 +0200

    feat: retrieve color from material (CURA-1371)

--- src/Material.h
+++ src/Material.h
@@ -10,6 +10,16 @@ class Material
 public:
     void setData(unsigned char* data);
     void setWidthHeight(int width, int height);
+    /*!
+     * get some value representing the getColor
+     * 
+     * red?
+     * 
+     * TODO
+     * 
+     * \return a value between zero and one
+     */
+    float getColor(float x, float y);
 protected:
     unsigned char* data; //!< pixel data in rgb-row-first (or bgr-row first ?)
     int width, height; //!< image dimensions
