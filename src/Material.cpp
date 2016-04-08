78e1595b83a55292a9e558bb90f97feab032bedf
Tim Kuipers <t.kuipers@ultimaker.com>
2016-04-08 15:39:59 +0200

    fix: lil int casting thing (CURA-1371)

--- src/Material.cpp
+++ src/Material.cpp
@@ -1,6 +1,7 @@
 /** Copyright (C) 2016 Tim Kuipers - Released under terms of the AGPLv3 License */
 
 #include <limits> // numeric limits
+#include <algorithm> // min max
 
 #include "Material.h"
 
@@ -21,8 +22,8 @@ void Material::setWidthHeight(int width, int height)
 
 float Material::getColor(float x, float y)
 {
-    int w_idx = std::max(0, std::min(x * width, width - 1));
-    int h_idx = std::max(0, std::min(y * height, height - 1));
+    int w_idx = std::max(0, std::min(int (x * width), width - 1));
+    int h_idx = std::max(0, std::min(int (y * height), height - 1));
     unsigned char r = data[(h_idx * width + w_idx) * 3];
     return (float) r / std::numeric_limits<unsigned char>::max();
 }
