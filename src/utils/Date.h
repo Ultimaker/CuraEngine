58234e41257345015c423c7c030e6736440fda2e
Tim Kuipers <t.kuipers@ultimaker.com>
2016-04-25 16:45:21 +0200

    doc: Date documentation (CURA-520)

--- src/utils/Date.h
+++ src/utils/Date.h
@@ -7,16 +7,21 @@
 namespace cura
 {
 
+/*!
+ * Simple class to represent a year, month and day.
+ */
 class Date
 {
 public:
-    Date(int year, int month, int day);
-    static Date getDate();
-    std::string toStringDashed();
+    Date(int year, int month, int day); //!< Simple constructor
+    static Date getDate(); //!< Get the current date (compile time)
+    std::string toStringDashed(); //!< Get a formatted string: yyyy-mm-dd
 protected:
-    int year, month, day;
+    int year; //!< Year, e.g. 2016
+    int month; //!< Month, e.g. 12, i.e. starting at 1
+    int day; //!< Day, e.g. 31, i.e. starting at 1
 private:
-    Date();
+    Date(); //!< Simple constructor initializing all to -1
 };
 
 } // namespace cura
