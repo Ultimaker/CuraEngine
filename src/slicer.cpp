0750c075aacc2f6966e38635d03607ece9426051
Tim Kuipers <t.kuipers@ultimaker.com>
2016-05-20 15:28:18 +0200

    fix: mesh surface mode should still connect line segments in polylines (CURA-833)

--- src/slicer.cpp
+++ src/slicer.cpp
@@ -510,24 +510,9 @@ Slicer::Slicer(Mesh* mesh, int initial, int thickness, int layer_count, bool kee
             layers[layer_nr].segments.push_back(s);
         }
     }
-    if (mesh->getSettingAsSurfaceMode("magic_mesh_surface_mode") == ESurfaceMode::SURFACE)
+    for(unsigned int layer_nr=0; layer_nr<layers.size(); layer_nr++)
     {
-        for (SlicerLayer& layer : layers)
-        {
-            for (SlicerSegment& segment : layer.segments)
-            {
-                PolygonRef line = layer.openPolylines.newPoly();
-                line.add(segment.start);
-                line.add(segment.end);
-            }
-        }
-    }
-    else
-    {
-        for(unsigned int layer_nr=0; layer_nr<layers.size(); layer_nr++)
-        {
-            layers[layer_nr].makePolygons(mesh, keep_none_closed, extensive_stitching);
-        }
+        layers[layer_nr].makePolygons(mesh, keep_none_closed, extensive_stitching);
     }
 
     int surface_thickness = mesh->getSettingInMicrons("magic_surface_thickness");
