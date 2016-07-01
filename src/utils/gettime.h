3b8771e4c846d81302d4f3966e720248e55e5fcf
Scott Lenser <slenser@gmail.com>
2016-07-01 19:52:24 -0400

    Switched to cpu usage time instead of wall clock for more stable profiling.
    This makes the results more stable and more accurate for purposes of figuring
    out how long the actual processing is taking.

--- src/utils/gettime.h
+++ src/utils/gettime.h
@@ -2,11 +2,14 @@
 #ifndef GETTIME_H
 #define GETTIME_H
 
+
 #ifdef __WIN32
 #include <windows.h>
 #else
+#include <sys/resource.h>
 #include <sys/time.h>
 #include <stddef.h>
+#include <cassert>
 #endif
 
 namespace cura
@@ -16,9 +19,17 @@ static inline double getTime()
 #ifdef __WIN32
     return double(GetTickCount()) / 1000.0;
 #else
-    struct timeval tv;
-    gettimeofday(&tv, nullptr);
-    return double(tv.tv_sec) + double(tv.tv_usec) / 1000000.0;
+    int ret;
+    struct rusage usage;
+    ret = getrusage(RUSAGE_SELF,&usage);
+    assert(ret==0);
+    double user_time =  double(usage.ru_utime.tv_sec) + double(usage.ru_utime.tv_usec) / 1000000.0;
+    double sys_time  =  double(usage.ru_stime.tv_sec) + double(usage.ru_stime.tv_usec) / 1000000.0;
+    return user_time + sys_time;
+                    
+    //struct timeval tv;
+    //gettimeofday(&tv, nullptr);
+    //return double(tv.tv_sec) + double(tv.tv_usec) / 1000000.0;
 #endif
 }
 
