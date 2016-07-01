7d452c33e281476c8c43250fba0bc05f2a0ec00d
Scott Lenser <slenser@gmail.com>
2016-07-01 18:34:28 -0400

    Added missing file.

---
+++ src/mason/Time.hpp
@@ -0,0 +1,14 @@
+/** Copyright (C) 2016 Scott Lenser - Released under terms of the AGPLv3 License */
+
+#ifndef INCLUDED_MASON_TIME_HPP
+#define INCLUDED_MASON_TIME_HPP
+
+#include "../utils/gettime.h"
+
+namespace mason {
+
+using TimeKeeper = cura::TimeKeeper;
+
+}
+
+#endif
