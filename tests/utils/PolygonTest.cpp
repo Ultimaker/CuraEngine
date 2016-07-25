ffb4cdaacdfc89445a61c21ce2c25411403abc4d
Tim Kuipers <t.kuipers@ultimaker.com>
2016-07-25 11:35:29 +0200

    refactor: split off PolygonTest from PolygonUtilsTest (CURA-1981)

---
+++ tests/utils/PolygonTest.cpp
@@ -0,0 +1,58 @@
+//Copyright (c) 2015 Ultimaker B.V.
+//CuraEngine is released under the terms of the AGPLv3 or higher.
+
+#include "PolygonTest.h"
+
+
+namespace cura
+{
+    CPPUNIT_TEST_SUITE_REGISTRATION(PolygonTest);
+
+void PolygonTest::setUp()
+{
+    test_square.emplace_back(0, 0);
+    test_square.emplace_back(100, 0);
+    test_square.emplace_back(100, 100);
+    test_square.emplace_back(0, 100);
+
+
+    pointy_square.emplace_back(0, 0);
+    pointy_square.emplace_back(47, 0);
+    pointy_square.emplace_back(50, 80);
+    pointy_square.emplace_back(53, 0);
+    pointy_square.emplace_back(100, 0);
+    pointy_square.emplace_back(100, 100);
+    pointy_square.emplace_back(55, 100);
+    pointy_square.emplace_back(50, 180);
+    pointy_square.emplace_back(45, 100);
+    pointy_square.emplace_back(0, 100);
+}
+
+void PolygonTest::tearDown()
+{
+    //Do nothing.
+}
+
+
+void PolygonTest::polygonOffsetTest()
+{
+    Polygons test_squares;
+    test_squares.add(test_square);
+    Polygons expanded = test_squares.offset(25);
+    int64_t expanded_length = expanded.polygonLength();
+
+    Polygons square_hole;
+    PolygonRef square_inverted = square_hole.newPoly();
+    for (int i = test_square.size() - 1; i >= 0; i--)
+    {
+        square_inverted.add(test_square[i]);
+    }
+    Polygons contracted = square_hole.offset(25);
+    int64_t contracted_length = contracted.polygonLength();
+
+    CPPUNIT_ASSERT_MESSAGE("Offset on outside poly is different from offset on inverted poly!", std::abs(expanded_length - contracted_length) < 5);
+}
+
+
+
+}
---
+++ tests/utils/PolygonTest.h
@@ -0,0 +1,59 @@
+//Copyright (c) 2015 Ultimaker B.V.
+//CuraEngine is released under the terms of the AGPLv3 or higher.
+
+#ifndef POLYGON_TEST_H
+#define POLYGON_TEST_H
+
+#include <functional> // function
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "../src/utils/intpoint.h"
+#include "../src/utils/polygon.h"
+
+namespace cura
+{
+
+class PolygonTest : public CppUnit::TestFixture
+{
+    CPPUNIT_TEST_SUITE(PolygonTest);
+    CPPUNIT_TEST(polygonOffsetTest);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    /*!
+     * \brief Sets up the test suite to prepare for testing.
+     * 
+     * Since <em>LinearAlg2DTest</em> only has static functions, no instance
+     * needs to be created here.
+     */
+    void setUp();
+
+    /*!
+     * \brief Tears down the test suite when testing is done.
+     * 
+     * Since <em>LinearAlg2DTest</em> only has static functions, no instance
+     * exists that needs to be destroyed.
+     */
+    void tearDown();
+    
+    //These are the actual test cases. The name of the function sort of describes what it tests but I refuse to document all of these, sorry.
+    void polygonOffsetTest();
+
+
+private:
+    /*!
+     * \brief The maximum allowed error in distance measurements.
+     */
+    static const int64_t maximum_error = 10;
+    
+    Polygon test_square;
+    Polygon pointy_square;
+
+};
+
+}
+
+#endif // POLYGON_TEST_H
+
