ce1fe33b2e382452877802c50df2d8535415cb4b
Scott Lenser <slenser@gmail.com>
2016-07-25 21:40:42 -0400

    Slicer: Added better documentation on SlicerLayer::Terminus::Index.

--- src/slicer.h
+++ src/slicer.h
@@ -135,7 +135,10 @@ private:
     class Terminus
     {
     public:
-        /*! A representation of Terminus that can be used as an array index. */
+        /*! A representation of Terminus that can be used as an array index.
+         *
+         * See \ref asIndex() for more information.
+         */
         using Index = size_t;
 
         /*! A Terminus value representing an invalid value.
@@ -179,6 +182,20 @@ private:
         }
 
         /*! Gets the Index representation of this Terminus.
+         *
+         * The index representation much satisfy the following:
+         * 1. for all Terminus t0, t1: t0 == t1 implies t0.asIndex() == t1.asIndex()
+         * 2. for all Terminus t0, t1: t0 != t1 implies t0.asIndex() != t1.asIndex()
+         * 3. t0.asIndex() >= 0
+         * 4. if y = \ref endIndexFromPolylineEndIndex(x), then for all Terminus t
+         *       if t.getPolylineIdx() < x then t.asIndex() < y
+         *
+         * In addition, the Index representation should be reasonably
+         * compact for efficiency.  This means that for polyline index
+         * in [0,x) and Terminus t with t.getPolylineIdx() < x, the
+         * set of containing all t.asIndex() union {0} should be
+         * small.  In other words, t.asIndex() should map to [0,y)
+         * where y is as small as possible.
          */
         Index asIndex() const
         {
@@ -197,7 +214,11 @@ private:
             return polyline_end_idx*2;
         }
 
-        /*! Tests for equality. */
+        /*! Tests for equality.
+        *
+        * Two Terminus are equal if they return the same results for
+        * \ref getPolylineIdx() and \ref isEnd().
+        */
         bool operator==(const Terminus &other)
         {
             return m_idx == other.m_idx;
