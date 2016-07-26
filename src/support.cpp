b8691c776e4207eb2c68b55223b5fe2a7494e0ca
Tim Kuipers <t.kuipers@ultimaker.com>
2016-07-26 18:31:41 +0200

    fix: (un)signed int syntax mistake (CUTA-1013)

--- src/support.cpp
+++ src/support.cpp
@@ -476,8 +476,8 @@ void AreaSupport::generateSupportRoofs(SliceDataStorage& storage, const SliceMes
     {
         SupportLayer& layer = supportLayers[layer_idx];
 
-        const int layer_idx_above = layer_idx + roof_layer_count + z_distance_top;
-        const int layer_idx_below = std::max(0, layer_idx - bottom_layer_count - z_distance_bottom);
+        const unsigned int layer_idx_above = layer_idx + roof_layer_count + z_distance_top;
+        const unsigned int layer_idx_below = std::max(0, int(layer_idx) - int(bottom_layer_count) - z_distance_bottom);
         if (layer_idx_above < supportLayers.size())
         {
             Polygons roofs;
