f810356bdb455030de9698c168a4bb586591e8b9
Ghostkeeper <rubend@tutanota.com>
2016-07-27 16:48:03 +0200

    Add draft shield height limitation enum
    This enum will be needed to determine what height we should use for the draft shield.
    
    Contributes to issue CURA-1278.

--- src/settings/settings.cpp
+++ src/settings/settings.cpp
@@ -181,6 +181,20 @@ double SettingsBaseVirtual::getSettingInSeconds(std::string key) const
     return std::max(0.0, atof(value.c_str()));
 }
 
+DraftShieldLimitation SettingsBaseVirtual::getSettingAsDraftShieldLimitation(const std::string key) const
+{
+    const std::string value = getSettingString(key);
+    if (value == "full")
+    {
+        return DraftShieldLimitation::FULL;
+    }
+    else if (value == "limited")
+    {
+        return DraftShieldLimitation::LIMITED;
+    }
+    return DraftShieldLimitation::FULL; //Default.
+}
+
 FlowTempGraph SettingsBaseVirtual::getSettingAsFlowTempGraph(std::string key) const
 {
     FlowTempGraph ret;
