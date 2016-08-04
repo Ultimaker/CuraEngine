8cf1f1646b3fc8bb2ddb36df017972ac0c84fd43
Tim Kuipers <t.kuipers@ultimaker.com>
2016-08-04 17:52:41 +0200

    safety: check for duplicate points in input polygon (CURA-1911)

--- src/utils/ListPolyIt.cpp
+++ src/utils/ListPolyIt.cpp
@@ -17,14 +17,18 @@ void ListPolyIt::convertPolygonsToLists(Polygons& polys, ListPolygons& result)
         result.emplace_back();
 #ifdef DEBUG
         Point last = poly.back();
-#endif
+#endif // DEBUG
         for (Point& p : poly) 
         {
             result.back().push_back(p);
 #ifdef DEBUG
+            // usually polygons shouldn't have such degenerate verts
+            // in PolygonProximityLinker (where this function is (also) used) it is
+            // required to not have degenerate verts, because verts are mapped
+            // to links, but if two different verts are at the same place the mapping fails.
             assert(p != last);
             last = p;
-#endif
+#endif // DEBUG
         }
     }
 }
