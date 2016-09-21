1a2ba63c748eca0d82f04ab3eccdedb089260d8a
Tim Kuipers <t.kuipers@ultimaker.com>
2016-09-21 18:03:11 +0200

    lil cleanup (CURA-2420)

--- src/PrimeTower.h
+++ src/PrimeTower.h
@@ -23,24 +23,17 @@ private:
 
     Point wipe_point;
 
-    class WallInfill
-    {
-        
-    };
+    std::vector<PolyLine> extruder_paths; //!< Precompiled 
+
 public:
+    Polygons ground_poly; //!< The outline of the prime tower to be used for each layer
+
     void initConfigs(MeshGroup* meshgroup, std::vector<RetractionConfig>& retraction_config_per_extruder);
     void setConfigs(MeshGroup* configs, int layer_thickness);
-    
-    Polygons ground_poly;
-    
-    std::vector<PolyLine> extruder_paths;
-    
-    
+
     void generateGroundpoly(SliceDataStorage& storage);
 
     std::vector<std::vector<Polygons>> patterns_per_extruder; //!< for each extruder a vector of patterns to alternate between, over the layers
-    
-    void generatePaths_denseInfill(SliceDataStorage& storage);
 
     /*!
      * Generate the area where the prime tower should be.
@@ -51,10 +44,13 @@ public:
     void generatePaths(SliceDataStorage& storage, unsigned int total_layers);
 
     void computePrimeTowerMax(SliceDataStorage& storage);
-    
+
     PrimeTower();
 
     void addToGcode(SliceDataStorage& storage, GCodePlanner& gcodeLayer, GCodeExport& gcode, int layer_nr, int prev_extruder, bool prime_tower_dir_outward, bool wipe, int* last_prime_tower_poly_printed);
+private:
+    void generatePaths_denseInfill(SliceDataStorage& storage);
+
     void addToGcode_denseInfill(SliceDataStorage& storage, GCodePlanner& gcodeLayer, GCodeExport& gcode, int layer_nr, int prev_extruder, bool prime_tower_dir_outward, bool wipe, int* last_prime_tower_poly_printed);
 
 };
