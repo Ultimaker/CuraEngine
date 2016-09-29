86e7dbf2b14d6adec3390ec639f3aef9bc4af9a0
Tim Kuipers <t.kuipers@ultimaker.com>
2016-09-29 10:00:36 +0200

    lil writeInt2mm improvements (CURA-2472)

--- src/utils/string.h
+++ src/utils/string.h
@@ -29,22 +29,23 @@ static inline int stringcasecompare(const char* a, const char* b)
  */
 static inline void writeInt2mm(int64_t coord, std::ostream& ss)
 {
-    char buffer[16];
-    int n_chars = sprintf(buffer, "%ld", coord); // convert int to string
-    int end_pos = n_chars; // the first cahracter not to write any more
-    int trailing_zeros;
-    for (trailing_zeros = 1; trailing_zeros < 4 && buffer[n_chars - trailing_zeros] == '0'; trailing_zeros++)
+    char buffer[24];
+    int char_count = sprintf(buffer, "%ld", coord); // convert int to string
+    int end_pos = char_count; // the first character not to write any more
+    int trailing_zeros = 1;
+    while (trailing_zeros < 4 && buffer[char_count - trailing_zeros] == '0')
     {
+        trailing_zeros++;
     }
     trailing_zeros--;
-    end_pos = n_chars - trailing_zeros;
+    end_pos = char_count - trailing_zeros;
     if (trailing_zeros == 3)
     { // no need to write the decimal dot
-        buffer[n_chars - trailing_zeros] = '\0';
+        buffer[char_count - trailing_zeros] = '\0';
         ss << buffer;
         return;
     }
-    if (n_chars <= 3)
+    if (char_count <= 3)
     {
         int start = 0; // where to start writing from the buffer
         if (coord < 0)
@@ -53,18 +54,18 @@ static inline void writeInt2mm(int64_t coord, std::ostream& ss)
             start = 1;
         }
         ss << '.';
-        for (int nulls = n_chars - start; nulls < 3; nulls++)
+        for (int nulls = char_count - start; nulls < 3; nulls++)
         { // fill up to 3 decimals with zeros
             ss << '0';
         }
-        buffer[n_chars - trailing_zeros] = '\0';
+        buffer[char_count - trailing_zeros] = '\0';
         ss << (static_cast<char*>(buffer) + start);
     }
     else
     {
         char prev = '.';
         int pos;
-        for (pos = n_chars - 3; pos <= end_pos; pos++)
+        for (pos = char_count - 3; pos <= end_pos; pos++)
         { // shift all characters and insert the decimal dot
             char next_prev = buffer[pos];
             buffer[pos] = prev;
