0a683ff05e6cf05aaffd0f7ec3296a5661a13b18
Ghostkeeper <rubend@tutanota.com>
2016-10-21 15:39:19 +0200

    Transform order algorithm to our code style
    Lukasz needs to obey the rulesz!
    
    Contributes to issue CURA-1103.

--- src/utils/algorithm.h
+++ src/utils/algorithm.h
@@ -14,26 +14,30 @@ namespace cura
 /*!
  * Get the order of a vector: the sorted indices of a vector
  * 
- * {1.6, 1.8, 1.7} returns {1, 3, 2} meaning {in[1], in[3], in[2]} is a sorted vector
- * 
+ * {1.6, 1.8, 1.7} returns {1, 3, 2} meaning {in[1], in[3], in[2]} is a sorted
+ * vector
  * 
  * Thanks to Lukasz Wiklendt
  * 
  * \param in The vector for which to get the order
  * \return An ordered vector of indices into \p in 
  */
-template <typename T>
-std::vector<size_t> order(const std::vector<T> &in) {
+template<typename T>
+std::vector<size_t> order(const std::vector<T> &in)
+{
+    // initialize original index locations
+    std::vector<size_t> order(in.size());
+    std::iota(order.begin(), order.end(), 0); // fill vector with 1, 2, 3,.. etc
 
-  // initialize original index locations
-  std::vector<size_t> order(in.size());
-  std::iota(order.begin(), order.end(), 0); // fill vector with 1, 2, 3,.. etc
+    // sort indexes based on comparing values in v
+    std::sort(order.begin(), order.end(),
+        [&in](size_t i1, size_t i2)
+        {
+            return in[i1] < in[i2];
+        }
+    );
 
-  // sort indexes based on comparing values in v
-  std::sort(order.begin(), order.end(),
-       [&in](size_t i1, size_t i2) {return in[i1] < in[i2];});
-
-  return order;
+    return order;
 }
 
 } // namespace cura
