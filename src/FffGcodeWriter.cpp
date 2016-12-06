b0487fa4c70e9f7ede47680cbeca6271712391b0
Ghostkeeper <rubend@tutanota.com>
2016-12-06 15:57:39 +0100

    Use filament park distance setting instead of direct heat & cool zones
    The sum of the heat and cool zone lengths is now computed via inheritance in the front-end.
    
    Contributes to issue CURA-2795.

--- src/FffGcodeWriter.cpp
+++ src/FffGcodeWriter.cpp
@@ -143,7 +143,7 @@ void FffGcodeWriter::setConfigRetraction(SliceDataStorage& storage)
         ExtruderTrain* train = storage.meshgroup->getExtruderTrain(extruder);
         RetractionConfig& retraction_config = storage.retraction_config_per_extruder[extruder];
         retraction_config.distance = (train->getSettingBoolean("retraction_enable"))? train->getSettingInMillimeters("retraction_amount") : 0;
-        retraction_config.park_distance = train->getSettingInMillimeters("machine_heat_zone_length") + train->getSettingInMillimeters("machine_cool_zone_length");
+        retraction_config.park_distance = train->getSettingInMillimeters("machine_filament_park_distance");
         retraction_config.prime_volume = train->getSettingInCubicMillimeters("retraction_extra_prime_amount");
         retraction_config.speed = train->getSettingInMillimetersPerSecond("retraction_retract_speed");
         retraction_config.primeSpeed = train->getSettingInMillimetersPerSecond("retraction_prime_speed");
@@ -154,7 +154,7 @@ void FffGcodeWriter::setConfigRetraction(SliceDataStorage& storage)
 
         RetractionConfig& switch_retraction_config = storage.extruder_switch_retraction_config_per_extruder[extruder];
         switch_retraction_config.distance = train->getSettingInMillimeters("switch_extruder_retraction_amount"); 
-        switch_retraction_config.park_distance = train->getSettingInMillimeters("machine_heat_zone_length") + train->getSettingInMillimeters("machine_cool_zone_length");
+        switch_retraction_config.park_distance = train->getSettingInMillimeters("machine_filament_park_distance");
         switch_retraction_config.prime_volume = 0.0;
         switch_retraction_config.speed = train->getSettingInMillimetersPerSecond("switch_extruder_retraction_speed");
         switch_retraction_config.primeSpeed = train->getSettingInMillimetersPerSecond("switch_extruder_prime_speed");
