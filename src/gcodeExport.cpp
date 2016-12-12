2067644d30f95fde7d124447ed9c8fb4bd3dd261
Ghostkeeper <rubend@tutanota.com>
2016-12-12 12:54:37 +0100

    Revert "Update prime speed corresponding to park retraction"
    This reverts commit 6c986e6cfe296de3f9a27f76ebee81c2b6989b3c.
    
    Contributes to issue CURA-2795.

--- src/gcodeExport.cpp
+++ src/gcodeExport.cpp
@@ -739,7 +739,6 @@ void GCodeExport::writePark(const RetractionConfig& config)
     currentSpeed = config.speed * 60;
     estimateCalculator.plan(TimeEstimateCalculator::Position(INT2MM(currentPosition.x), INT2MM(currentPosition.y), INT2MM(currentPosition.z), eToMm(current_e_value)), currentSpeed);
     extruder_attributes.retraction_e_amount_current = target_retraction_e;
-    extruder_attributes.last_retraction_prime_speed = config.primeSpeed;
 }
 
 void GCodeExport::writeZhopStart(int hop_height)
