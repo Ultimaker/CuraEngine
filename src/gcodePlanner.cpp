38a1ee4270ba9c13c19d56c991244e8cf893cab0
Ghostkeeper <rubend@tutanota.com>
2016-12-12 12:54:25 +0100

    Revert "Allow going to stand-by temperature if nozzle is later used"
    This reverts commit 3c0d3f1b2d5012cb6ff615f4089056de374041ab.
    
    Contributes to issue CURA-2795.

--- src/gcodePlanner.cpp
+++ src/gcodePlanner.cpp
@@ -671,7 +671,7 @@ void GCodePlanner::writeGCode(GCodeExport& gcode)
             extruder = extruder_plan.extruder;
 
             const int prev_layer_nr = (extruder_plan_idx == 0) ? layer_nr - 1 : layer_nr;
-            const bool turn_off_extruder = prev_layer_nr >= storage.max_print_height_per_extruder[prev_extruder]; //Previous extruder is never used any more after this.
+            const bool turn_off_extruder = extruder_plan.prev_extruder_standby_temp && prev_layer_nr >= storage.max_print_height_per_extruder[prev_extruder]; //Previous extruder is never used any more after this.
             gcode.switchExtruder(extruder, storage.extruder_switch_retraction_config_per_extruder[prev_extruder], turn_off_extruder);
 
             if (train->getSettingInMillimetersPerSecond("max_feedrate_z_override") > 0)
@@ -688,14 +688,11 @@ void GCodePlanner::writeGCode(GCodeExport& gcode)
             gcode.writePrimeTrain(train->getSettingInMillimetersPerSecond("speed_travel"));
             gcode.writeRetraction(&retraction_config);
 
-            if (extruder_plan.prev_extruder_standby_temp)
+            if (turn_off_extruder)
             { //Turn off previous extruder. Must happen after the extruder switch to prevent accumulation of heat-up times.
                 constexpr bool wait = false;
                 double prev_extruder_temp = *extruder_plan.prev_extruder_standby_temp;
-                if (turn_off_extruder)
-                {
-                    prev_extruder_temp = 0; // TODO ? should there be a setting for extruder_off_temperature ?
-                }
+                prev_extruder_temp = 0; // TODO ? should there be a setting for extruder_off_temperature ?
                 gcode.writeTemperatureCommand(prev_extruder, prev_extruder_temp, wait);
             }
         }
