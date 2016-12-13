120a9c440cee9ba45ede3b143eef01192271630f
Ghostkeeper <rubend@tutanota.com>
2016-12-13 15:17:26 +0100

    Revert "Park filament even if printer doesn't support retraction"
    This reverts commit 837f992f697806e7d8667c7504d9317e359e345b.
    
    Contributes to issue CURA-2795.

--- src/gcodeExport.cpp
+++ src/gcodeExport.cpp
@@ -723,6 +723,12 @@ void GCodeExport::writeRetraction(const RetractionConfig& config, bool force, bo
 
 void GCodeExport::writePark(const RetractionConfig& config)
 {
+    if (flavor == EGCodeFlavor::BFB) //BitsFromBytes doesn't allow retracting a manual amount. Do a normal retract instead.
+    {
+        writeRetraction(&config);
+        return;
+    }
+
     ExtruderTrainAttributes extruder_attributes = extruder_attr[current_extruder];
 
     const double old_retraction_e = extruder_attributes.retraction_e_amount_current;
@@ -730,6 +736,11 @@ void GCodeExport::writePark(const RetractionConfig& config)
     const double to_retract_e = target_retraction_e - old_retraction_e;
     if (to_retract_e < 0.001) //Already in parking position or beyond.
     {
+        return;
+    }
+    if (firmware_retract) //Doesn't allow retracting a manual amount either, but we can track the retractions.
+    {
+        writeRetraction(&config);
         return;
     }
 
