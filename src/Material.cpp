c3689e0b58c14adac0d761b72ef9a7ed088dc82d
Tim Kuipers <t.kuipers@ultimaker.com>
2016-12-14 13:34:05 +0100

    feat: more color options in Material (CURA-1371)

--- src/Material.cpp
+++ src/Material.cpp
@@ -4,6 +4,7 @@
 #include <algorithm> // min max
 
 #include <iostream>
+#include <cassert>
 
 #include "Material.h"
 
@@ -11,24 +12,77 @@
 namespace cura
 {
 
+Material::Material()
+: data(nullptr)
+, width(0)
+, height(0)
+, depth(0)
+{
+
+}
+
+
 void Material::setData(unsigned char* data)
 {
     this->data = data;
 }
 
-void Material::setDimensions(int width, int height, int depth)
+void Material::setDimensions(unsigned int width, unsigned int height, unsigned int depth)
 {
     this->width = width;
     this->height = height;
     this->depth = depth;
 }
 
-float Material::getColor(float x, float y) const
+float Material::getRed(float x, float y) const
 {
-    int w_idx = std::max(0, std::min(int (x * width), width - 1));
-    int h_idx = std::max(0, std::min(int (y * height), height - 1));
-    unsigned char r = data[(h_idx * width + w_idx) * depth];
-    return (float) r / std::numeric_limits<unsigned char>::max();
+    return getColor(x, y, 0);
 }
+float Material::getGreen(float x, float y) const
+{
+    return getColor(x, y, 1);
+}
+float Material::getBlue(float x, float y) const
+{
+    return getColor(x, y, 2);
+}
+float Material::getAlpha(float x, float y) const
+{
+    return getColor(x, y, 3);
+}
+
+float Material::getGrey(float x, float y) const
+{
+    unsigned int w_idx, h_idx;
+    getPixelCoords(x, y, w_idx, h_idx);
+    unsigned int r = getColorData(w_idx, h_idx, 0);
+    unsigned int g = getColorData(w_idx, h_idx, 1);
+    unsigned int b = getColorData(w_idx, h_idx, 2);
+    return (float) (r + g + b) / std::numeric_limits<unsigned char>::max() / 3.0;
+}
+
+
+float Material::getColor(float x, float y, unsigned int z) const
+{
+    assert((int)z >= 0 && z < depth && "Z out of bounds!");
+    unsigned int w_idx, h_idx;
+    getPixelCoords(x, y, w_idx, h_idx);
+    unsigned char col = getColorData(w_idx, h_idx, z);
+    return (float) col / std::numeric_limits<unsigned char>::max();
+}
+
+void Material::getPixelCoords(const float x_in, const float y_in, unsigned int& x_out, unsigned int& y_out) const
+{
+    x_out = std::max(0u, std::min((unsigned int) (x_in * width), width - 1));
+    y_out = std::max(0u, std::min((unsigned int) (y_in * height), height - 1));
+}
+
+
+unsigned char Material::getColorData(unsigned int x, unsigned int y, unsigned int z) const
+{
+    return data[(y * width + x) * depth + z];
+}
+
+
 
 } // namespace cura
--- src/Material.h
+++ src/Material.h
@@ -8,21 +8,101 @@ namespace cura
 class Material
 {
 public:
-    void setData(unsigned char* data);
-    void setDimensions(int width, int height, int depth);
     /*!
-     * get some value representing the getColor
+     * non-initializing constructor
+     */
+    Material();
+
+    /*!
+     * Set the pixel data of the image
+     * \param data pointer to the array of data in RGBA, left-to-right, top-to-bottom
+     */
+    void setData(unsigned char* data);
+
+    /*!
+     * Set the dimensions of the image
+     * \param width The horizontal length of the imnage
+     * \param height The vertical length of the imnage
+     * \param depth The number of color channels
+     */
+    void setDimensions(unsigned int width, unsigned int height, unsigned int depth);
+
+    /*!
+     * get the red value at a particular place in the image
      * 
-     * red?
-     * 
-     * TODO
-     * 
+     * \param x place in the horizontal direction left to right (value between zero and one)
+     * \param y place in the vertical direction top to bottom (value between zero and one)
      * \return a value between zero and one
      */
-    float getColor(float x, float y) const;
+    float getRed(float x, float y) const;
+    /*!
+     * get the green value at a particular place in the image
+     * 
+     * \param x place in the horizontal direction left to right (value between zero and one)
+     * \param y place in the vertical direction top to bottom (value between zero and one)
+     * \return a value between zero and one
+     */
+    float getGreen(float x, float y) const;
+    /*!
+     * get the blue value at a particular place in the image
+     * 
+     * \param x place in the horizontal direction left to right (value between zero and one)
+     * \param y place in the vertical direction top to bottom (value between zero and one)
+     * \return a value between zero and one
+     */
+    float getBlue(float x, float y) const;
+    /*!
+     * get the alpha value at a particular place in the image
+     * 
+     * \param x place in the horizontal direction left to right (value between zero and one)
+     * \param y place in the vertical direction top to bottom (value between zero and one)
+     * \return a value between zero and one
+     */
+    float getAlpha(float x, float y) const;
+    /*!
+     * get the R/G/B/A value at a particular place in the image
+     * 
+     * for z:
+     * 0=red
+     * 1=green
+     * 2=blue
+     * 3=alpha
+     * 
+     * \param x place in the horizontal direction left to right (value between zero and one)
+     * \param y place in the vertical direction top to bottom (value between zero and one)
+     * \param z The color channel to check
+     * \return a value between zero and one
+     */
+    float getColor(float x, float y, unsigned int z) const;
+    /*!
+     * get the grey value at a particular place in the image
+     * 
+     * \param x place in the horizontal direction left to right (value between zero and one)
+     * \param y place in the vertical direction top to bottom (value between zero and one)
+     * \return a value between zero and one
+     */
+    float getGrey(float x, float y) const;
 protected:
     unsigned char* data; //!< pixel data in rgb-row-first (or bgr-row first ?)
-    int width, height, depth; //!< image dimensions
+    unsigned int width, height, depth; //!< image dimensions
+
+    /*!
+     * Get a color value from the data
+     * \param x place in the horizontal direction left to right (value between zero and one)
+     * \param y place in the vertical direction top to bottom (value between zero and one)
+     * \return the color data (0-256)
+     */
+    unsigned char getColorData(unsigned int x, unsigned int y, unsigned int z) const;
+
+    /*!
+     * Convert float coordinates to pixel coordinates
+     * 
+     * \param[in] x_in place in the horizontal direction left to right (value between zero and one)
+     * \param[in] y_in place in the vertical direction top to bottom (value between zero and one)
+     * \param[out] x_out The x component of the pixel location
+     * \param[out] y_out The y component of the pixel location
+     */
+    void getPixelCoords(const float x_in, const float y_in, unsigned int& x_out, unsigned int& y_out) const;
 };
 
 } // namespace cura
