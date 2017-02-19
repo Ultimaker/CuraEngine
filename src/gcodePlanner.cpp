bed44cbef1f1020e94a605917cfc589ca616dec7
Mark Burton <markb@smartavionics.com>
2017-02-19 16:59:37 +0000

    Only add one polygon when when processing insets in spiralize mode.
    For me, this makes spiralize usable. Before, it was outputting two perimeter
    lines (on top of each other) instead of one.
    
    Whether this is the best place to fix this problem, I do not know.

--- src/gcodePlanner.cpp
+++ src/gcodePlanner.cpp
@@ -409,15 +409,23 @@ void GCodePlanner::addPolygonsByOptimizer(Polygons& polygons, GCodePathConfig* c
     {
         return;
     }
-    PathOrderOptimizer orderOptimizer(lastPosition, z_seam_pos, z_seam_type);
-    for (unsigned int poly_idx = 0; poly_idx < polygons.size(); poly_idx++)
+    if (spiralize)
     {
-        orderOptimizer.addPolygon(polygons[poly_idx]);
+        // only interested in the outer perimeter
+        addPolygon(polygons[0], 0, config, wall_overlap_computation, wall_0_wipe_dist, spiralize);
     }
-    orderOptimizer.optimize();
-    for (unsigned int poly_idx : orderOptimizer.polyOrder)
+    else
     {
-        addPolygon(polygons[poly_idx], orderOptimizer.polyStart[poly_idx], config, wall_overlap_computation, wall_0_wipe_dist, spiralize);
+        PathOrderOptimizer orderOptimizer(lastPosition, z_seam_pos, z_seam_type);
+        for (unsigned int poly_idx = 0; poly_idx < polygons.size(); poly_idx++)
+        {
+            orderOptimizer.addPolygon(polygons[poly_idx]);
+        }
+        orderOptimizer.optimize();
+        for (unsigned int poly_idx : orderOptimizer.polyOrder)
+        {
+            addPolygon(polygons[poly_idx], orderOptimizer.polyStart[poly_idx], config, wall_overlap_computation, wall_0_wipe_dist, spiralize);
+        }
     }
 }
 void GCodePlanner::addLinesByOptimizer(Polygons& polygons, GCodePathConfig* config, SpaceFillType space_fill_type, int wipe_dist)
