Ultimaker/CuraEngine

commit 9e7b1bedcbef00b9877df863f705213aa1565af4

refactor: fsml ==> fan_speed_layer_time_settings.cool_fan_speed_0 (CURA-3339)

src/LayerPlan.cpp
@@ -565,12 +565,12 @@ TimeMaterialEstimates ExtruderPlan::computeNaiveTimeEstimates()
 
 void ExtruderPlan::processFanSpeedAndMinimalLayerTime(bool force_minimal_layer_time)
 {
-    const FanSpeedLayerTimeSettings& fsml = fan_speed_layer_time_settings;
+    const FanSpeedLayerTimeSettings& fan_speed_layer_time_settings = fan_speed_layer_time_settings;
     TimeMaterialEstimates estimates = computeNaiveTimeEstimates();
     totalPrintTime = estimates.getTotalTime();
     if (force_minimal_layer_time)
     {
-        forceMinimalLayerTime(fsml.cool_min_layer_time, fsml.cool_min_speed, estimates.getTravelTime(), estimates.getExtrudeTime());
+        forceMinimalLayerTime(fan_speed_layer_time_settings.cool_min_layer_time, fan_speed_layer_time_settings.cool_min_speed, estimates.getTravelTime(), estimates.getExtrudeTime());
     }
 
     /*
@@ -588,19 +588,19 @@ void ExtruderPlan::processFanSpeedAndMinimalLayerTime(bool force_minimal_layer_t
 
     */
     // interpolate fan speed (for cool_fan_full_layer and for cool_min_layer_time_fan_speed_max)
-    fan_speed = fsml.cool_fan_speed_min;
+    fan_speed = fan_speed_layer_time_settings.cool_fan_speed_min;
     double totalLayerTime = estimates.unretracted_travel_time + estimates.extrude_time;
-    if (force_minimal_layer_time && totalLayerTime < fsml.cool_min_layer_time)
+    if (force_minimal_layer_time && totalLayerTime < fan_speed_layer_time_settings.cool_min_layer_time)
     {
-        fan_speed = fsml.cool_fan_speed_max;
+        fan_speed = fan_speed_layer_time_settings.cool_fan_speed_max;
     }
-    else if (force_minimal_layer_time && totalLayerTime < fsml.cool_min_layer_time_fan_speed_max)
+    else if (force_minimal_layer_time && totalLayerTime < fan_speed_layer_time_settings.cool_min_layer_time_fan_speed_max)
     { 
         // when forceMinimalLayerTime didn't change the extrusionSpeedFactor, we adjust the fan speed
-        double fan_speed_diff = fsml.cool_fan_speed_max - fsml.cool_fan_speed_min;
-        double layer_time_diff = fsml.cool_min_layer_time_fan_speed_max - fsml.cool_min_layer_time;
-        double fraction_of_slope = (totalLayerTime - fsml.cool_min_layer_time) / layer_time_diff;
-        fan_speed = fsml.cool_fan_speed_max - fan_speed_diff * fraction_of_slope;
+        double fan_speed_diff = fan_speed_layer_time_settings.cool_fan_speed_max - fan_speed_layer_time_settings.cool_fan_speed_min;
+        double layer_time_diff = fan_speed_layer_time_settings.cool_min_layer_time_fan_speed_max - fan_speed_layer_time_settings.cool_min_layer_time;
+        double fraction_of_slope = (totalLayerTime - fan_speed_layer_time_settings.cool_min_layer_time) / layer_time_diff;
+        fan_speed = fan_speed_layer_time_settings.cool_fan_speed_max - fan_speed_diff * fraction_of_slope;
     }
     /*
     Supposing no influence of minimal layer time;
@@ -618,10 +618,10 @@ void ExtruderPlan::processFanSpeedAndMinimalLayerTime(bool force_minimal_layer_t
                      layer nr >
 
     */
-    if (layer_nr < fsml.cool_fan_full_layer)
+    if (layer_nr < fan_speed_layer_time_settings.cool_fan_full_layer)
     {
         //Slow down the fan on the layers below the [cool_fan_full_layer], where layer 0 is speed 0.
-        fan_speed = fsml.cool_fan_speed_0 + (fan_speed - fsml.cool_fan_speed_0) * std::max(0, layer_nr) / fsml.cool_fan_full_layer;
+        fan_speed = fan_speed_layer_time_settings.cool_fan_speed_0 + (fan_speed - fan_speed_layer_time_settings.cool_fan_speed_0) * std::max(0, layer_nr) / fan_speed_layer_time_settings.cool_fan_full_layer;
     }
 }
 

src/FffGcodeWriter.cpp
@@ -322,11 +322,11 @@ void FffGcodeWriter::processRaft(const SliceDataStorage& storage, unsigned int t
         int64_t comb_offset = train->getSettingInMicrons("raft_base_line_spacing");
 
         std::vector<FanSpeedLayerTimeSettings> fan_speed_layer_time_settings_per_extruder_raft_base = fan_speed_layer_time_settings_per_extruder; // copy so that we change only the local copy
-        for (FanSpeedLayerTimeSettings& fsml : fan_speed_layer_time_settings_per_extruder_raft_base)
+        for (FanSpeedLayerTimeSettings& fan_speed_layer_time_settings : fan_speed_layer_time_settings_per_extruder_raft_base)
         {
             double regular_fan_speed = train->getSettingInPercentage("raft_base_fan_speed");
-            fsml.cool_fan_speed_min = regular_fan_speed;
-            fsml.cool_fan_speed_0 = regular_fan_speed; // ignore initial layer fan speed stuff
+            fan_speed_layer_time_settings.cool_fan_speed_min = regular_fan_speed;
+            fan_speed_layer_time_settings.cool_fan_speed_0 = regular_fan_speed; // ignore initial layer fan speed stuff
         }
 
         LayerPlan& gcode_layer = *new LayerPlan(storage, layer_nr, z, layer_height, extruder_nr, fan_speed_layer_time_settings_per_extruder_raft_base, combing_mode, comb_offset, train->getSettingBoolean("travel_avoid_other_parts"), train->getSettingInMicrons("travel_avoid_distance"));
@@ -364,11 +364,11 @@ void FffGcodeWriter::processRaft(const SliceDataStorage& storage, unsigned int t
         int64_t comb_offset = train->getSettingInMicrons("raft_interface_line_spacing");
 
         std::vector<FanSpeedLayerTimeSettings> fan_speed_layer_time_settings_per_extruder_raft_interface = fan_speed_layer_time_settings_per_extruder; // copy so that we change only the local copy
-        for (FanSpeedLayerTimeSettings& fsml : fan_speed_layer_time_settings_per_extruder_raft_interface)
+        for (FanSpeedLayerTimeSettings& fan_speed_layer_time_settings : fan_speed_layer_time_settings_per_extruder_raft_interface)
         {
             double regular_fan_speed = train->getSettingInPercentage("raft_interface_fan_speed");
-            fsml.cool_fan_speed_min = regular_fan_speed;
-            fsml.cool_fan_speed_0 = regular_fan_speed; // ignore initial layer fan speed stuff
+            fan_speed_layer_time_settings.cool_fan_speed_min = regular_fan_speed;
+            fan_speed_layer_time_settings.cool_fan_speed_0 = regular_fan_speed; // ignore initial layer fan speed stuff
         }
 
         LayerPlan& gcode_layer = *new LayerPlan(storage, layer_nr, z, layer_height, extruder_nr, fan_speed_layer_time_settings_per_extruder_raft_interface, combing_mode, comb_offset, train->getSettingBoolean("travel_avoid_other_parts"), train->getSettingInMicrons("travel_avoid_distance"));
@@ -406,11 +406,11 @@ void FffGcodeWriter::processRaft(const SliceDataStorage& storage, unsigned int t
         const int64_t comb_offset = train->getSettingInMicrons("raft_surface_line_spacing");
 
         std::vector<FanSpeedLayerTimeSettings> fan_speed_layer_time_settings_per_extruder_raft_surface = fan_speed_layer_time_settings_per_extruder; // copy so that we change only the local copy
-        for (FanSpeedLayerTimeSettings& fsml : fan_speed_layer_time_settings_per_extruder_raft_surface)
+        for (FanSpeedLayerTimeSettings& fan_speed_layer_time_settings : fan_speed_layer_time_settings_per_extruder_raft_surface)
         {
             double regular_fan_speed = train->getSettingInPercentage("raft_surface_fan_speed");
-            fsml.cool_fan_speed_min = regular_fan_speed;
-            fsml.cool_fan_speed_0 = regular_fan_speed; // ignore initial layer fan speed stuff
+            fan_speed_layer_time_settings.cool_fan_speed_min = regular_fan_speed;
+            fan_speed_layer_time_settings.cool_fan_speed_0 = regular_fan_speed; // ignore initial layer fan speed stuff
         }
 
         LayerPlan& gcode_layer = *new LayerPlan(storage, layer_nr, z, layer_height, extruder_nr, fan_speed_layer_time_settings_per_extruder_raft_surface, combing_mode, comb_offset, train->getSettingBoolean("travel_avoid_other_parts"), train->getSettingInMicrons("travel_avoid_distance"));

commit 0eb6e46e391f9d780a6eb05b9d45f284ef6a5854

cleanup: bs regression from 9e7b1bedcbef00b9877df863f705213aa1565af4 (CURA-3339)

src/LayerPlan.cpp
@@ -563,7 +563,6 @@ TimeMaterialEstimates ExtruderPlan::computeNaiveTimeEstimates(Point starting_pos
 
 void ExtruderPlan::processFanSpeedAndMinimalLayerTime(bool force_minimal_layer_time, Point starting_position)
 {
-    const FanSpeedLayerTimeSettings& fan_speed_layer_time_settings = fan_speed_layer_time_settings;
     TimeMaterialEstimates estimates = computeNaiveTimeEstimates(starting_position);
     totalPrintTime = estimates.getTotalTime();
     if (force_minimal_layer_time)