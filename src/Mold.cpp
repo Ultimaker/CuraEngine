74c36697af1a331a96985b77fe4a448eab7aca55
Tim Kuipers <t.kuipers@ultimaker.com>
2017-03-13 17:53:53 +0100

    fix: mold didn't apply mold_width offset (CURA-3512)

--- src/Mold.cpp
+++ src/Mold.cpp
@@ -13,15 +13,15 @@ void Mold::process(Slicer& slicer, coord_t layer_height, double angle, coord_t w
     for (int layer_nr = slicer.layers.size() - 1; layer_nr >= 0; layer_nr--)
     {
         SlicerLayer& layer = slicer.layers[layer_nr];
-        Polygons layer_outlines = layer.polygons.unionPolygons(layer.openPolylines.offsetPolyLine(open_polyline_width / 2));
+        Polygons model_outlines = layer.polygons.unionPolygons(layer.openPolylines.offsetPolyLine(open_polyline_width / 2));
         if (angle >= 90)
         {
-            layer.polygons = layer_outlines.offset(width).difference(layer_outlines);
+            layer.polygons = model_outlines.offset(width).difference(model_outlines);
         }
         else
         {
-            mold_outline_above = mold_outline_above.offset(-inset).unionPolygons(layer_outlines);
-            layer.polygons = mold_outline_above.difference(layer_outlines);
+            mold_outline_above = mold_outline_above.offset(-inset).unionPolygons(model_outlines.offset(width));
+            layer.polygons = mold_outline_above.difference(model_outlines);
         }
     }
 
