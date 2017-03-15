7d0613ededfd24a10158505c79ee7ee58e0ecc1a
Tim Kuipers <t.kuipers@ultimaker.com>
2017-03-15 16:17:28 +0100

    doc (CURA-3512)

--- src/Mold.h
+++ src/Mold.h
@@ -14,6 +14,18 @@ namespace cura
 class Mold
 {
 public:
+    /*!
+     * Convert the polygons (and open polylines) in \p slicer
+     * in order to make a mold with a cutout in the shape of the outlines.
+     * 
+     * The open polylines of the model are also used to cut out the shape inside the mold.
+     * 
+     * \param slicer The container for the sliced polygons (and open polylines)
+     * \param layer_height The overall layer height used (used to compute an offset from the \p angle)
+     * \param angle The overhang angle for the outer outlines of the mold
+     * \param width The minmal width of the mold; the minmal distance between the outside and inside of the mold in horizontal direction
+     * \param open_polyline_width The width with which the open polylines are cut out of the mold
+     */
     static void process(Slicer& slicer, coord_t layer_height, double angle, coord_t width, coord_t open_polyline_width);
 private:
 };
