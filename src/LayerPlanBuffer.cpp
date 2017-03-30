8be21f6ec8809f3a88a1a2838329de3d171cbd8f
Tim Kuipers <t.kuipers@ultimaker.com>
2017-03-30 15:32:59 +0200

    fix: initial print temp every other layer (CURA-3537)
    I assumed the previous layer had a warm up command from initial to extrusion temperature, but that doesn't hold in the single extrusion context.

--- src/LayerPlanBuffer.cpp
+++ src/LayerPlanBuffer.cpp
@@ -486,10 +486,10 @@ void LayerPlanBuffer::insertTempCommands()
 
         double print_temp = preheat_config.getTemp(extruder, avg_flow, extruder_plan.is_initial_layer);
         double initial_print_temp = preheat_config.getInitialPrintTemp(extruder);
-        if (initial_print_temp == 0.0 // user doesn't wnat to use initial print temp feature
+        if (initial_print_temp == 0.0 // user doesn't want to use initial print temp feature
             || !extruder_used_in_meshgroup[extruder] // prime poop uses print temp rather than initial print temp
             || (overall_extruder_plan_idx > 0 && extruder_plans[overall_extruder_plan_idx - 1]->extruder == extruder  // prev plan has same extruder ..
-                && extruder_plans[overall_extruder_plan_idx - 1]->extrusion_temperature) // and prev extruder plan already heated to printing temperature
+                && extruder_plans[overall_extruder_plan_idx - 1]->estimates.getTotalUnretractedTime() > 0.0) // and prev extruder plan already heated to printing temperature
         )
         {
             extruder_plan.required_start_temperature = print_temp;
