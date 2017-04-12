0d25cd18be51027b9cb2f46173b92eef44438275
Jack Ha <jackha@gmail.com>
2017-04-12 16:59:28 +0200

    Writing tests AABB.

--- tests/utils/AABBTest.cpp
+++ tests/utils/AABBTest.cpp
@@ -9,6 +9,19 @@ namespace cura
 
 void AABBTest::setUp()
 {
+    test_square.emplace_back(0, 0);
+    test_square.emplace_back(100, 0);
+    test_square.emplace_back(100, 100);
+    test_square.emplace_back(0, 100);
+
+    test_triangle.emplace_back(0, 0);
+    test_triangle.emplace_back(-50, 40);
+    test_triangle.emplace_back(60, 60);
+
+    Point point_min = Point(-10, -12);
+    Point point_max = Point(11, 13);
+    my_aabb = AABB(point_min, point_max);
+
 }
 
 void AABBTest::tearDown()
@@ -19,7 +32,56 @@ void AABBTest::tearDown()
 
 void AABBTest::smokeTest()
 {
-    CPPUNIT_ASSERT_MESSAGE("Boom!", 1 < 5);
+    Point point_min = Point(-10, -12);
+    Point point_max = Point(11, 13);
+    AABB aabb(point_min, point_max);
+}
+
+void AABBTest::smokeTest2()
+{
+    Polygons polys = Polygons();
+    polys.add(test_square);
+    AABB aabb(polys);
+}
+
+void AABBTest::smokeTest3()
+{
+    Polygons test_polys;
+    PolygonRef poly = test_polys.newPoly();
+    poly.add(Point(82124,98235));
+    poly.add(Point(83179,98691));
+    poly.add(Point(83434,98950));
+    poly.add(Point(82751,99026));
+    poly.add(Point(82528,99019));
+    ConstPolygonRef cpoly = ConstPolygonRef(poly);
+    AABB aabb(cpoly);
+}
+
+void AABBTest::calculateTest()
+{
+    Polygons polys = Polygons();
+    polys.add(test_triangle);
+    my_aabb.calculate(polys);
+}
+
+void AABBTest::getMiddleTest()
+{
+
+}
+
+void AABBTest::hitTest()
+{
+
+}
+
+void AABBTest::includeTest()
+{
+
+}
+
+void AABBTest::expandTest()
+{
+
 }
 
 
--- tests/utils/AABBTest.h
+++ tests/utils/AABBTest.h
@@ -6,6 +6,10 @@
 
 #include <cppunit/TestFixture.h>
 #include <cppunit/extensions/HelperMacros.h>
+#include <../src/utils/intpoint.h>
+#include <../src/utils/AABB.h>
+#include <../src/utils/polygon.h>
+#include <clipper/clipper.hpp>
 
 namespace cura
 {
@@ -14,6 +18,13 @@ class AABBTest : public CppUnit::TestFixture
 {
     CPPUNIT_TEST_SUITE(AABBTest);
     CPPUNIT_TEST(smokeTest);
+    CPPUNIT_TEST(smokeTest2);
+    CPPUNIT_TEST(smokeTest3);
+    CPPUNIT_TEST(calculateTest);
+    CPPUNIT_TEST(getMiddleTest);
+    CPPUNIT_TEST(hitTest);
+    CPPUNIT_TEST(includeTest);
+    CPPUNIT_TEST(expandTest);
     CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -22,9 +33,19 @@ public:
     void tearDown();
 
     void smokeTest();
+    void smokeTest2();
+    void smokeTest3();
+
+    void calculateTest();
+    void getMiddleTest();
+    void hitTest();
+    void includeTest();
+    void expandTest();
 
 private:
-
+    Polygon test_square;
+    Polygon test_triangle;
+    AABB my_aabb;
 };
 
 }
