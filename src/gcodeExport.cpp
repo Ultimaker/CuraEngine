2ea20c6763e1d0b1eeac44b25fc01fedd1545cbd
Mark Burton <markb@smartavionics.com>
2017-08-30 18:41:39 +0100

    Don't output G92 E0 (or similar) when using relative extrusion mode.

--- src/gcodeExport.cpp
+++ src/gcodeExport.cpp
@@ -499,7 +499,10 @@ void GCodeExport::resetExtrusionValue()
 {
     if (flavor != EGCodeFlavor::MAKERBOT && flavor != EGCodeFlavor::BFB)
     {
-        *output_stream << "G92 " << extruder_attr[current_extruder].extruderCharacter << "0" << new_line;
+        if (!relative_extrusion)
+        {
+            *output_stream << "G92 " << extruder_attr[current_extruder].extruderCharacter << "0" << new_line;
+        }
         double current_extruded_volume = getCurrentExtrudedVolume();
         extruder_attr[current_extruder].totalFilament += current_extruded_volume;
         for (double& extruded_volume_at_retraction : extruder_attr[current_extruder].extruded_volume_at_previous_n_retractions)
