f754d1dd3cfb83a578787fc51b892adb9a89fa70
Mark Burton <markb@smartavionics.com>
2017-09-18 08:59:41 +0100

    Small refactor, pass infill_origin into infill generating methods.

--- src/infill/SpaghettiInfillPathGenerator.h
+++ src/infill/SpaghettiInfillPathGenerator.h
@@ -47,9 +47,10 @@ public:
      * \param infill_line_distance The distance between the infill lines
      * \param infill_overlap The distance by which the infill overlaps with the wall insets.
      * \param fillAngle The angle in the XY plane at which the infill is generated.
+     * \param infill_origin The origin of the infill pattern.
      * \return Whether this function added anything to the layer plan
      */
-    static bool processSpaghettiInfill(const SliceDataStorage& storage, const FffGcodeWriter& fff_gcode_writer, LayerPlan& gcode_layer, const SliceMeshStorage& mesh, const int extruder_nr, const PathConfigStorage::MeshPathConfigs& mesh_config, const SliceLayerPart& part, int infill_line_distance, int infill_overlap, int fillAngle);
+    static bool processSpaghettiInfill(const SliceDataStorage& storage, const FffGcodeWriter& fff_gcode_writer, LayerPlan& gcode_layer, const SliceMeshStorage& mesh, const int extruder_nr, const PathConfigStorage::MeshPathConfigs& mesh_config, const SliceLayerPart& part, int infill_line_distance, int infill_overlap, int fillAngle, const Point& infill_origin);
 
 };
 
