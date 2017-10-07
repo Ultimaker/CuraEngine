2463b1a8701ec0b24d771abecd5da2fe63a2b6e4
Tim Kuipers <t.kuipers@ultimaker.com>
2017-10-07 11:29:20 +0200

    variable cross infill debugging

--- tests/infill/SierpinskiFillTest.cpp
+++ tests/infill/SierpinskiFillTest.cpp
@@ -19,18 +19,17 @@ void SierpinskiFillTest::tearDown()
 
 void SierpinskiFillTest::debugCheck()
 {
-    AABB aabb(Point(0,0), Point (1024, 1024));
+    AABB aabb(Point(0,0), Point(1024, 1024)*8);
     //aabb.expand(512);
-    SVG svg("output/sierpinski.html", aabb);
+    SVG svg("output/sierpinski.html", aabb, Point(1000, 1000)*2);
     
     
 //     srand(1);
-//     for (int i = 20; i < 21; i++)
-    int i = 18;
+    int max_depth = 18;
     {
-        SierpinskiFill f(aabb, i);
+        SierpinskiFill f(aabb, max_depth);
         SVG::Color color = SVG::Color::GREEN;
-        switch (i % 4)
+        switch (max_depth % 4)
         {
             default:
             case 0: color = SVG::Color::GREEN; break;
@@ -38,8 +37,9 @@ void SierpinskiFillTest::debugCheck()
             case 2: color = SVG::Color::BLUE; break;
             case 3: color = SVG::Color::YELLOW; break;
         }
-        color = SVG::Color::GREEN;
-        svg.writePolygon(f.generateCross(), color);
+        color = SVG::Color::RAINBOW;
+        color = SVG::Color::BLACK;
+        svg.writePolygon(f.generateCross(5200, 16), color, 4);
 //         svg.writePolygon(f.generateSierpinski(), color);
 //         f.debugOutput(svg);
     }
