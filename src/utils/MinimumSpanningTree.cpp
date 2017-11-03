ca4bc339309f42644cf462b5ea3f48eb95d9895a
Ghostkeeper <rubend@tutanota.com>
2017-11-03 11:09:04 +0100

    Add licence header
    Contributes to issue CURA-4523.

--- src/utils/MinimumSpanningTree.cpp
+++ src/utils/MinimumSpanningTree.cpp
@@ -1,3 +1,6 @@
+//Copyright (c) 2017 Ultimaker B.V.
+//CuraEngine is released under the terms of the AGPLv3 or higher.
+
 #include "MinimumSpanningTree.h"
 
 namespace cura
--- src/utils/MinimumSpanningTree.h
+++ src/utils/MinimumSpanningTree.h
@@ -1,3 +1,6 @@
+//Copyright (c) 2017 Ultimaker B.V.
+//CuraEngine is released under the terms of the AGPLv3 or higher.
+
 #ifndef MINIMUMSPANNINGTREE_H
 #define MINIMUMSPANNINGTREE_H
 
