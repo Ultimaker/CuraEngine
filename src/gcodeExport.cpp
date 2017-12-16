05176edb0dbc7e2307f3400cc4ff82f3e9b5299f
Chris Thierauf <chris@cthierauf.com>
2017-12-16 16:33:49 -0500

    Made gcode comment style consistent

--- src/gcodeExport.cpp
+++ src/gcodeExport.cpp
@@ -486,11 +486,11 @@ void GCodeExport::writeExtrusionMode(bool set_relative_extrusion_mode)
 {
     if (set_relative_extrusion_mode)
     {
-        *output_stream << "M83 ; relative extrusion mode" << new_line;
+        *output_stream << "M83 ;relative extrusion mode" << new_line;
     }
     else
     {
-        *output_stream << "M82 ; absolute extrusion mode" << new_line;
+        *output_stream << "M82 ;absolute extrusion mode" << new_line;
     }
 }
 
