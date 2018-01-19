6adfa58925fd55e0cb8d05634249550355b60821
Ghostkeeper <rubend@tutanota.com>
2018-01-19 17:25:49 +0100

    Don't insert temperature command if there is a zero-length move in an extruder plan
    If there are only zero-length moves in an extruder plan, it's not properly detected that the extruder plan is empty. To properly detect that, you'd need to compute the total time estimate and see if that is zero. This takes computing power though, but luckily we already have the time estimate here just in time to do something about it.
    
    Contributes to issue CURA-4814.

--- src/LayerPlanBuffer.cpp
+++ src/LayerPlanBuffer.cpp
@@ -379,7 +379,11 @@ void LayerPlanBuffer::insertFinalPrintTempCommand(std::vector<ExtruderPlan*>& ex
                 initial_print_temp = prev_extruder_plan.required_start_temperature;
             }
         }
-        assert(time_window != 0.0 && "ExtruderPlans have been added while they weren't needed! Current code cannot handle this situation. SliceDataStorage::getExtrudersUsed should probably be updated!");
+        if (time_window <= 0.0) //There was a move in this plan but it was length 0.
+        {
+            logWarning("Unneccesary extruder switch detected! SliceDataStorage::getExtrudersUsed should probably be updated.\n");
+            return;
+        }
         weighted_average_extrusion_temp /= time_window;
         time_window -= heated_pre_travel_time + heated_post_travel_time;
         assert(heated_pre_travel_time != -1 && "heated_pre_travel_time must have been computed; there must have been an extruder plan!");
