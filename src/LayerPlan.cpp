1ab09b494541b8e9ce4534d48ce589fb2943246e
Mark Burton <markb@smartavionics.com>
2018-02-15 10:49:18 +0000

    Implement bridge_wall_coast - controls coasting distance that precedes a bridge wall.

--- src/LayerPlan.cpp
+++ src/LayerPlan.cpp
@@ -459,7 +459,7 @@ void LayerPlan::addPolygonsByOptimizer(const Polygons& polygons, const GCodePath
     }
 }
 
-void LayerPlan::addWallLine(const Point& p0, const Point& p1, const GCodePathConfig& non_bridge_config, const GCodePathConfig& bridge_config, float flow)
+void LayerPlan::addWallLine(const Point& p0, const Point& p1, const GCodePathConfig& non_bridge_config, const GCodePathConfig& bridge_config, float flow, float& non_bridge_length_so_far)
 {
     if (air_below_part.empty())
     {
@@ -511,16 +511,37 @@ void LayerPlan::addWallLine(const Point& p0, const Point& p1, const GCodePathCon
                     b1 = bridge[0];
                 }
 
+                float len2 = vSize2f(cur_point - b0);
                 // extrude using non_bridge_config to the start of the next bridge segment
-                if (vSize2f(cur_point - b0) > min_line_len2)
+                if (len2 > min_line_len2)
                 {
-                    addExtrusionMove(b0, non_bridge_config, SpaceFillType::Polygons, flow);
+                    const SettingsBaseVirtual* extr = getLastPlannedExtruderTrainSettings();
+                    float coast_dist = std::min(non_bridge_length_so_far, 10000.0f) * non_bridge_config.getSpeed() * extr->getSettingInPercentage("bridge_wall_coast") / 5000;
+                    if (coast_dist > 0)
+                    {
+                        if (len2 < coast_dist * coast_dist)
+                        {
+                            coast_dist = std::sqrt(len2);
+                        }
+                        Point coast_from = b0 + coast_dist * (cur_point - b0) / std::sqrt(len2);
+                        if (vSize2f(cur_point - coast_from) > min_line_len2)
+                        {
+                            addExtrusionMove(coast_from, non_bridge_config, SpaceFillType::Polygons, flow);
+                        }
+                        addExtrusionMove(b0, non_bridge_config, SpaceFillType::Polygons, 0);
+                    }
+                    else
+                    {
+                        addExtrusionMove(b0, non_bridge_config, SpaceFillType::Polygons, flow);
+                    }
+                    non_bridge_length_so_far += len2 * flow;
                     cur_point = b0;
                 }
                 // extrude using bridge_config the bridge segment
                 if (vSize2f(b1 - b0) > min_line_len2)
                 {
                     addExtrusionMove(b1, bridge_config, SpaceFillType::Polygons, flow);
+                    non_bridge_length_so_far = 0;
                     cur_point = b1;
                 }
 
@@ -532,17 +553,24 @@ void LayerPlan::addWallLine(const Point& p0, const Point& p1, const GCodePathCon
             if (vSize2f(cur_point - p1) > min_line_len2)
             {
                 addExtrusionMove(p1, non_bridge_config, SpaceFillType::Polygons, flow);
+                non_bridge_length_so_far += vSize(cur_point - p1) * flow;
             }
         }
         else if (air_below_part.inside(p0, true))
         {
             // both p0 and p1 must be above air (the result will be ugly!)
             addExtrusionMove(p1, bridge_config, SpaceFillType::Polygons, flow);
+            non_bridge_length_so_far = 0;
         }
         else
         {
             // no part of the line is above air
             addExtrusionMove(p1, non_bridge_config, SpaceFillType::Polygons, flow);
+            non_bridge_length_so_far += vSize(p1 - p0) * flow;
+
+            // FIXME? - a line following this one could be a bridge and so, potentially, part of this line we have just output could
+            // need to be coasted like we do above but that's not being handled here - it would be a lot of effort to implement that
+            // and probably not worth the effort
         }
     }
 }
@@ -563,18 +591,19 @@ void LayerPlan::addWall(ConstPolygonRef wall, int start_idx, const GCodePathConf
 
     Point p0 = wall[start_idx];
     addTravel(p0, always_retract);
+    float non_bridge_length_so_far = 0;
     for (unsigned int point_idx = 1; point_idx < wall.size(); point_idx++)
     {
         const Point& p1 = wall[(start_idx + point_idx) % wall.size()];
         const float flow = (wall_overlap_computation)? flow_ratio * wall_overlap_computation->getFlow(p0, p1) : flow_ratio;
-        addWallLine(p0, p1, non_bridge_config, bridge_config, flow);
+        addWallLine(p0, p1, non_bridge_config, bridge_config, flow, non_bridge_length_so_far);
         p0 = p1;
     }
     if (wall.size() > 2)
     {
         const Point& p1 = wall[start_idx];
         const float flow = (wall_overlap_computation)? flow_ratio * wall_overlap_computation->getFlow(p0, p1) : flow_ratio;
-        addWallLine(p0, p1, non_bridge_config, bridge_config, flow);
+        addWallLine(p0, p1, non_bridge_config, bridge_config, flow, non_bridge_length_so_far);
 
         if (wall_0_wipe_dist > 0)
         { // apply outer wall wipe
