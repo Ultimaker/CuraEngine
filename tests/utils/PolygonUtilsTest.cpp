7f4aae948c1af0e1dbaf06d850f585cbcf4efd4f
Tim Kuipers <t.kuipers@ultimaker.com>
2018-05-17 15:10:40 +0200

    lil: remove unused param

--- tests/utils/PolygonUtilsTest.cpp
+++ tests/utils/PolygonUtilsTest.cpp
@@ -513,7 +513,7 @@ void PolygonUtilsTest::getNextParallelIntersectionAssert(std::optional<Point> pr
 
     constexpr bool draw_problem_scenario = false; // make this true if you are debugging the function getNextParallelIntersection(.)
 
-    auto draw = [this, predicted, start, line_to, forward, dist, computed]()
+    auto draw = [this, predicted, start, line_to, dist, computed]()
         {
             if (!draw_problem_scenario)
             {
