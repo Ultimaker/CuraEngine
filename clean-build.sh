9fc2f85d60860fd464ebeb38befd123ec7716561
Ricky Zhang <rickyzhang@gmail.com>
2018-05-18 05:30:55 -0400

    Add production parm
    Signed-off-by: Ricky Zhang <rickyzhang@gmail.com>

--- clean-build.sh
+++ clean-build.sh
@@ -11,6 +11,10 @@ if [ "$1" == "debug" ];
     then
         cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_G3LOG=ON ..
         make -j 8
+elif [ "$1" == "production" ]; 
+    then
+        cmake -DCMAKE_BUILD_TYPE=Release ..
+        make VERBOSE=1 -j 8
 else
         cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_G3LOG=ON ..
         make VERBOSE=1 -j 8
