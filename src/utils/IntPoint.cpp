911fdbb0a727902390a17ea4ad3e166a4474c1df
Ghostkeeper <rubend@tutanota.com>
2018-05-29 16:31:10 +0200

    Add functions for multiplying and dividing in-place
    Not all of them, because I want to do another refactor now. Hold on.
    
    Contributes to issue CURA-5037.

---
+++ src/utils/IntPoint.cpp
@@ -0,0 +1,25 @@
+//Copyright (c) 2018 Ultimaker B.V.
+//CuraEngine is released under the terms of the AGPLv3 or higher.
+
+#include "IntPoint.h" //The headers we're implementing.
+
+namespace cura
+{
+
+Point3& Point3::operator *=(const Point3& p)
+{
+    x *= p.x;
+    y *= p.y;
+    z *= p.z;
+    return *this;
+}
+
+Point3& Point3::operator /=(const Point3& p)
+{
+    x /= p.x;
+    y /= p.y;
+    z /= p.z;
+    return *this;
+}
+
+}
--- src/utils/IntPoint.h
+++ src/utils/IntPoint.h
@@ -59,8 +59,10 @@ public:
     Point3 operator*(const int32_t i) const { return Point3(x*i, y*i, z*i); }
     Point3 operator*(const double d) const { return Point3(d*x, d*y, d*z); }
 
-    Point3& operator += (const Point3& p) { x += p.x; y += p.y; z += p.z; return *this; }
-    Point3& operator -= (const Point3& p) { x -= p.x; y -= p.y; z -= p.z; return *this; }
+    Point3& operator +=(const Point3& p) { x += p.x; y += p.y; z += p.z; return *this; }
+    Point3& operator -=(const Point3& p) { x -= p.x; y -= p.y; z -= p.z; return *this; }
+    Point3& operator *=(const Point3& p);
+    Point3& operator /=(const Point3& p);
 
     bool operator==(const Point3& p) const { return x==p.x&&y==p.y&&z==p.z; }
     bool operator!=(const Point3& p) const { return x!=p.x||y!=p.y||z!=p.z; }
