c39c6e2ff3cb1e76fd933406d2938132293f9a18
Tim Kuipers <t.kuipers@ultimaker.com>
2018-06-13 16:40:54 +0200

    Revert "remove SierpinskiFillTest"
    This reverts commit 86acdbb3f536fbfbe3bf36a2c7fc73f26164418c.

---
+++ tests/infill/SierpinskiFillTest.cpp
@@ -0,0 +1,84 @@
+//Copyright (c) 2017 Ultimaker B.V.
+//CuraEngine is released under the terms of the AGPLv3 or higher.
+
+#include "SierpinskiFillTest.h"
+#include "../src/infill/ImageBasedDensityProvider.h"
+
+namespace cura
+{
+    CPPUNIT_TEST_SUITE_REGISTRATION(SierpinskiFillTest);
+
+void SierpinskiFillTest::setUp()
+{
+    // no dothing
+}
+
+void SierpinskiFillTest::tearDown()
+{
+    // no dothing
+}
+
+void SierpinskiFillTest::debugCheck()
+{
+    coord_t line_width = 400;
+    AABB aabb(Point(0,0), Point(line_width, line_width)*512);
+    //aabb.expand(512);
+    Point canvas_size = Point(1000, 1000) * 2;
+    SVG svg("output/sierpinski.svg", aabb, canvas_size);
+    
+    
+    int drawing_line_width = line_width * canvas_size.X / aabb.max.X;
+    
+//     DensityProvider* subdivider = new ImageBasedDensityProvider("/home/t.kuipers/Documents/PhD/Cross Fractal/lena.png", aabb);
+//     DensityProvider* subdivider = new ImageBasedDensityProvider("/home/t.kuipers/Documents/PhD/Cross Fractal/gradient.png", aabb);
+//     DensityProvider* subdivider = new ImageBasedDensityProvider("/home/t.kuipers/Documents/PhD/Cross Fractal/slight_gradient.png", aabb);
+//     DensityProvider* subdivider = new ImageBasedDensityProvider("/home/t.kuipers/Documents/PhD/Cross Fractal/simple.png", aabb);
+//     DensityProvider* subdivider = new ImageBasedDensityProvider("/home/t.kuipers/Documents/PhD/Cross Fractal/gray.png", aabb);
+//     DensityProvider* subdivider = new ImageBasedDensityProvider("/home/t.kuipers/Documents/PhD/Cross Fractal/triangle.png", aabb);
+    
+    DensityProvider* subdivider = new ImageBasedDensityProvider("/home/t.kuipers/Documents/PhD/Cross Fractal/hitler.jpg", aabb);
+//     DensityProvider* subdivider = new ImageBasedDensityProvider("/home/t.kuipers/Documents/PhD/Cross Fractal/cheshire_cat.jpg", aabb);
+//     DensityProvider* subdivider = new ImageBasedDensityProvider("/home/t.kuipers/Documents/PhD/Cross Fractal/vader.jpg", aabb);
+//     DensityProvider* subdivider = new ImageBasedDensityProvider("/home/t.kuipers/Documents/PhD/Cross Fractal/sinterklaas.jpg", aabb);
+//     DensityProvider* subdivider = new ImageBasedDensityProvider("/home/t.kuipers/Documents/PhD/Cross Fractal/diamond.jpg", aabb);
+//     DensityProvider* subdivider = new ImageBasedDensityProvider("/home/t.kuipers/Documents/PhD/Cross Fractal/trex.jpeg", aabb);
+//     DensityProvider* subdivider = new ImageBasedDensityProvider("/home/t.kuipers/Documents/PhD/Cross Fractal/soulpilot.jpg", aabb);
+//     DensityProvider* subdivider = new ImageBasedDensityProvider("/home/t.kuipers/Documents/PhD/Cross Fractal/soulpilot_dark.jpg", aabb);
+//     DensityProvider* subdivider = new ImageBasedDensityProvider("/home/t.kuipers/Documents/PhD/Cross Fractal/cura-logo.jpg", aabb);
+//     subdivider = new UniformSubdivider();
+//     srand(1);
+    int max_depth = 1;
+    for (coord_t size = line_width; size < aabb.max.X; size = size << 1)
+    {
+        max_depth += 2;
+    }
+    bool dithering = true;
+    {
+        SierpinskiFill f(*subdivider, aabb, max_depth, line_width, dithering);
+        SVG::Color color = SVG::Color::GREEN;
+        switch (max_depth % 4)
+        {
+            default:
+            case 0: color = SVG::Color::GREEN; break;
+            case 1: color = SVG::Color::RED; break;
+            case 2: color = SVG::Color::BLUE; break;
+            case 3: color = SVG::Color::YELLOW; break;
+        }
+        color = SVG::Color::RAINBOW;
+        color = SVG::Color::BLACK;
+//         svg.writePolygon(f.generateCross(1200, 16), color, 4);
+//         svg.writePolygon(f.generateCross(), color, drawing_line_width);
+        svg.writeAreas(f.generateCross(), SVG::Color::WHITE, color, drawing_line_width);
+//         svg.writePoints(f.generateCross());
+//         svg.writePolygon(f.generateSierpinski(), color);
+//         f.debugOutput(svg);
+    }
+    
+}
+
+void SierpinskiFillTest::boundsCheck()
+{
+    
+}
+
+}
