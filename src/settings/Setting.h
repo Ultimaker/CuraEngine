e68a75145d5b91e6591f0eaef132fa59d0f8beae
Ghostkeeper <rubend@tutanota.com>
2018-07-19 14:41:35 +0200

    Add Setting struct
    It might as well be an std::pair<string, ExtruderTrain*> but this is more semantic.
    
    Contributes to issue CURA-4410.

---
+++ src/settings/Setting.h
@@ -0,0 +1,33 @@
+//Copyright (c) 2018 Ultimaker B.V.
+//CuraEngine is released under the terms of the AGPLv3 or higher.
+
+#ifndef SETTING_H
+#define SETTING_H
+
+namespace cura
+{
+
+class ExtruderTrain; //Forward declaration to prevent having to include ExtruderTrain.h. Saves compilation time.
+
+/*!
+ * \brief Represents a single setting that is set to some value.
+ */
+struct Setting
+{
+    /*!
+     * The value of the setting in it serialised form.
+     * \todo Store this as a void pointer in its pre-parsed form so that getting
+     * a setting doesn't need to parse it every time.
+     */
+    const std::string value;
+
+    /*!
+     * Unless overridden by a more specific setting container, limit the setting
+     * to be used by a certain extruder.
+     */
+    ExtruderTrain* limit_to_extruder = nullptr;
+};
+
+} //namespace cura
+
+#endif //SETTING_H
