f501213a75d810bf9ab467278f12fb62dc30a1fc
Ghostkeeper <rubend@tutanota.com>
2018-07-25 09:47:51 +0200

    Add alternative constructor with new Settings object
    The ExtruderTrain object still inherits from SettingsBaseVirtual, the legacy settings object, because otherwise it wouldn't compile for a lot of things. But now it also has the new Settings object.
    
    Contributes to issue CURA-4410.

--- src/ExtruderTrain.h
+++ src/ExtruderTrain.h
@@ -11,12 +11,30 @@ namespace cura
     
 class ExtruderTrain : public SettingsBase
 {
-    int extruder_nr;
 public:
-    int getExtruderNr();
+    size_t getExtruderNr();
 
-    ExtruderTrain(SettingsBaseVirtual* settings, int extruder_nr);
+    ExtruderTrain(SettingsBaseVirtual* settings, const size_t extruder_nr);
 
+    /*
+     * Creates a new extruder. This extruder will have no settings but inherits
+     * everything from the global settings.
+     */
+    ExtruderTrain(const size_t extruder_nr, Settings* parent_settings);
+
+private:
+    /*
+     * \brief The position of this extruder.
+     *
+     * This may be used by g-code commands such as T to indicate to which
+     * tool we must switch.
+     */
+    const size_t extruder_nr;
+
+    /*
+     * \brief The settings that this extruder overwrites.
+     */
+    Settings settings;
 };
 
 }//namespace cura
