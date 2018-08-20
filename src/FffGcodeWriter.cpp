4846059fcc46a7283aea57e4bf871f5339b227d1
Aleksei S <a.sasin@ultimaker.com>
2018-08-20 16:32:11 +0200

    Change "unsinged int" type to "int", to prevenet message in make: "warning: comparison between signed and unsigned integer expressions"

--- src/FffGcodeWriter.cpp
+++ src/FffGcodeWriter.cpp
@@ -2542,7 +2542,7 @@ void FffGcodeWriter::addPrimeTower(const SliceDataStorage& storage, LayerPlan& g
         return;
     }
 
-    const unsigned int outermost_prime_tower_extruder = storage.primeTower.extruder_order[0];
+    const int outermost_prime_tower_extruder = storage.primeTower.extruder_order[0];
     if (gcode_layer.getLayerNr() == 0 && outermost_prime_tower_extruder != gcode_layer.getExtruder())
     {
         return;
