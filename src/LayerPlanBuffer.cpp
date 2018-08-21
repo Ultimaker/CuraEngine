17dc1d153eb87af12d5b5ccc762ad0515b95b47e
Cherubim <rubend@tutanota.com>
2018-08-21 15:15:54 +0200

    Use some more specialised variable types
    They are temperatures after all. Let's keep track of that.
    
    Contributes to issue CURA-4410.

--- src/LayerPlanBuffer.cpp
+++ src/LayerPlanBuffer.cpp
@@ -494,8 +494,8 @@ void LayerPlanBuffer::insertTempCommands()
             avg_flow = 0.0;
         }
 
-        double print_temp = preheat_config.getTemp(extruder, avg_flow, extruder_plan.is_initial_layer);
-        double initial_print_temp = preheat_config.getInitialPrintTemp(extruder);
+        const Temperature print_temp = preheat_config.getTemp(extruder, avg_flow, extruder_plan.is_initial_layer);
+        const Temperature initial_print_temp = preheat_config.getInitialPrintTemp(extruder);
         if (initial_print_temp == 0.0 // user doesn't want to use initial print temp feature
             || !extruder_used_in_meshgroup[extruder] // prime blob uses print temp rather than initial print temp
             || (overall_extruder_plan_idx > 0 && extruder_plans[overall_extruder_plan_idx - 1]->extruder == extruder  // prev plan has same extruder ..
