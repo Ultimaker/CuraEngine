573a42010e90ffb5570d090ed2581310c9f68502
Ghostkeeper <rubend@tutanota.com>
2018-08-30 15:06:16 +0200

    Add empty test suite for ArcusCommunicationPrivate
    Contributes to issue CURA-4410.

---
+++ tests/arcus/ArcusCommunicationPrivateTest.h
@@ -0,0 +1,24 @@
+//Copyright (c) 2018 Ultimaker B.V.
+//CuraEngine is released under the terms of the AGPLv3 or higher.
+
+#ifndef ARCUSCOMMUNICATIONPRIVATETEST_H
+#define ARCUSCOMMUNICATIONPRIVATETEST_H
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+namespace cura
+{
+
+class ArcusCommunicationPrivateTest : CppUnit::TestFixture
+{
+    CPPUNIT_TEST_SUITE(ArcusCommunicationPrivateTest);
+
+    
+
+    CPPUNIT_TEST_SUITE_END();
+}
+
+} //namespace cura
+
+#endif //ARCUSCOMMUNICATIONPRIVATETEST_H
