784335576e03924c67b780505553805b133022b7
Remco Burema <r.burema@ultimaker.com>
2018-09-04 16:52:23 +0200

    Slightly cleaned up last commit.

--- src/MergeInfillLines.cpp
+++ src/MergeInfillLines.cpp
@@ -152,7 +152,7 @@ namespace cura
         const Point second_path_end = second_path.points.back();
         const coord_t line_width = first_path.config->getLineWidth();
 
-        if (vSize2(first_path.points.back() - second_path_start) < line_width * line_width)
+        if (vSize2(first_path_end - second_path_start) < line_width * line_width)
         {
             return false;
         }
