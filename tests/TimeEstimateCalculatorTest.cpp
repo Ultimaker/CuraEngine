bb1eb4a64c38a236aa492ee9cce3a4d56aafeff6
Ghostkeeper <rubend@tutanota.com>
2018-09-27 15:29:29 +0200

    Add basic test boilerplate for time estimate calculator
    This is the simplest test I could imagine, just to start with.

---
+++ tests/TimeEstimateCalculatorTest.cpp
@@ -0,0 +1,24 @@
+//Copyright (c) 2018 Ultimaker B.V.
+//CuraEngine is released under the terms of the AGPLv3 or higher.
+
+#include "TimeEstimateCalculatorTest.h"
+#include "../src/PrintFeature.h" //We get time estimates per print feature.
+#include "../src/settings/types/Duration.h"
+
+namespace cura
+{
+
+void TimeEstimateCalculatorTest::startWithZero()
+{
+    TimeEstimateCalculator calculator;
+    std::vector<Duration> result = calculator.calculate();
+
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(PrintFeatureType::NumPrintFeatureTypes), result.size());
+
+    for (const Duration estimate : result)
+    {
+        CPPUNIT_ASSERT_EQUAL_MESSAGE("Time estimates must be zero before anything has been planned.", Duration(0.0), estimate);
+    }
+}
+
+} //namespace cura
