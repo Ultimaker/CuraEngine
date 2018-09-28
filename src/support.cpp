8e1f9d125e9835d80c2c3e5f459267128f87d5f3
Vlad Gribinchuk <vgribinchuk@amcbridge.com>
2018-09-28 13:05:03 +0300

    "Minimum Support/Interface Area" feature: Remove supports and interface areas smaller than set value

--- src/support.cpp
+++ src/support.cpp
@@ -673,10 +673,15 @@ void AreaSupport::generateSupportAreas(SliceDataStorage& storage)
         }
         std::vector<Polygons> mesh_support_areas_per_layer;
         mesh_support_areas_per_layer.resize(storage.print_layer_count, Polygons());
-
         generateSupportAreasForMesh(storage, infill_settings, roof_settings, bottom_settings, mesh_idx, storage.print_layer_count, mesh_support_areas_per_layer);
+
+        const double minimum_support_area = mesh.settings.get<double>("minimum_support_area");
         for (size_t layer_idx = 0; layer_idx < storage.print_layer_count; layer_idx++)
         {
+            if (minimum_support_area > 0.0)
+            {
+                mesh_support_areas_per_layer[layer_idx].removeSmallAreas(minimum_support_area);
+            }
             global_support_areas_per_layer[layer_idx].add(mesh_support_areas_per_layer[layer_idx]);
         }
 
@@ -1377,6 +1382,7 @@ void AreaSupport::generateSupportBottom(SliceDataStorage& storage, const SliceMe
 
     const size_t scan_count = std::max(size_t(1), (bottom_layer_count - 1) / skip_layer_count); //How many measurements to take to generate bottom areas.
     const float z_skip = std::max(1.0f, float(bottom_layer_count - 1) / float(scan_count)); //How many layers to skip between measurements. Using float for better spread, but this is later rounded.
+    const double minimum_bottom_area = mesh.settings.get<double>("minimum_bottom_area");
 
     std::vector<SupportLayer>& support_layers = storage.support.supportLayers;
     for (unsigned int layer_idx = z_distance_bottom; layer_idx < support_layers.size(); layer_idx++)
@@ -1387,8 +1393,7 @@ void AreaSupport::generateSupportBottom(SliceDataStorage& storage, const SliceMe
         {
             mesh_outlines.add(mesh.layers[std::round(layer_idx_below)].getOutlines());
         }
-        Polygons bottoms;
-        generateSupportInterfaceLayer(global_support_areas_per_layer[layer_idx], mesh_outlines, bottom_line_width, bottoms);
+        Polygons bottoms = generateSupportInterfaceLayer(global_support_areas_per_layer[layer_idx], mesh_outlines, bottom_line_width, minimum_bottom_area);
         support_layers[layer_idx].support_bottom.add(bottoms);
     }
 }
@@ -1408,6 +1413,7 @@ void AreaSupport::generateSupportRoof(SliceDataStorage& storage, const SliceMesh
 
     const size_t scan_count = std::max(size_t(1), (roof_layer_count - 1) / skip_layer_count); //How many measurements to take to generate roof areas.
     const float z_skip = std::max(1.0f, float(roof_layer_count - 1) / float(scan_count)); //How many layers to skip between measurements. Using float for better spread, but this is later rounded.
+    const double minimum_roof_area = mesh.settings.get<double>("minimum_roof_area");
 
     std::vector<SupportLayer>& support_layers = storage.support.supportLayers;
     for (LayerIndex layer_idx = 0; layer_idx < static_cast<int>(support_layers.size() - z_distance_top); layer_idx++)
@@ -1418,19 +1424,22 @@ void AreaSupport::generateSupportRoof(SliceDataStorage& storage, const SliceMesh
         {
             mesh_outlines.add(mesh.layers[std::round(layer_idx_above)].getOutlines());
         }
-        Polygons roofs;
-        generateSupportInterfaceLayer(global_support_areas_per_layer[layer_idx], mesh_outlines, roof_line_width, roofs);
+        Polygons roofs = generateSupportInterfaceLayer(global_support_areas_per_layer[layer_idx], mesh_outlines, roof_line_width, minimum_roof_area);
         support_layers[layer_idx].support_roof.add(roofs);
     }
 }
 
-void AreaSupport::generateSupportInterfaceLayer(Polygons& support_areas, const Polygons colliding_mesh_outlines, const coord_t safety_offset, Polygons& interface_polygons)
+Polygons AreaSupport::generateSupportInterfaceLayer(Polygons& support_areas, const Polygons colliding_mesh_outlines, const coord_t safety_offset, const double minimum_interface_area)
 {
     Polygons model = colliding_mesh_outlines.unionPolygons();
-    interface_polygons = support_areas.intersection(model);
+    Polygons interface_polygons = support_areas.intersection(model);
     interface_polygons = interface_polygons.offset(safety_offset).intersection(support_areas); //Make sure we don't generate any models that are not printable.
-    interface_polygons.removeSmallAreas(1.0);
+    if (minimum_interface_area > 0.0)
+    {
+        interface_polygons.removeSmallAreas(minimum_interface_area);
+    }
     support_areas = support_areas.difference(interface_polygons);
+    return interface_polygons;
 }
 
 }//namespace cura
