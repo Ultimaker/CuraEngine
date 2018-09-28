91a1269c4ddb7cb92113dbf3394960c77c8bff91
Ghostkeeper <rubend@tutanota.com>
2018-09-28 12:38:09 +0200

    Remove simple X move test
    The calculation is not simple. We're going to have to investigate this more in order to verify that this is happening.
    The problem with this test is that even if the jerk is not limiting anything any more, it still decides to decelerate slowly to the minimum planner speed (rather than using jerk) at the very last command of a series of commands. Since there was only one command, that one command was the last command so it slowly decelerated. This makes the computation more complex. We'll investigate it some day.

--- tests/TimeEstimateCalculatorTest.cpp
+++ tests/TimeEstimateCalculatorTest.cpp
@@ -43,17 +43,4 @@ void TimeEstimateCalculatorTest::startWithZero()
     }
 }
 
-void TimeEstimateCalculatorTest::singleMoveX()
-{
-    constexpr double distance = 100;
-    constexpr Velocity feed_rate = 20;
-    constexpr PrintFeatureType feature = PrintFeatureType::Skin;
-    calculator.plan(TimeEstimateCalculator::Position(distance, 0, 0, 0), feed_rate, feature); //Move to 100,0 with 20mm/s.
-
-    //The jerk is by default set to 20.0, which is equal to our feedrate, so we expect to be at maximum velocity immediately.
-    constexpr Duration expected_result = distance / feed_rate;
-
-    CPPUNIT_ASSERT_EQUAL(Duration(5.0), calculator.calculate()[static_cast<size_t>(feature)]);
-}
-
 } //namespace cura
