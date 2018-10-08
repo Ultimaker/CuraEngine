717e8b10526c75de9c668a06a80e1107f46f3674
Aleksei S <a.sasin@ultimaker.com>
2018-10-08 10:49:21 +0200

    Fix optimizePaths when coordinate origin at the build plate center CURA-5777

--- src/MergeInfillLines.cpp
+++ src/MergeInfillLines.cpp
@@ -73,7 +73,7 @@ MergeInfillLines::MergeInfillLines(ExtruderPlan& plan)
             {
                 average_first_path += point;
             }
-            average_first_path /= (first_path.points.size() + 1);
+            average_first_path = average_first_path / (first_path.points.size() + 1);
         }
 
         coord_t second_path_length = calcPathLength(second_path_start, second_path);
