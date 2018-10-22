35d741465c35850d31420f844f7b387800f04cdc
Tim Kuipers <t.kuipers@ultimaker.com>
2018-10-22 15:51:45 +0200

    feat: initial_bottom_layer_count
    should work irrespective of the bottom_layer_count.
    You should be able to turn off bottom layers, but turn on initial bottom layers - and vice versa.

--- src/skin.cpp
+++ src/skin.cpp
@@ -64,7 +64,7 @@ SkinInfillAreaComputation::SkinInfillAreaComputation(const LayerIndex& layer_nr,
 : layer_nr(layer_nr)
 , mesh(mesh)
 , bottom_layer_count(mesh.settings.get<size_t>("bottom_layers"))
-, layer_0_skin_enabled(mesh.settings.get<bool>("layer_0_skin_enabled"))
+, initial_bottom_layer_count(mesh.settings.get<size_t>("initial_bottom_layers"))
 , top_layer_count(mesh.settings.get<size_t>("top_layers"))
 , wall_line_count(mesh.settings.get<size_t>("wall_line_count"))
 , skin_line_width(getSkinLineWidth(mesh, layer_nr))
@@ -205,7 +205,7 @@ void SkinInfillAreaComputation::generateSkinAndInfillAreas(SliceLayerPart& part)
         upskin = Polygons(original_outline);
     }
     Polygons downskin;
-    if (bottom_layer_count > 0)
+    if (bottom_layer_count > 0 || layer_nr < LayerIndex(initial_bottom_layer_count))
     {
         downskin = Polygons(original_outline);
     }
@@ -242,8 +242,14 @@ void SkinInfillAreaComputation::generateSkinAndInfillAreas(SliceLayerPart& part)
  */
 void SkinInfillAreaComputation::calculateBottomSkin(const SliceLayerPart& part, Polygons& downskin)
 {
-    if ((static_cast<int>(layer_nr - bottom_layer_count) >= 0 || !layer_0_skin_enabled) && bottom_layer_count > 0)
+    if (bottom_layer_count == 0 && initial_bottom_layer_count == 0)
     {
+        return; // downskin remains empty
+    }
+    if (layer_nr < LayerIndex(initial_bottom_layer_count))
+    {
+        return; // don't subtract anything form the downskin
+    }
         LayerIndex bottom_check_start_layer_idx = std::max(LayerIndex(0), layer_nr - bottom_layer_count);
         Polygons not_air = getWalls(part, bottom_check_start_layer_idx, bottom_reference_wall_idx).offset(bottom_reference_wall_expansion);
         if (!no_small_gaps_heuristic)
@@ -259,7 +265,6 @@ void SkinInfillAreaComputation::calculateBottomSkin(const SliceLayerPart& part, 
             not_air.removeSmallAreas(min_infill_area);
         }
         downskin = downskin.difference(not_air); // skin overlaps with the walls
-    }
 }
 
 void SkinInfillAreaComputation::calculateTopSkin(const SliceLayerPart& part, Polygons& upskin)
@@ -551,7 +556,7 @@ void SkinInfillAreaComputation::generateGradualInfill(SliceMeshStorage& mesh)
     layer_skip_count = gradual_infill_step_layer_count / n_skip_steps_per_gradual_step;
     const size_t max_infill_steps = mesh.settings.get<size_t>("gradual_infill_steps");
 
-    const LayerIndex min_layer = mesh.settings.get<bool>("layer_0_skin_enabled")? mesh.settings.get<size_t>("bottom_layers") : 0;
+    const LayerIndex min_layer = mesh.settings.get<size_t>("initial_bottom_layers");
     const LayerIndex max_layer = mesh.layers.size() - 1 - mesh.settings.get<size_t>("top_layers");
 
     for (LayerIndex layer_idx = 0; layer_idx < static_cast<LayerIndex>(mesh.layers.size()); layer_idx++)
@@ -633,7 +638,7 @@ void SkinInfillAreaComputation::combineInfillLayers(SliceMeshStorage& mesh)
     divisible index. Otherwise we get some parts that have infill at divisible
     layers and some at non-divisible layers. Those layers would then miss each
     other. */
-    int bottom_most_layers = mesh.settings.get<bool>("layer_0_skin_enabled")? mesh.settings.get<size_t>("bottom_layers") : 0;
+    size_t bottom_most_layers = mesh.settings.get<size_t>("initial_bottom_layers");
     LayerIndex min_layer = static_cast<LayerIndex>(bottom_most_layers + amount) - 1;
     min_layer -= min_layer % amount; //Round upwards to the nearest layer divisible by infill_sparse_combine.
     LayerIndex max_layer = static_cast<LayerIndex>(mesh.layers.size()) - 1 - mesh.settings.get<size_t>("top_layers");
