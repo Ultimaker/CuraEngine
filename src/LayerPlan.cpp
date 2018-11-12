7692082f02b3b3f6a951d65ebcbad1a4157a7004
Aleksei S <a.sasin@ultimaker.com>
2018-11-12 01:18:02 +0100

    Combing functionality should not depend on minimum retraction travel distance. In previous implementation the setting combing minimum retraction distance influenced to combing behaviour. The setting affected not only enabling/disabling retraction, but also changing the travel path. Which would lead the travel path was outside the model boundaries. CURA-5818

--- src/LayerPlan.cpp
+++ src/LayerPlan.cpp
@@ -375,7 +375,11 @@ GCodePath& LayerPlan::addTravel(Point p, bool force_comb_retract)
     if (comb != nullptr && !bypass_combing)
     {
         CombPaths combPaths;
-        combed = comb->calc(*extruder, *last_planned_position, p, combPaths, was_inside, is_inside, retraction_config.retraction_min_travel_distance);
+        
+        // Multiply by 2 because if two lines start and end points places very closed then will be applied combing with retractions. (Ex: for bRim)
+        coord_t max_distance_ignored = extruder->settings.get<coord_t>("line_width") * 2;
+        
+        combed = comb->calc(*extruder, *last_planned_position, p, combPaths, was_inside, is_inside, max_distance_ignored);
         if (combed)
         {
             bool retract = path->retract || combPaths.size() > 1;
