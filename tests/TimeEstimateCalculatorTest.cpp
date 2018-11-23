479bd739bf20350d284b17cefe32faefbcc74e04
Ghostkeeper <rubend@tutanota.com>
2018-11-23 17:29:46 +0100

    Use pre-defined epsilon for all double comparison tests

--- tests/TimeEstimateCalculatorTest.cpp
+++ tests/TimeEstimateCalculatorTest.cpp
@@ -11,6 +11,7 @@ namespace cura
 {
 
 #define MINIMUM_PLANNER_SPEED 0.05 // mm/sec
+#define EPSILON 0.00001 //Allowed error in comparing floating point values.
 
 CPPUNIT_TEST_SUITE_REGISTRATION(TimeEstimateCalculatorTest);
 
@@ -69,26 +70,26 @@ void TimeEstimateCalculatorTest::addTime()
 {
     calculator.addTime(2);
     std::vector<Duration> result = calculator.calculate();
-    CPPUNIT_ASSERT_EQUAL(Duration(2.0), result[static_cast<size_t>(PrintFeatureType::NoneType)]);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(Duration(2.0), result[static_cast<size_t>(PrintFeatureType::NoneType)], EPSILON);
 
     calculator.addTime(3); //Has to add up, not replace.
     result = calculator.calculate();
-    CPPUNIT_ASSERT_EQUAL(Duration(5.0), result[static_cast<size_t>(PrintFeatureType::NoneType)]);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(Duration(5.0), result[static_cast<size_t>(PrintFeatureType::NoneType)], EPSILON);
 
     calculator.addTime(-7);
     result = calculator.calculate();
-    CPPUNIT_ASSERT_EQUAL(Duration(5.0), result[static_cast<size_t>(PrintFeatureType::NoneType)]); //Due to how Duration works, it can never go lower.
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(Duration(5.0), result[static_cast<size_t>(PrintFeatureType::NoneType)], EPSILON); //Due to how Duration works, it can never go lower.
 }
 
 void TimeEstimateCalculatorTest::startWithZero()
 {
     const std::vector<Duration> result = calculator.calculate();
 
-    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(PrintFeatureType::NumPrintFeatureTypes), result.size());
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(static_cast<size_t>(PrintFeatureType::NumPrintFeatureTypes), result.size(), EPSILON);
 
     for (const Duration estimate : result)
     {
-        CPPUNIT_ASSERT_EQUAL_MESSAGE("Time estimates must be zero before anything has been planned.", Duration(0.0), estimate);
+        CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("Time estimates must be zero before anything has been planned.", Duration(0.0), estimate, EPSILON);
     }
 }
 
@@ -99,13 +100,13 @@ void TimeEstimateCalculatorTest::moveToCurrentLocation()
 
     std::vector<Duration> result = calculator.calculate();
     Duration estimate = std::accumulate(result.begin(), result.end(), Duration(0.0));
-    CPPUNIT_ASSERT_EQUAL_MESSAGE("setPosition should not add any time to the estimate.", Duration(0.0), estimate);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("setPosition should not add any time to the estimate.", Duration(0.0), estimate, EPSILON);
 
     calculator.plan(position, Velocity(10), PrintFeatureType::Infill);
 
     result = calculator.calculate();
     estimate = std::accumulate(result.begin(), result.end(), Duration(0.0));
-    CPPUNIT_ASSERT_EQUAL_MESSAGE("Moving to the same location as where you already were should not cost any time.", Duration(0.0), estimate);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("Moving to the same location as where you already were should not cost any time.", Duration(0.0), estimate, EPSILON);
 }
 
 void TimeEstimateCalculatorTest::singleLineOnlyJerk()
@@ -128,9 +129,10 @@ void TimeEstimateCalculatorTest::singleLineOnlyJerk()
     const double cruise_distance = 1000.0 - decelerate_distance;
 
     const std::vector<Duration> result = calculator.calculate();
-    CPPUNIT_ASSERT_EQUAL(
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(
         Duration(cruise_distance / 50.0 + t), //1000mm at 50mm/s, then decelerate for almost one second.
-        result[static_cast<size_t>(PrintFeatureType::Infill)]
+        result[static_cast<size_t>(PrintFeatureType::Infill)],
+        EPSILON
     );
 }
 
@@ -160,7 +162,7 @@ void TimeEstimateCalculatorTest::doubleLineOnlyJerk()
     CPPUNIT_ASSERT_DOUBLES_EQUAL(
         Duration(cruise_distance / 50.0 + t), //2000mm at 50mm/s, then decelerate for almost one second.
         result[static_cast<size_t>(PrintFeatureType::Infill)],
-        0.00001
+        EPSILON
     );
 }
 
@@ -184,9 +186,10 @@ void TimeEstimateCalculatorTest::singleLineNoJerk()
     const double cruise_distance = 1000.0 - accelerate_distance - decelerate_distance;
 
     const std::vector<Duration> result = calculator.calculate();
-    CPPUNIT_ASSERT_EQUAL(
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(
         Duration(1.0 + cruise_distance / 50.0 + decelerate_t), //Accelerate, cruise, decelerate.
-        result[static_cast<size_t>(PrintFeatureType::Infill)]
+        result[static_cast<size_t>(PrintFeatureType::Infill)],
+        EPSILON
     );
 }
 
@@ -216,7 +219,7 @@ void TimeEstimateCalculatorTest::doubleLineNoJerk()
     CPPUNIT_ASSERT_DOUBLES_EQUAL(
         Duration(1.0 + cruise_distance / 50.0 + decelerate_t), //Accelerate, cruise, decelerate.
         result[static_cast<size_t>(PrintFeatureType::Infill)],
-        0.00001
+        EPSILON
     );
 }
 
@@ -244,7 +247,7 @@ void TimeEstimateCalculatorTest::diagonalLineNoJerk()
     CPPUNIT_ASSERT_DOUBLES_EQUAL(
         Duration(1.0 + cruise_distance / 50.0 + decelerate_t), //Accelerate, cruise, decelerate.
         result[static_cast<size_t>(PrintFeatureType::Infill)],
-        0.00001
+        EPSILON
     );
 }
 
