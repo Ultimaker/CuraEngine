375fb0002de1a12c50e9bb883e621878f52549df
Remco Burema <r.burema@ultimaker.com>
2018-12-21 17:34:34 +0100

    Fix center-position for prime-tower when prime-tower-only brim is enabled. [CURA-5864]

--- src/PrimeTower.cpp
+++ src/PrimeTower.cpp
@@ -55,10 +55,15 @@ void PrimeTower::generateGroundpoly()
     const coord_t tower_size = mesh_group_settings.get<coord_t>("prime_tower_size");
     const bool circular_prime_tower = mesh_group_settings.get<bool>("prime_tower_circular");
 
+    const coord_t offset = (! mesh_group_settings.get<bool>("prime_tower_brim_enable")) ? 0 :
+        mesh_group_settings.get<size_t>("brim_line_count") *
+        mesh_group_settings.get<coord_t>("skirt_brim_line_width") *
+        mesh_group_settings.get<Ratio>("initial_layer_line_width_factor");
+
     PolygonRef p = outer_poly.newPoly();
     int tower_distance = 0; 
-    const coord_t x = mesh_group_settings.get<coord_t>("prime_tower_position_x");
-    const coord_t y = mesh_group_settings.get<coord_t>("prime_tower_position_y");
+    const coord_t x = mesh_group_settings.get<coord_t>("prime_tower_position_x") - offset;
+    const coord_t y = mesh_group_settings.get<coord_t>("prime_tower_position_y") - offset;
     if (circular_prime_tower)
     {
         const coord_t tower_radius = tower_size / 2;
