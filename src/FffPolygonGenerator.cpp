99ee78237b8d356ccc6096a2575fa4d73e15d166
Ghostkeeper <rubend@tutanota.com>
2019-03-11 13:16:52 +0100

    Don't limit adaptive threshold to 360
    This must've been a wrong interpretation of this setting. It's a bit abstract.
    
    Contributes to issue CURA-6327.

--- src/FffPolygonGenerator.cpp
+++ src/FffPolygonGenerator.cpp
@@ -121,9 +121,9 @@ bool FffPolygonGenerator::sliceModel(MeshGroup* meshgroup, TimeKeeper& timeKeepe
     if (use_variable_layer_heights)
     {
         // Calculate adaptive layer heights
-        coord_t variable_layer_height_max_variation = mesh_group_settings.get<coord_t>("adaptive_layer_height_variation");
-        coord_t variable_layer_height_variation_step = mesh_group_settings.get<coord_t>("adaptive_layer_height_variation_step");
-        AngleDegrees adaptive_threshold = mesh_group_settings.get<AngleDegrees>("adaptive_layer_height_threshold");
+        const coord_t variable_layer_height_max_variation = mesh_group_settings.get<coord_t>("adaptive_layer_height_variation");
+        const coord_t variable_layer_height_variation_step = mesh_group_settings.get<coord_t>("adaptive_layer_height_variation_step");
+        const double adaptive_threshold = mesh_group_settings.get<double>("adaptive_layer_height_threshold");
         adaptive_layer_heights = new AdaptiveLayerHeights(layer_thickness, variable_layer_height_max_variation,
                                                           variable_layer_height_variation_step, adaptive_threshold);
 
