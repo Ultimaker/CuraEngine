ba4ae485bd53c4dc137394981088083f4eb3530d
Mark Burton <markb@smartavionics.com>
2019-04-07 16:23:46 +0100

    Update extruder_prime_layer_nr for all extruders used in a layer, not just the last used.

--- src/FffGcodeWriter.cpp
+++ src/FffGcodeWriter.cpp
@@ -1062,7 +1062,11 @@ void FffGcodeWriter::calculateExtruderOrderPerLayer(const SliceDataStorage& stor
         std::vector<std::vector<size_t>>& extruder_order_per_layer_here = (layer_nr < 0) ? extruder_order_per_layer_negative_layers : extruder_order_per_layer;
         extruder_order_per_layer_here.push_back(getUsedExtrudersOnLayerExcludingStartingExtruder(storage, last_extruder, layer_nr));
         last_extruder = extruder_order_per_layer_here.back().back();
-        extruder_prime_layer_nr[last_extruder] = std::min(extruder_prime_layer_nr[last_extruder], layer_nr);
+
+        for (size_t extruder_nr : extruder_order_per_layer_here.back())
+        {
+            extruder_prime_layer_nr[extruder_nr] = std::min(extruder_prime_layer_nr[extruder_nr], layer_nr);
+        }
     }
 }
 
