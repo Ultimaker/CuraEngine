f249ebee1bb4b2f7dbc6b490f6f235e94b192dec
Tim Kuipers <t.kuipers@ultimaker.com>
2019-05-26 23:03:14 +0200

    lil

--- src/VoronoiQuadrilateralization.h
+++ src/VoronoiQuadrilateralization.h
@@ -24,6 +24,9 @@ class VoronoiQuadrilateralization
     using graph_t = HalfEdgeGraph<VoronoiQuadrilateralizationJoint, VoronoiQuadrilateralizationEdge>;
     using edge_t = HalfEdge<VoronoiQuadrilateralizationJoint, VoronoiQuadrilateralizationEdge>;
     using node_t = HalfEdgeNode<VoronoiQuadrilateralizationJoint, VoronoiQuadrilateralizationEdge>;
+
+    coord_t snap_dist = 10; // generic arithmatic inaccuracy
+    coord_t rib_snap_distance = 100; // smallest segment cut off of an outline segment by an introduced rib. smaller than this the rib is canceled because it lies too close to an existing edge
 public:
     using Segment = PolygonsSegmentIndex;
     VoronoiQuadrilateralization(const Polygons& polys);
@@ -33,7 +36,6 @@ protected:
     std::unordered_map<vd_t::vertex_type*, node_t*> vd_node_to_he_node;
     node_t& make_node(vd_t::vertex_type& vd_node, Point p);
     edge_t& make_edge(Point from, Point to, vd_t::edge_type& vd_edge);
-    coord_t snap_dist = 10;
     void debugCheckGraphCompleteness();
     void debugOutput(SVG& svg);
     SVG::Color getColor(edge_t& edge);
