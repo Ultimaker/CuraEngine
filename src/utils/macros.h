3eed89215f561aebadfc8b60a97f6cc12d1eecd5
Tim Kuipers <t.kuipers@ultimaker.com>
2019-05-26 20:51:44 +0200

    aux RUN_ONCE macro

--- src/utils/macros.h
+++ src/utils/macros.h
@@ -5,4 +5,16 @@
 // macro to suppress unused parameter warnings from the compiler
 #define UNUSED_PARAM(param) (void)(param)
 
+// simple function to be used to reduce the number of times an error is logged
+// no guarantees in a mutli-threaded context
+#define RUN_ONCE(runcode) \
+{ \
+    static bool code_ran = 0; \
+    if(!code_ran){ \
+        code_ran = 1; \
+        runcode; \
+    } \
+}
+
+
 #endif // UTILS_MACROS_H
