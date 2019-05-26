959d9bed034a8194fb924cb5ae5b7c6638b88149
Tim Kuipers <t.kuipers@ultimaker.com>
2019-05-26 22:05:27 +0200

    use simpler test case

--- src/mainArachne.cpp
+++ src/mainArachne.cpp
@@ -45,6 +45,24 @@ Polygons generateTestPoly(size_t size, Point border)
     return polys;
 }
 
+static Polygons test_poly_1;
+
+void generateTestPolys()
+{
+    PolygonRef poly = test_poly_1.newPoly();
+    poly.emplace_back(0, 0);
+    poly.emplace_back(10000, 0);
+    poly.emplace_back(5000, 1000);
+    poly.emplace_back(4000, 2000);
+    poly.emplace_back(3000, 5000);
+    poly.emplace_back(2000, 6000);
+    poly.emplace_back(1000, 5000);
+    poly.emplace_back(0, 3000);
+    PolygonRef hole = test_poly_1.newPoly();
+    hole.emplace_back(500,500);
+    hole.emplace_back(600,500);
+    hole.emplace_back(650,450);
+}
 
 void test()
 {
@@ -59,9 +77,11 @@ void test()
     printf("random seed: %d\n", r);
     logError("boost version: %s\n", BOOST_LIB_VERSION);
     
+    generateTestPolys();
     
     
-    Polygons polys = generateTestPoly(20, Point(10000, 10000));
+//     Polygons polys = generateTestPoly(20, Point(10000, 10000));
+    Polygons polys = test_poly_1;
     {
         SVG svg("output/outline.svg", AABB(Point(0,0), Point(10000, 10000)));
         svg.writePolygons(polys);
