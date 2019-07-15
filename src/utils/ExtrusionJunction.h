6632f76771df1cf3dbb9a54fccfd92c5fc0d74b4
Tim Kuipers <t.kuipers@ultimaker.com>
2019-07-15 18:09:18 +0200

    aux ExtrusionJunction.operator==

--- src/utils/ExtrusionJunction.h
+++ src/utils/ExtrusionJunction.h
@@ -16,6 +16,12 @@ struct ExtrusionJunction
     coord_t perimeter_index;
     ExtrusionJunction(Point p, coord_t w, coord_t perimeter_index)
     : p(p), w(w), perimeter_index(perimeter_index) {}
+    bool operator==(const ExtrusionJunction& other) const
+    {
+        return p == other.p
+            && w == other.w
+            && perimeter_index == other.perimeter_index;
+    }
 };
 
 
