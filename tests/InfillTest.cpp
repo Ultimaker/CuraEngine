1c71c0577163eca95bf1b0e5dbbc2a5344ba49a0
Remco Burema <r.burema@ultimaker.com>
2019-07-19 15:04:56 +0200

    Finalize first infill test (unparameterized).
    part of Turbo Testing & Tooling sessions

--- tests/InfillTest.cpp
+++ tests/InfillTest.cpp
@@ -17,7 +17,7 @@ namespace cura
         {
             return false;
         }
-        
+
         Polygon next_path;
         Polygons next_shape;
 
@@ -69,19 +69,37 @@ namespace cura
         return true;
     }
 
+    double calculateInfillArea(const coord_t& infill_line_width, Polygons& infill_shapes)
+    {
+        double total_infill_area = 0;
+        for (PolygonRef poly : infill_shapes)
+        {
+            Point last_line = poly[0];
+            for (const Point& line : poly)
+            {
+                // don't have to skip the 0th line, as it'll have an area of 0 anyway
+                const double vector_x = last_line.X - line.X;
+                const double vector_y = last_line.Y - line.Y;
+                total_infill_area += std::sqrt(vector_x * vector_x + vector_y * vector_y) * infill_line_width;
+
+                last_line = line;
+            }
+        }
+        return total_infill_area;
+    }
+
     TEST(Infill, TestVerticesInside)
     {
         const EFillMethod pattern = cura::EFillMethod::LINES;  // param
         const bool zig_zagify = false; // param
         const bool conect_polygons = false; // param
-        //const Polygons outline = OUTLINE; // param
         const coord_t outline_offset = 0;
         const coord_t infill_line_width = 350;
-        const coord_t line_distance = 1200; //350;
+        const coord_t line_distance = 1200; // param
         const coord_t infill_overlap = 0;
         const size_t infill_multiplier = 1;
         const AngleDegrees fill_angle = 0;
-        const coord_t z = 100; // param? (for some)
+        const coord_t z = 100; // even / uneven layers
         const coord_t shift = 0;
 
         std::vector<Polygons> polygons;
@@ -113,24 +131,12 @@ namespace cura
 
             ASSERT_FALSE(result_polygons.empty() && result_lines.empty()) << "Infill should have been generated.";
 
-            double total_area = 0;
-            for (PolygonRef poly : result_lines)
-            {
-                Point last_line = poly[0];
-                for (Point line : poly)
-                {
-                    // don't have to skip the 0th line, as it'll have an area of 0 anyway
-                    const double vector_x = last_line.X - line.X;
-                    const double vector_y = last_line.Y - line.Y;
-                    total_area += std::sqrt(vector_x * vector_x + vector_y * vector_y) * infill_line_width;
-
-                    last_line = line;
-                }
-            }
-
-            ASSERT_GT(total_area, 0) << "Infill area should occupy at least some space.";
-            ASSERT_GT(std::abs(outline.area()), total_area) << "There shouldn't be more infill than area to fill.";
-            // TODO: compare (fuzzyly) with 'percentage'/expected, not just shape total
+            const double available_area = std::abs(outline.area());
+            const double expected_infill_area = (available_area * infill_line_width) / line_distance;
+            const double total_infill_area = calculateInfillArea(infill_line_width, result_polygons) + calculateInfillArea(infill_line_width, result_lines);
+            
+            ASSERT_GT((coord_t) available_area, (coord_t) total_infill_area) << "Infill area should allways be less than the total area available.";
+            ASSERT_NEAR((coord_t) total_infill_area, (coord_t) expected_infill_area, (coord_t) (expected_infill_area * 0.05)) << "Infill area should be within 5% of expected size.";
         }
     }
 
