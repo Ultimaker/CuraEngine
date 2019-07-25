b8a8711c3cfaa96d4daea325622671eb36fb113a
Tim Kuipers <t.kuipers@ultimaker.com>
2019-07-25 11:34:14 +0200

    Modify SVGloader to only work for polygon objects, not path objects
    path objects are hella difficult

--- src/TestGeometry/SVGloader.h
+++ src/TestGeometry/SVGloader.h
@@ -30,7 +30,7 @@ public:
         }
         std::regex poly_regex("<(polygon)|(path) .*/>");
 
-        std::regex points_regex("( points)|( d)=\"([^\"]*)\"");
+        std::regex points_regex(" points=\"([^\"]*)\"");
         std::smatch points_sm;
 
         std::regex d_regex("d=\"([^\"]*)\"");
@@ -44,12 +44,12 @@ public:
         {
             if (std::regex_search(line, points_sm, points_regex))
             {
-                if (points_sm.size() < 2)
+                if (points_sm.size() < 1)
                 {
                     assert(false);
                     continue; // didn't contain points
                 }
-                std::string points_str = points_sm[3];
+                std::string points_str = points_sm[1];
                         
                 std::transform(points_str.begin(), points_str.end(), points_str.begin(),
                     [](unsigned char c){ return std::tolower(c); });
@@ -64,42 +64,13 @@ public:
                 while (std::regex_search (points_str, point_sm, point_regex))
                 {
                     float x, y;
-                    std::string point_str = point_sm[1];
-                    if (point_str.compare("m") == 0)
-                    {
-                        additive = true;
-                        painting = false;
-                    }
-                    else if (point_str.compare("z") == 0)
-                    {
-                        PolygonRef new_poly = ret.newPoly();
-                        poly_p = new_poly;
-                    }
-                    else if (point_str.compare("l") == 0)
-                    {
-                        painting = true;
-                    }
-                    else if (point_str.compare("h") == 0)
-                    {
-                        is_horizontal = true;
-                    }
-                    else if (point_str.compare("v") == 0)
-                    {
-                        is_vertical = true;
-                    }
-                    else if (std::sscanf(point_str.c_str(), "%f,%f", &x, &y) == 2)
+                    std::string point_str = point_sm[0];
+                    if (point_str.compare("") == 0) break;
+                    if (std::sscanf(point_str.c_str(), "%f,%f", &x, &y) == 2)
                     {
                         Point here(MM2INT(x), MM2INT(y));
                         current = additive? current + here : here;
                         poly_p->emplace_back(current);
-                    }
-                    else if ((is_vertical || is_horizontal) && std::sscanf(point_str.c_str(), "%f", &x) > 0)
-                    {
-                        Point here = is_vertical? Point(MM2INT(x), 0) : Point(0, MM2INT(x));
-                        current = additive? current + here : here;
-                        poly_p->emplace_back(current);
-                        is_vertical = false;
-                        is_horizontal = false;
                     }
                     else
                     {
