74374170949f6d0294f63a84c1473ce9940b5e04
Tim Kuipers <t.kuipers@ultimaker.com>
2019-09-05 15:17:03 +0200

    feat: limited bead count variant for any strategy

---
+++ src/LimitedBeadingStrategy.cpp
@@ -0,0 +1,68 @@
+//Copyright (c) 2019 Ultimaker B.V.
+
+
+#include "LimitedBeadingStrategy.h"
+
+namespace arachne
+{
+
+LimitedBeadingStrategy::Beading LimitedBeadingStrategy::compute(coord_t thickness, coord_t bead_count) const
+{
+    if (thickness < parent->transition_thickness(max_bead_count))
+    {
+        return parent->compute(thickness, bead_count);
+    }
+
+    Beading ret = parent->compute(parent->optimal_thickness(bead_count), bead_count);
+    ret.left_over += thickness - ret.total_thickness;
+    ret.total_thickness = thickness;
+    
+    // enforece symmetry
+    if (bead_count % 2 == 1)
+        ret.toolpath_locations[bead_count / 2] = thickness / 2;
+    for (coord_t bead_idx = 0; bead_idx < (bead_count + 1) / 2; bead_idx++)
+    {
+        ret.toolpath_locations[bead_count - 1 - bead_idx] = thickness - ret.toolpath_locations[bead_count];
+    }
+    return ret;
+}
+
+coord_t LimitedBeadingStrategy::optimal_thickness(coord_t bead_count) const
+{
+    if (bead_count <= max_bead_count)
+    {
+        return parent->optimal_thickness(bead_count);
+    }
+    return 10000000; // 10 meter
+}
+
+coord_t LimitedBeadingStrategy::transition_thickness(coord_t lower_bead_count) const
+{
+    if (lower_bead_count < max_bead_count)
+    {
+        return parent->transition_thickness(lower_bead_count);
+    }
+    return 9000000; // 9 meter
+}
+
+coord_t LimitedBeadingStrategy::optimal_bead_count(coord_t thickness) const
+{
+    if (thickness < parent->optimal_thickness(max_bead_count))
+    {
+        return parent->optimal_bead_count(thickness);
+    }
+    return max_bead_count;
+}
+
+std::vector<coord_t> LimitedBeadingStrategy::getNonlinearThicknesses(coord_t lower_bead_count) const
+{
+    std::vector<coord_t> ret;
+    if (lower_bead_count == max_bead_count)
+    {
+        ret.emplace_back(optimal_thickness(max_bead_count));
+        ret.emplace_back(parent->transition_thickness(max_bead_count));
+        ret.emplace_back(parent->transition_thickness(max_bead_count) + optimal_width / 2); // ideally the transition from a thin section to a section where all beads have preferred widths is a constant length, while now the length is determined by the slope of the marked region.
+    }
+    return ret;
+}
+} // namespace arachne
---
+++ src/LimitedBeadingStrategy.h
@@ -0,0 +1,47 @@
+//Copyright (c) 2019 Ultimaker B.V.
+
+
+#ifndef LIMITED_BEADING_STRATEGY_H
+#define LIMITED_BEADING_STRATEGY_H
+
+#include "BeadingStrategy.h"
+#include "utils/logoutput.h"
+#include "utils/macros.h"
+
+namespace arachne
+{
+
+/*!
+ * Beading strategy which evenly subdivides the thickness and tries to stay close to the optimal width.
+ */
+class LimitedBeadingStrategy : public BeadingStrategy
+{
+public:
+    const coord_t max_bead_count;
+    const BeadingStrategy* parent;
+    LimitedBeadingStrategy(const coord_t max_bead_count, BeadingStrategy* parent)
+    : BeadingStrategy(parent->optimal_width, parent->transitioning_angle)
+    , max_bead_count(max_bead_count)
+    , parent(parent)
+    {
+        if (max_bead_count % 2 == 1)
+        {
+            RUN_ONCE(logWarning("LimitedBeadingStrategy with odd bead count is odd indeed!\n"));
+        }
+    }
+    virtual ~LimitedBeadingStrategy() override
+    {
+        delete parent;
+    }
+    Beading compute(coord_t thickness, coord_t bead_count) const override;
+    coord_t optimal_thickness(coord_t bead_count) const override;
+    coord_t transition_thickness(coord_t lower_bead_count) const override;
+    coord_t optimal_bead_count(coord_t thickness) const override;
+    std::vector<coord_t> getNonlinearThicknesses(coord_t lower_bead_count) const override;
+};
+
+
+
+
+} // namespace arachne
+#endif // LIMITED_DISTRIBUTED_BEADING_STRATEGY_H
