673ce81c5bab7231644e4fe96fb51cc7b9cf9a8e
Ghostkeeper <rubend@tutanota.com>
2019-09-10 13:40:34 +0200

    Fix bug that caused the corner shift to be weighted too much
    The *2 was applied to the entire score. It should be applied just to the weighting of the corner angle.
    
    Contributes to issue CURA-6760.

--- src/pathOrderOptimizer.cpp
+++ src/pathOrderOptimizer.cpp
@@ -181,13 +181,16 @@ int PathOrderOptimizer::getClosestPointInPolygon(Point prev_point, int poly_idx)
                 dist_score -= fabs(corner_angle - 1) * corner_shift;
                 break;
             case EZSeamCornerPrefType::Z_SEAM_CORNER_PREF_WEIGHTED:
+            {
                 //More curve is better score (reduced distance), but slightly in favour of concave curves.
-                dist_score -= fabs(corner_angle - 1) * corner_shift;
+                float dist_score_corner = fabs(corner_angle - 1) * corner_shift;
                 if (corner_angle < 1)
                 {
-                    dist_score *= 2;
+                    dist_score_corner *= 2;
                 }
+                dist_score -= dist_score_corner;
                 break;
+            }
             case EZSeamCornerPrefType::Z_SEAM_CORNER_PREF_NONE:
             default:
                 // do nothing
