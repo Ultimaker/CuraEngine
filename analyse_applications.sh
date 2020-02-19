4ad1ce83595c16d136109fd3e4457b1cd38d852f
Tim Kuipers <t.kuipers@ultimaker.com>
2020-02-19 16:12:54 +0100

    remove outdated analysis scripts and results

--- analyse_applications.sh
+++
@@ -1,6 +0,0 @@
-for file in /home/t.kuipers/Documents/PhD/Variable_Width_project/implementation/libArachne/input/Applicatas/TKs_version/APPLICATIONS/*.svg; do
- filename=$(basename "$file")
- fname="${filename%.*}"
- echo $file;
- ./release_build/arachne $file $fname
-done
--- analyse_full_database.sh
+++
@@ -1,6 +0,0 @@
-for file in /home/t.kuipers/Development/CuraEngine/output/outlines/*.svg; do
- filename=$(basename "$file")
- fname="${filename%.*}"
- echo $file;
- ./release_build/arachne $file $fname
-done
