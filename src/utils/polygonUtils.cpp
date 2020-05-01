c51311a4645ce73b9d689554838dfdbbb21f5370
DerGenaue <DerGenaue@users.noreply.github.com>
2020-05-02 00:10:58 +0200

    MoveInside: Improve Naming: ax_length2 -> dot_prod
    because it actually isn't ax_length squared

--- src/utils/polygonUtils.cpp
+++ src/utils/polygonUtils.cpp
@@ -238,7 +238,8 @@ unsigned int PolygonUtils::moveInside(const Polygons& polygons, Point& from, int
         bool projected_p_beyond_prev_segment = dot(p1 - p0, from - p0) >= vSize2(p1 - p0);
         for(const Point& p2 : poly)
         {   
-            // X = A + Normal( B - A ) * ((( B - A ) dot ( P - A )) / VSize( A - B ));
+            // X = A + Normal(B-A) * (((B-A) dot (P-A)) / VSize(B-A));
+            //   = A +       (B-A) *  ((B-A) dot (P-A)) / VSize2(B-A);
             // X = P projected on AB
             const Point& a = p1;
             const Point& b = p2;
@@ -251,8 +252,8 @@ unsigned int PolygonUtils::moveInside(const Polygons& polygons, Point& from, int
                 p1 = p2; //Skip only one of the points.
                 continue;
             }
-            int64_t ax_length2 = dot(ab, ap);
-            if (ax_length2 <= 0) // x is projected to before ab
+            int64_t dot_prod = dot(ab, ap);
+            if (dot_prod <= 0) // x is projected to before ab
             {
                 if (projected_p_beyond_prev_segment)
                 { //  case which looks like:   > .
@@ -282,7 +283,7 @@ unsigned int PolygonUtils::moveInside(const Polygons& polygons, Point& from, int
                     continue;
                 }
             }
-            else if (ax_length2 >= ab_length2) // x is projected to beyond ab
+            else if (dot_prod >= ab_length2) // x is projected to beyond ab
             {
                 projected_p_beyond_prev_segment = true;
                 p0 = p1;
@@ -292,7 +293,7 @@ unsigned int PolygonUtils::moveInside(const Polygons& polygons, Point& from, int
             else 
             { // x is projected to a point properly on the line segment (not onto a vertex). The case which looks like | .
                 projected_p_beyond_prev_segment = false;
-                Point x = a + ab * ax_length2 / ab_length2;
+                Point x = a + ab * dot_prod / ab_length2;
 
                 int64_t dist2 = vSize2(p - x);
                 if (dist2 < bestDist2)
@@ -359,7 +360,8 @@ unsigned int PolygonUtils::moveInside(const ConstPolygonRef polygon, Point& from
     bool projected_p_beyond_prev_segment = dot(p1 - p0, from - p0) >= vSize2(p1 - p0);
     for(const Point& p2 : polygon)
     {
-        // X = A + Normal( B - A ) * ((( B - A ) dot ( P - A )) / VSize( A - B ));
+        // X = A + Normal(B-A) * (((B-A) dot (P-A)) / VSize(B-A));
+        //   = A +       (B-A) *  ((B-A) dot (P-A)) / VSize2(B-A);
         // X = P projected on AB
         const Point& a = p1;
         const Point& b = p2;
@@ -372,8 +374,8 @@ unsigned int PolygonUtils::moveInside(const ConstPolygonRef polygon, Point& from
             p1 = p2; //Skip only one of the points.
             continue;
         }
-        int64_t ax_length2 = dot(ab, ap);
-        if (ax_length2 <= 0) // x is projected to before ab
+        int64_t dot_prod = dot(ab, ap);
+        if (dot_prod <= 0) // x is projected to before ab
         {
             if (projected_p_beyond_prev_segment)
             { //  case which looks like:   > .
@@ -405,7 +407,7 @@ unsigned int PolygonUtils::moveInside(const ConstPolygonRef polygon, Point& from
                 continue;
             }
         }
-        else if (ax_length2 >= ab_length2) // x is projected to beyond ab
+        else if (dot_prod >= ab_length2) // x is projected to beyond ab
         {
             projected_p_beyond_prev_segment = true;
             p0 = p1;
@@ -415,7 +417,7 @@ unsigned int PolygonUtils::moveInside(const ConstPolygonRef polygon, Point& from
         else
         { // x is projected to a point properly on the line segment (not onto a vertex). The case which looks like | .
             projected_p_beyond_prev_segment = false;
-            Point x = a + ab * ax_length2 / ab_length2;
+            Point x = a + ab * dot_prod / ab_length2;
 
             int64_t dist2 = vSize2(p - x);
             if (dist2 < bestDist2)
