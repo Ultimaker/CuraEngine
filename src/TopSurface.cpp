d8b9a98f01a2f6a6b64c83796f6dff3f0136b5c9
Kostas Karmas <konskarm@gmail.com>
2020-05-04 16:17:20 +0200

    Fix comments
    CURA-7318

--- src/TopSurface.cpp
+++ src/TopSurface.cpp
@@ -48,13 +48,13 @@ bool TopSurface::ironing(const SliceMeshStorage& mesh, const GCodePathConfig& li
     coord_t ironing_inset = -mesh.settings.get<coord_t>("ironing_inset");
     if (pattern == EFillMethod::ZIG_ZAG && ironing_inset == 0)
     {
-        //Compensate for the outline_offset decrease that takes place when infill is generated for the zigzag pattern
+        //Compensate for the outline_offset decrease that takes place when using the infill generator to generate ironing with the zigzag pattern
         const float width_scale = (float)mesh.settings.get<coord_t>("layer_height") / mesh.settings.get<coord_t>("infill_sparse_thickness");
         ironing_inset += width_scale * line_width / 2;
     }
     else if (pattern == EFillMethod::CONCENTRIC)
     {
-        //Counteract the outline_offset increase that takes place when infill is generated for the concentric pattern
+        //Counteract the outline_offset increase that takes place when using the infill generator to generate ironing with the concentric pattern
         ironing_inset -= line_width / 2;
     }
     const coord_t outline_offset = ironing_inset;
