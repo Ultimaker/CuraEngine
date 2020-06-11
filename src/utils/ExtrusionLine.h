50e01b5bb11d1b8cd937307102c0af0ce3505398
Ghostkeeper <rubend@tutanota.com>
2020-06-11 17:40:42 +0200

    Document the ExtrusionLine structure
    Contributes to issue CURA-7373.

--- src/utils/ExtrusionLine.h
+++ src/utils/ExtrusionLine.h
@@ -1,4 +1,5 @@
-//Copyright (c) 2019 Ultimaker B.V.
+//Copyright (c) 2020 Ultimaker B.V.
+//CuraEngine is released under the terms of the AGPLv3 or higher.
 
 
 #ifndef UTILS_EXTRUSION_LINE_H
@@ -10,18 +11,52 @@ namespace arachne
 {
     using namespace cura;
 
+/*!
+ * Represents a polyline (not just a line) that is to be extruded with variable
+ * line width.
+ *
+ * This polyline is a sequence of \ref ExtrusionJunction, with a bit of metadata
+ * about which inset it represents.
+ */
 struct ExtrusionLine
 {
+    /*!
+     * Which inset this path represents, counted from the outside inwards.
+     *
+     * The outer wall has index 0.
+     */
     coord_t inset_idx;
+
+    /*!
+     * If a thin piece needs to be printed with an odd number of walls (e.g. 5
+     * walls) then there will be one wall in the middle that is not a loop. This
+     * field indicates whether this path is such a line through the middle, that
+     * has no companion line going back on the other side and is not a closed
+     * loop.
+     */
     bool is_odd;
+
+    /*!
+     * The list of vertices along which this path runs.
+     *
+     * Each junction has a width, making this path a variable-width path.
+     */
     std::list<ExtrusionJunction> junctions;
+
     ExtrusionLine(coord_t inset_idx, bool is_odd)
     : inset_idx(inset_idx)
     , is_odd(is_odd)
     {}
+
+    /*!
+     * Sum the total length of this path.
+     */
     coord_t getLength() const
     {
-        if (junctions.size() <= 1) return 0;
+        if (junctions.empty())
+        {
+            return 0;
+        }
         coord_t len = 0;
         ExtrusionJunction prev = junctions.front();
         for (const ExtrusionJunction& next : junctions)
