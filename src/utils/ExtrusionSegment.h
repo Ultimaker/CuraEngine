c3ebde99b815fafc2d730df527bc30a85901eb7d
Ghostkeeper <rubend@tutanota.com>
2020-06-15 13:47:23 +0200

    Document functions of ExtrusionSegment class
    Contributes to issue CURA-7373.

--- src/utils/ExtrusionSegment.h
+++ src/utils/ExtrusionSegment.h
@@ -1,4 +1,5 @@
-//Copyright (c) 2019 Ultimaker B.V.
+//Copyright (c) 2020 Ultimaker B.V.
+//CuraEngine is released under the terms of the AGPLv3 or higher.
 
 
 #ifndef UTILS_EXTRUSION_SEGMENT_H
@@ -16,8 +17,10 @@ namespace arachne
     using namespace cura;
 
 /*!
- * extrusion bead
- * suports varying width
+ * Represents a single line segment as part of an extrusion path.
+ *
+ * This supports varying line width, since the two junctions this is composed
+ * out of can have different widths.
  */
 class ExtrusionSegment
 {
@@ -36,15 +39,33 @@ public:
     , is_reduced(is_reduced)
     {}
 
+    /*!
+     * Converts this segment to an outline of the area that the segment covers.
+     * \return The area that would be covered by this extrusion segment.
+     */
     Polygons toPolygons(); 
 
     /*!
-     * \param reduced Whether to remove the circle from the to-location because it will be included in the next extrusion move
-     * 
-     * Overrides class field is_reduced
+     * Converts this segment to an outline of the area that the segment covers.
+     * \param reduced Whether to remove the circle from the to-location because
+     * it will be included in the next extrusion move. Overrides class field
+     * \ref is_reduced .
      */
     Polygons toPolygons(bool reduced);
 
+    /*!
+     * Discretize a variable-line-width extrusion segment into multiple
+     * constant-line-width extrusion segments.
+     *
+     * The line segments will have the length of approximately the step size. If
+     * this doesn't fit, the number of steps is rounded to the nearest integer,
+     * and the steps may be slightly longer or shorter.
+     * \param step_size The desired length of the steps. A smaller step size
+     * gives better width resolution, but increases the number of segments,
+     * resulting in longer processing and a larger file in the end.
+     * \return A list of extrusion segments. All of these extrusion segments
+     * have a constant line width.
+     */
     std::vector<ExtrusionSegment> discretize(coord_t step_size);
 };
 
