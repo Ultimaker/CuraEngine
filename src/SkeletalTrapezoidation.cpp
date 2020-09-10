9b892423f6f0b3b4e788e2d4942aab7879c65051
Jelle Spijker <j.spijker@ultimaker.com>
2020-09-10 15:08:40 +0200

    Applied new using aliases ToolPaths and BinJunctions
    CURA-7626_infill_walls_with_libArachne

--- src/SkeletalTrapezoidation.cpp
+++ src/SkeletalTrapezoidation.cpp
@@ -471,7 +471,7 @@ void SkeletalTrapezoidation::separatePointyQuadEndNodes()
 // vvvvvvvvvvvvvvvvvvvvv
 //
 
-void SkeletalTrapezoidation::generateToolpaths(WallToolPaths& generated_toolpaths, bool filter_outermost_central_edges)
+void SkeletalTrapezoidation::generateToolpaths(ToolPaths& generated_toolpaths, bool filter_outermost_central_edges)
 {
     p_generated_toolpaths = &generated_toolpaths;
 
@@ -1728,7 +1728,7 @@ void SkeletalTrapezoidation::addToolpathSegment(const ExtrusionJunction& from, c
 {
     if (from == to) return;
 
-    WallToolPaths& generated_toolpaths = *p_generated_toolpaths;
+    ToolPaths& generated_toolpaths = *p_generated_toolpaths;
 
     size_t inset_idx = from.perimeter_index;
     if (inset_idx >= generated_toolpaths.size())
@@ -1858,7 +1858,7 @@ void SkeletalTrapezoidation::connectJunctions(ptr_vector_t<LineJunctions>& edge_
 
 void SkeletalTrapezoidation::generateLocalMaximaSingleBeads()
 {
-    WallToolPaths& generated_toolpaths = *p_generated_toolpaths;
+    ToolPaths& generated_toolpaths = *p_generated_toolpaths;
 
     for (auto& node : graph.nodes)
     {
