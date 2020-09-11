8924bc955e32a0014c57a74d01927dc2a787bd97
Jelle Spijker <j.spijker@ultimaker.com>
2020-09-11 09:36:56 +0200

    Fixed type mismatch in BeadingStrategyFactory
    makeStrategy max_bead_count was defined as int because the default value
    was -1 while all consumers of this variable accepted a const coord_t.
    
    CURA-7626

--- src/BeadingStrategy/BeadingStrategyFactory.h
+++ src/BeadingStrategy/BeadingStrategyFactory.h
@@ -40,7 +40,7 @@ std::string to_string(StrategyType type);
 class BeadingStrategyFactory
 {
 public:
-    static BeadingStrategy* makeStrategy(StrategyType type, coord_t preferred_bead_width = MM2INT(0.5), coord_t preferred_transition_length = 400, float transitioning_angle = M_PI / 4, const std::unique_ptr<coord_t>& min_bead_width = nullptr, const std::unique_ptr<coord_t>& min_feature_size = nullptr, int max_bead_count = -1);
+    static BeadingStrategy* makeStrategy(StrategyType type, coord_t preferred_bead_width = MM2INT(0.5), coord_t preferred_transition_length = 400, float transitioning_angle = M_PI / 4, const std::unique_ptr<coord_t>& min_bead_width = nullptr, const std::unique_ptr<coord_t>& min_feature_size = nullptr,  const coord_t max_bead_count = 0);
 };
 
 } // namespace cura
