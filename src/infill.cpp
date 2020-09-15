3b43ae0582ba181eaa0ee15bfbbd3787f55d310e
Jelle Spijker <j.spijker@ultimaker.com>
2020-09-15 18:22:24 +0200

    Fix infill multiplier > 3 for unconnected lines.
    When Connect Infill Lines was unchecked the multiplier didn't get the
    correct number of offsets. A global flag was introduced mirror_offset
    which ensures that the extra infill line offsets are generated in the
    correct direction.
    This is now only done for infill multiplier values higher then 3.

--- src/infill.cpp
+++ src/infill.cpp
@@ -201,14 +201,19 @@ void Infill::multiplyInfill(Polygons& result_polygons, Polygons& result_lines)
         }
     }
     result.add(first_offset);
-    Polygons reference_polygons = first_offset;
-    for (size_t infill_line = 1; infill_line < infill_multiplier / 2; infill_line++) // 2 because we are making lines on both sides at the same time
+    if (infill_multiplier > 3)
     {
-        Polygons extra_offset = reference_polygons.offset(-infill_line_width);
-        result.add(extra_offset);
-        reference_polygons = std::move(extra_offset);
-    }
+        Polygons reference_polygons = first_offset;
+        const size_t multiplier = static_cast<size_t>(infill_multiplier / 2);
 
+        const int extra_offset = mirror_offset ? -infill_line_width : infill_line_width;
+        for (size_t infill_line = 1; infill_line < multiplier; ++infill_line)
+        {
+            Polygons extra_polys = reference_polygons.offset(extra_offset);
+            result.add(extra_polys);
+            reference_polygons = std::move(extra_polys);
+        }
+    }
     if (zig_zaggify)
     {
         result = result.intersection(outline);
