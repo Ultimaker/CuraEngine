e6517200b228f1d92b62f6dee33d48b13dddb6d8
Remco Burema <r.burema@ultimaker.com>
2020-11-13 20:49:19 +0100

    Made a meta-beading-strat. that takes outer and inner wall widths into account.
    The idea is as follows: Initially, for the parent (base) beading strategy, set the 'optimum' width as a weighted average defined by 'what the total thickness of the fill would be if max-bead width is always reached' (so take the situation where the actually requested total wall width is reached as the ground truth we base all the rest on). The base beading-strategy will thus produce the 'right' number of walls but the wrong bead-widths (and locations) -- becasue it assumes the walls are some average. This is then rectified by the newly introduced meta-strategy, which redistributes the bead-widths. It takes a look at the factor of the thickness the outer and inner walls would have occupied in the ideal situation and applies that to the current situation. This keeps a) the original beadings simple to understand and debug b) the meanings and behaviour of the original beadings intact while still c) preserving the widths of the outer and inner walls the user (settings) has/have input at the very least in the case of 'full' walls (and does a decent attempt at at least preserving the relative relation between those two values with a lesser thickness).
    
    part of CURA-7597

--- src/BeadingStrategy/RedistributeBeadingStrategy.cpp
+++ src/BeadingStrategy/RedistributeBeadingStrategy.cpp
@@ -5,12 +5,63 @@
 
 namespace cura
 {
+    coord_t countPositive(const std::vector<coord_t>& bead_widths)
+    {
+        coord_t ret = 0;
+        for (const auto& width : bead_widths)
+        {
+            ret += width > 0 ? 1 : 0;
+        }
+        return ret;
+    }
 
     BeadingStrategy::Beading RedistributeBeadingStrategy::compute(coord_t thickness, coord_t bead_count) const
     {
         Beading ret = parent->compute(thickness, bead_count);
+        bead_count = countPositive(ret.bead_widths); // Don't count any zero-width 'signalling' walls.
+        thickness -= ret.left_over;
+        if (bead_count < 3)
+        {
+            return ret;
+        }
 
-        // TODO!
+        const coord_t current_total_outer_walls_width = ret.bead_widths.front() + ret.bead_widths.back();
+        const coord_t current_total_inner_walls_width = thickness - current_total_outer_walls_width;
+        const float current_outer_factor = current_total_outer_walls_width / static_cast<float>(thickness);
+        const float current_inner_factor = current_total_inner_walls_width / static_cast<float>(thickness);
+
+        const coord_t optimal_total_outer_walls_width = optimal_width_outer * 2;
+        const coord_t optimal_total_inner_walls_width = optimal_width_inner * (bead_count - 2);
+        const float optimal_outer_factor = optimal_total_outer_walls_width / static_cast<float>(optimal_total_outer_walls_width + optimal_total_inner_walls_width);
+        const float optimal_inner_factor = optimal_total_inner_walls_width / static_cast<float>(optimal_total_outer_walls_width + optimal_total_inner_walls_width);
+
+        const float outer_factor = optimal_outer_factor / current_outer_factor;
+        const float inner_factor = optimal_inner_factor / current_inner_factor;
+
+        ret.bead_widths[0] *= outer_factor;
+        for (coord_t i_width = 1; i_width < (bead_count - 1); ++i_width)
+        {
+            ret.bead_widths[i_width] *= inner_factor;
+        }
+        ret.bead_widths[bead_count - 1] *= outer_factor;
+
+        coord_t last_coord = 0;
+        coord_t last_width = 0;
+        for (coord_t i_location = 0; i_location < bead_count / 2; ++i_location)
+        {
+            ret.toolpath_locations[i_location] = last_coord + (last_width + ret.bead_widths[i_location]) / 2;
+            last_coord = ret.toolpath_locations[i_location];
+            last_width = ret.bead_widths[i_location];
+        }
+
+        last_coord = thickness;
+        last_width = 0;
+        for (coord_t i_location = bead_count - 1; i_location >= bead_count - (bead_count / 2); --i_location)
+        {
+            ret.toolpath_locations[i_location] = last_coord - (last_width + ret.bead_widths[i_location]) / 2;
+            last_coord = ret.toolpath_locations[i_location];
+            last_width = ret.bead_widths[i_location];
+        }
 
         return ret;
     }
