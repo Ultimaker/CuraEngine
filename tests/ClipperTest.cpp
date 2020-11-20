eb22a28125ccdfb2f4fa82b68ca17fca165f97a1
Tim Kuipers <t.kuipers@ultimaker.com>
2020-11-20 11:24:16 +0100

    make ClipperTest into a proper gtest

--- tests/ClipperTest.cpp
+++ tests/ClipperTest.cpp
@@ -3,10 +3,12 @@
 
 #include <gtest/gtest.h>
 
-#include "../src/utils/polygon.h"
+#include "clipper.hpp"
+#include <math.h>
 
 // #define TEST_INFILL_SVG_OUTPUT
 #ifdef TEST_INFILL_SVG_OUTPUT
+#include "../src/utils/polygon.h"
 #include <cstdlib>
 #include "../src/utils/SVG.h"
 #endif //TEST_INFILL_SVG_OUTPUT
@@ -17,7 +19,13 @@ namespace cura
 class ClipperTest : public testing::Test
 {
 public:
+    using Paths = ClipperLib::Paths;
+    using Path = ClipperLib::Path;
+    using coord_t = ClipperLib::cInt;
+    using Point = ClipperLib::IntPoint;
 
+    std::vector<Paths> all_outlines;
+    std::vector<Paths> all_polylines;
 
     ClipperTest()
     {
@@ -25,110 +33,123 @@ public:
 
     void SetUp()
     {
+        
+        {
+            all_outlines.emplace_back();
+            all_outlines.back().emplace_back();
+            Path& outline = all_outlines.back().back();
+            outline.emplace_back(1100, 500);
+            outline.emplace_back(500, 500);
+            outline.emplace_back(500, 400);
+            outline.emplace_back(1100, 400);
+            
+            all_polylines.emplace_back();
+            all_polylines.back().emplace_back();
+            Path& polyline = all_polylines.back().back();
+            polyline.emplace_back(1075, 425);
+            polyline.emplace_back(672, 425 );
+            polyline.emplace_back(651, 425 );
+            polyline.emplace_back(595, 424 );
+            polyline.emplace_back(617, 437 );
+        }
+        {
+            all_outlines.emplace_back();
+            all_outlines.back().emplace_back();
+            Path& outline = all_outlines.back().back();
+            outline.emplace_back(1000, 1100);
+            outline.emplace_back(300, 1100);
+            outline.emplace_back(200, 1000);
+            outline.emplace_back(1000, 1000);
+            
+            all_polylines.emplace_back();
+            all_polylines.back().emplace_back();
+            Path& polyline = all_polylines.back().back();
+            polyline.emplace_back(992, 1050);
+            polyline.emplace_back(617, 1049);
+            polyline.emplace_back(660, 1074);
+            polyline.emplace_back(617, 1049);
+            polyline.emplace_back(582, 1049);
+            polyline.emplace_back(538, 1074);
+        }
     }
 
     void TearDown()
     {
     }
+    
+    static Paths intersectPolylines(const Paths& outlines, const Paths& polylines);
+
+    static coord_t sq(coord_t i) { return i*i; };
+
+    static coord_t calculateLength(const Paths& polylines);
+
+    static void outputSVG(const Paths& outlines, const Paths& polylines, const Paths& intersected, const char* filename);
 };
 
-TEST_F(ClipperTest, PolylinesTest)
+ClipperTest::Paths ClipperTest::intersectPolylines(const Paths& outlines, const Paths& polylines)
 {
-
-    auto sq = [](int i) { return i*i; };
-    
-    ClipperLib::Paths outlines;
-    {
-        outlines.emplace_back();
-        ClipperLib::Path& outline = outlines.back();
-        outline.emplace_back(1100, 500);
-        outline.emplace_back(500, 500);
-        outline.emplace_back(500, 400);
-        outline.emplace_back(1100, 400);
-        
-//         outline.emplace_back(1000, 1100);
-//         outline.emplace_back(300, 1100);
-//         outline.emplace_back(200, 1000);
-//         outline.emplace_back(1000, 1000);
-    }
-    
-    ClipperLib::Paths polylines;
-    {
-        polylines.emplace_back();
-        ClipperLib::Path& polyline = polylines.back();
-        polyline.emplace_back(1075, 425);
-        polyline.emplace_back(672, 425 );
-        polyline.emplace_back(651, 425 );
-        polyline.emplace_back(595, 424 );
-        polyline.emplace_back(617, 437 );
-        // old ^
-        
-        
-//         polyline.emplace_back(992, 1050);
-//         polyline.emplace_back(617, 1049);
-//         polyline.emplace_back(660, 1074);
-//         polyline.emplace_back(617, 1049);
-//         polyline.emplace_back(582, 1049);
-//         polyline.emplace_back(538, 1074);
-    }
     ClipperLib::PolyTree result_tree;
     ClipperLib::Clipper clipper(0);
     clipper.AddPaths(polylines, ClipperLib::ptSubject, false);
     clipper.AddPaths(outlines, ClipperLib::ptClip, true);
     clipper.Execute(ClipperLib::ctIntersection, result_tree);
-    ClipperLib::Paths result;
+    Paths result;
     ClipperLib::PolyTreeToPaths(result_tree, result);
-
-    coord_t original_polyline_length = 0;
-    for (ClipperLib::Path path : polylines)
-    {
-        Point* last = nullptr;
-        for (Point& p : path)
-        {
-            if (last)
-            {
-                original_polyline_length += std::sqrt(sq(p.X - last->X) + sq(p.Y - last->Y));
-            }
-            last = &p;
-        }
-    }
-
-    coord_t intersected_polyline_length = 0;
-    for (ClipperLib::Path path : result)
-    {
-        Point* last = nullptr;
-        for (Point& p : path)
-        {
-            if (last)
-            {
-                intersected_polyline_length += std::sqrt(sq(p.X - last->X) + sq(p.Y - last->Y));
-            }
-            last = &p;
-        }
-    }
-
-    
-    
-#ifdef TEST_INFILL_SVG_OUTPUT
-    {
-        Polygons outs;
-        outs.paths = outlines;
-        Polygons lines;
-        lines.paths = polylines;
-        Polygons lines_after;
-        lines_after.paths = result;
-        SVG svg("/tmp/problem.svg", AABB(outs));
-        svg.writePolygons(outs, SVG::Color::BLACK);
-        svg.nextLayer();
-        svg.writePolylines(lines_after, SVG::Color::RED);
-        svg.nextLayer();
-        svg.writePolylines(lines, SVG::Color::GREEN);
-    }
-#endif // TEST_INFILL_SVG_OUTPUT
-    
-    
-    assert(intersected_polyline_length == original_polyline_length);
+    return result;
 }
+
+ClipperTest::coord_t ClipperTest::calculateLength(const Paths& polylines)
+{
+    coord_t polyline_length = 0;
+    for (const Path& path : polylines)
+    {
+        const Point* last = nullptr;
+        for (const Point& p : path)
+        {
+            if (last)
+            {
+                polyline_length += std::sqrt(sq(p.X - last->X) + sq(p.Y - last->Y));
+            }
+            last = &p;
+        }
+    }
+    return polyline_length;
+}
+
+
+TEST_F(ClipperTest, PolylinesTest1)
+{
+    Paths intersected = intersectPolylines(all_outlines[0], all_polylines[0]);
+    outputSVG(all_outlines[0], all_polylines[0], intersected, "/tmp/clipper_test_0");
+    ASSERT_EQ(calculateLength(intersected), calculateLength(all_polylines[0]));
+}
+
+TEST_F(ClipperTest, PolylinesTest2)
+{
+    Paths intersected = intersectPolylines(all_outlines[1], all_polylines[1]);
+    outputSVG(all_outlines[1], all_polylines[1], intersected, "/tmp/clipper_test_1");
+    ASSERT_EQ(calculateLength(intersected), calculateLength(all_polylines[1]));
+}
+
+    
+    
+void ClipperTest::outputSVG(const Paths& outlines, const Paths& polylines, const Paths& intersected, const char* filename)
+{
+#ifdef TEST_INFILL_SVG_OUTPUT
+    Polygons outs;
+    outs.set(outlines);
+    Polygons lines;
+    lines.set(polylines);
+    Polygons lines_after;
+    lines_after.set(intersected);
+    SVG svg(filename, AABB(outs));
+    svg.writePolygons(outs, SVG::Color::BLACK);
+    svg.nextLayer();
+    svg.writePolylines(lines_after, SVG::Color::RED);
+    svg.nextLayer();
+    svg.writePolylines(lines, SVG::Color::GREEN);
+#endif // TEST_INFILL_SVG_OUTPUT
+}    
 
 
 } //namespace cura
