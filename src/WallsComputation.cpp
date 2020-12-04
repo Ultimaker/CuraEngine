1703faa79185098eb3c2c8fd21c1e0f7ee73b1e7
Kostas Karmas <konskarm@gmail.com>
2020-12-04 17:14:44 +0100

    Apply the outer wall inset only on the normal walls
    CURA-7830

--- src/WallsComputation.cpp
+++ src/WallsComputation.cpp
@@ -126,7 +126,7 @@ void WallsComputation::generateInsets(SliceLayerPart* part)
     }
 
     // Call on libArachne:
-    WallToolPaths wall_tool_paths(part->outline, line_width_0, line_width_x, inset_count, settings);
+    WallToolPaths wall_tool_paths(part->outline, line_width_0, line_width_x, inset_count, settings, wall_0_inset);
     part->wall_toolpaths = wall_tool_paths.getToolPaths();
     part->inner_area = wall_tool_paths.getInnerContour();
 }
