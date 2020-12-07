75d0b9b8a64c6305a9ca488f9c66baac796ea989
Ghostkeeper <rubend@tutanota.com>
2020-12-07 17:13:03 +0100

    Print all parts when spiralising
    Other parts are not printed with the spiralize routine, as explained here, but they are still printed.
    
    Contributes to issue CURA-7874.

--- src/FffGcodeWriter.cpp
+++ src/FffGcodeWriter.cpp
@@ -1967,17 +1967,22 @@ bool FffGcodeWriter::processInsets(const SliceDataStorage& storage, LayerPlan& g
         gcode_layer.setOverhangMask(Polygons());
     }
 
-    // Only spiralize the first part in the mesh, any other parts will be printed using the normal, non-spiralize codepath.
-    // This sounds weird but actually does the right thing when you have a model that has multiple parts at the bottom that merge into
-    // one part higher up. Once all the parts have merged, layers above that level will be spiralized
-    if (spiralize && &mesh.layers[gcode_layer.getLayerNr()].parts[0] == &part)
+    if(spiralize && extruder_nr == mesh.settings.get<ExtruderTrain&>("wall_0_extruder_nr").extruder_nr && !part.spiral_wall.empty())
     {
-        if (!part.spiral_wall.empty() && extruder_nr == mesh.settings.get<ExtruderTrain&>("wall_0_extruder_nr").extruder_nr)
+        // Only spiralize the first part in the mesh, any other parts will be printed using the normal, non-spiralize codepath.
+        // This sounds weird but actually does the right thing when you have a model that has multiple parts at the bottom that merge into
+        // one part higher up. Once all the parts have merged, layers above that level will be spiralized
+        if(&mesh.layers[gcode_layer.getLayerNr()].parts[0] == &part)
         {
             added_something = true;
             setExtruder_addPrime(storage, gcode_layer, extruder_nr);
             gcode_layer.setIsInside(true); // going to print stuff inside print object
             processSpiralizedWall(storage, gcode_layer, mesh_config, part, mesh);
+        }
+        else
+        {
+            //Print the spiral walls of other parts as single walls without Z gradient.
+            gcode_layer.addWalls(part.spiral_wall, mesh, mesh_config.inset0_config, mesh_config.inset0_config);
         }
     }
     else
