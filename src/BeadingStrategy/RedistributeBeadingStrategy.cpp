21c93de9a333d02a51e85eb14fadc4023663f6ed
Jelle Spijker <j.spijker@ultimaker.com>
2021-02-10 16:46:26 +0100

    Rewrote outer wall lock factor usage.
    CURA-7979_fix_outer_wall_artifacts

--- src/BeadingStrategy/RedistributeBeadingStrategy.cpp
+++ src/BeadingStrategy/RedistributeBeadingStrategy.cpp
@@ -4,6 +4,7 @@
 #include "RedistributeBeadingStrategy.h"
 
 #include <algorithm>
+#include <numeric>
 
 namespace cura
 {
@@ -40,62 +41,66 @@ namespace cura
             return ret;
         }
 
-        // Calculate the factors with which to multiply the outer and inner walls:
-        const coord_t current_total_outer_walls_width = ret.bead_widths.front() + ret.bead_widths.back();
-        const coord_t current_total_inner_walls_width = thickness - current_total_outer_walls_width;
-        const coord_t optimal_total_outer_walls_width = optimal_width_outer * 2;
-        const coord_t optimal_total_inner_walls_width = optimal_width_inner * (bead_count - 2);
+        // Determine outer bead ratio
+        const coord_t outer_width = ret.bead_widths.front() + ret.bead_widths.back();
+        const coord_t outer_width_optimal = optimal_width_outer * 2;
+        const coord_t outer_width_deviation = static_cast<coord_t>(static_cast<double>(outer_width_optimal - outer_width) * outer_wall_lock_factor + 0.5);
+        const coord_t outer_width_required = outer_width + outer_width_deviation;
+        const Ratio outer_bead_ratio = static_cast<double>(outer_width_required) / static_cast<double>(outer_width);
 
-        // ... do some compensation for the 'outer wall lock factor' in between the calculations:
-        // NOTE: The weights are inverted here, since these widhts end up in the denominator.
-        const coord_t optimal_total_thickness = static_cast<coord_t>(outer_wall_lock_factor * thickness + outer_wall_lock_inverse * (optimal_total_outer_walls_width + optimal_total_inner_walls_width));
-
-        const coord_t outer_factor_numerator = optimal_total_outer_walls_width * thickness;
-        const coord_t outer_factor_denominator = current_total_outer_walls_width * optimal_total_thickness;
-        const coord_t inner_factor_numerator = optimal_total_inner_walls_width * thickness;
-        const coord_t inner_factor_denominator = current_total_inner_walls_width * optimal_total_thickness;
-
-        // Multiply the bead-widths with the right factors:
-        ret.bead_widths[0] = (ret.bead_widths[0] * outer_factor_numerator) / outer_factor_denominator;
-        for (coord_t i_width = 1; i_width < (bead_count - 1); ++i_width)
+        if (outer_bead_ratio != 1._r) // Only update if needed
         {
-            ret.bead_widths[i_width] = (ret.bead_widths[i_width] * inner_factor_numerator) / inner_factor_denominator;
+            // Update outer beads
+            ret.bead_widths.front() = static_cast<coord_t>(static_cast<double>(ret.bead_widths.front()) * outer_bead_ratio + 0.5);
+            ret.bead_widths.back() = static_cast<coord_t>(static_cast<double>(ret.bead_widths.back()) * outer_bead_ratio + 0.5);
+            // TODO: validate size
+
+            // Determine inner bead ratio
+            auto inner_bead_width_begin = std::next(ret.bead_widths.begin());
+            auto inner_bead_width_end = std::prev(ret.bead_widths.end());
+            const coord_t total_inner_bead_width = std::accumulate(inner_bead_width_begin, inner_bead_width_end, static_cast<coord_t>(0));
+            const coord_t total_required_inner_bead_width = thickness - ret.bead_widths.front() - ret.bead_widths.back();
+            const Ratio inner_bead_ratio = static_cast<double>(total_required_inner_bead_width) / static_cast<double>(total_inner_bead_width);
+
+            for (auto bead_width_it = inner_bead_width_begin; bead_width_it < inner_bead_width_end; bead_width_it++)
+            {
+                *bead_width_it = static_cast<coord_t>(static_cast<double>(*bead_width_it) * inner_bead_ratio + 0.5);
+                // TODO: validate Size
+            }
+
+            // TODO: insert inner_bead if needed
+
+            // TODO: lock position of outer wall
+
+            // Update the first half of the toolpath-locations with the updated bead-widths (starting from 0, up to half):
+            coord_t last_coord = 0;
+            coord_t last_width = 0;
+            for (coord_t i_location = 0; i_location < bead_count / 2; ++i_location)
+            {
+                ret.toolpath_locations[i_location] = last_coord + (last_width + ret.bead_widths[i_location]) / 2;
+                last_coord = ret.toolpath_locations[i_location];
+                last_width = ret.bead_widths[i_location];
+            }
+
+            // Handle the position of any middle wall (note that the width will already have been set correctly):
+            if (bead_count % 2 == 1)
+            {
+                ret.toolpath_locations[bead_count / 2] = thickness / 2;
+            }
+
+            // Update the last half of the toolpath-locations with the updated bead-widths (starting from thickness, down to half):
+            last_coord = thickness;
+            last_width = 0;
+            for (coord_t i_location = bead_count - 1; i_location >= bead_count - (bead_count / 2); --i_location)
+            {
+                ret.toolpath_locations[i_location] = last_coord - (last_width + ret.bead_widths[i_location]) / 2;
+                last_coord = ret.toolpath_locations[i_location];
+                last_width = ret.bead_widths[i_location];
+            }
+
+            ret.left_over = thickness - std::accumulate(ret.bead_widths.cbegin(), ret.bead_widths.cend(), static_cast<coord_t>(0));
         }
-        ret.bead_widths[bead_count - 1] = (ret.bead_widths[bead_count - 1] * outer_factor_numerator) / outer_factor_denominator;
-
-        // Filter out any non-positive width walls:
-        ret.bead_widths.erase(std::remove_if(ret.bead_widths.begin(), ret.bead_widths.end(), [](const coord_t width){ return width <= 0; }), ret.bead_widths.end());
-        bead_count = ret.bead_widths.size();
-        ret.toolpath_locations.resize(bead_count);
-
-        // Update the first half of the toolpath-locations with the updated bead-widths (starting from 0, up to half):
-        coord_t last_coord = 0;
-        coord_t last_width = 0;
-        for (coord_t i_location = 0; i_location < bead_count / 2; ++i_location)
-        {
-            ret.toolpath_locations[i_location] = last_coord + (last_width + ret.bead_widths[i_location]) / 2;
-            last_coord = ret.toolpath_locations[i_location];
-            last_width = ret.bead_widths[i_location];
-        }
-
-        // Handle the position of any middle wall (note that the width will already have been set correctly):
-        if (bead_count % 2 == 1)
-        {
-            ret.toolpath_locations[bead_count / 2] = thickness / 2;
-        }
-
-        // Update the last half of the toolpath-locations with the updated bead-widths (starting from thickness, down to half):
-        last_coord = thickness;
-        last_width = 0;
-        for (coord_t i_location = bead_count - 1; i_location >= bead_count - (bead_count / 2); --i_location)
-        {
-            ret.toolpath_locations[i_location] = last_coord - (last_width + ret.bead_widths[i_location]) / 2;
-            last_coord = ret.toolpath_locations[i_location];
-            last_width = ret.bead_widths[i_location];
-        }
-
-        ret.left_over = std::max(static_cast<coord_t>(0), thickness - (ret.toolpath_locations.back() + ret.bead_widths.back() / 2));
         return ret;
     }
 
-} // namespace cura
+    } // namespace cura
