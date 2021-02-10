5582c239ad0f9b793c38553a540fb597d91707be
Jelle Spijker <j.spijker@ultimaker.com>
2021-02-10 17:01:53 +0100

    Calculate weights only once
    The weights were calculated twice, first to determine the total weight,
    then to determine the actual weight of the bead. These where a lot of
    unnecessary multiplications. Stored the weight in a vector, summed it up
    and called upon the weight later on.
    
    Contributes to CURA-7979

--- src/BeadingStrategy/InwardDistributedBeadingStrategy.cpp
+++ src/BeadingStrategy/InwardDistributedBeadingStrategy.cpp
@@ -1,6 +1,8 @@
 //Copyright (c) 2020 Ultimaker B.V.
 //CuraEngine is released under the terms of the AGPLv3 or higher.
 
+#include <numeric>
+
 #include "InwardDistributedBeadingStrategy.h"
 
 namespace cura
@@ -14,7 +16,6 @@ namespace cura
         if (bead_count > 0)
         {
             const coord_t to_be_divided = thickness - bead_count * optimal_width;
-            float total_weight = 0;
             const float middle = static_cast<float>(bead_count - 1) / 2;
 
             const auto getWeight = [middle, this](coord_t bead_idx)
@@ -23,14 +24,19 @@ namespace cura
                 return std::max(0.0f, 1.0f - one_over_distribution_radius_squared * dev_from_middle * dev_from_middle);
             };
 
+            std::vector<float> weights;
+            weights.resize(bead_count);
             for (coord_t bead_idx = 0; bead_idx < bead_count; bead_idx++)
             {
-                total_weight += getWeight(bead_idx);
+                weights[bead_idx] = getWeight(bead_idx);
             }
 
+            const float total_weight = std::accumulate(weights.cbegin(), weights.cend(), 0.f);
             for (coord_t bead_idx = 0; bead_idx < bead_count; bead_idx++)
             {
-                const coord_t width = optimal_width + to_be_divided * getWeight(bead_idx) / total_weight;
+                const float weight_fraction = weights[bead_idx] / total_weight;
+                const coord_t splitup_left_over_weight = to_be_divided * weight_fraction;
+                const coord_t width = optimal_width + splitup_left_over_weight;
                 if (bead_idx == 0)
                 {
                     ret.toolpath_locations.emplace_back(width / 2);
