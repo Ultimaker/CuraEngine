ea8261f79f0b2809833c8c885560c7dd4deaef41
Ghostkeeper <rubend@tutanota.com>
2021-07-01 17:12:15 +0200

    Add test for getting the optimal width for a number of beads
    Contributes to issue CURA-8285.

--- tests/beading_strategy/CenterDeviationBeadingStrategyTest.cpp
+++ tests/beading_strategy/CenterDeviationBeadingStrategyTest.cpp
@@ -24,4 +24,19 @@ TEST(CenterDeviationBeadingStrategy, Construction)
     EXPECT_EQ(strategy.minimum_line_width, 400) << "Since the transition threshold was 100%, the minimum line width equals the preferred width.";
 }
 
+/*!
+ * Tests getting the optimal thickness with Center Deviation.
+ *
+ * This should simply be a multiplication of the line width, when using Center
+ * Deviation. It doesn't adjust the line widths by itself.
+ */
+TEST(CenterDeviationBeadingStrategy, GetOptimalThickness)
+{
+    constexpr coord_t line_width = 400;
+    CenterDeviationBeadingStrategy strategy(line_width, 0.6, 0.5);
+    EXPECT_EQ(strategy.getOptimalThickness(0), 0) << "With 0 beads, you'll fill 0 space.";
+    EXPECT_EQ(strategy.getOptimalThickness(1), line_width) << "With 1 bead, optimally you'll want to print that bead at the optimal line width.";
+    EXPECT_EQ(strategy.getOptimalThickness(4), 4 * line_width) << "With 4 beads, optimally fill 4 line widths.";
+}
+
 }
