6fd745e5b789e85f82ea31788a79cbe24ff47e10
Ghostkeeper <rubend@tutanota.com>
2021-08-06 16:59:43 +0200

    Add more test paths to test with
    With varying flow rates and speeds.
    
    Contributes to issue CURA-7279.

--- tests/ExtruderPlanTest.cpp
+++ tests/ExtruderPlanTest.cpp
@@ -26,6 +26,24 @@ public:
     std::vector<GCodePath> lines;
 
     /*!
+     * Three lines side by side with travel moves in between, but adjusted flow.
+     *
+     * The first line gets 120% flow.
+     * The second line gets 80% flow.
+     * The third line gets 40% flow.
+     */
+    std::vector<GCodePath> decreasing_flow;
+
+    /*!
+     * Three lines side by side with their speed factors adjusted.
+     *
+     * The first line gets 120% speed.
+     * The second line gets 80% speed.
+     * The third line gets 40% speed.
+     */
+    std::vector<GCodePath> decreasing_speed;
+
+    /*!
      * Configuration to print extruded paths with in the fixture.
      *
      * This config is referred to via pointer, so changing it will immediately
@@ -60,8 +78,8 @@ public:
         const std::string mesh_id = "test_mesh";
         constexpr Ratio flow_1 = 1.0_r;
         constexpr bool no_spiralize = false;
-        constexpr Ratio speed_factor_1 = 1.0_r;
-        square.assign({GCodePath(extrusion_config, mesh_id, SpaceFillType::PolyLines, flow_1, no_spiralize, speed_factor_1)});
+        constexpr Ratio speed_1 = 1.0_r;
+        square.assign({GCodePath(extrusion_config, mesh_id, SpaceFillType::PolyLines, flow_1, no_spiralize, speed_1)});
         square.back().points = {
             Point(0, 0),
             Point(1000, 0),
@@ -71,17 +89,49 @@ public:
         };
 
         lines.assign({
-            GCodePath(extrusion_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_factor_1),
-            GCodePath(travel_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_factor_1),
-            GCodePath(extrusion_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_factor_1),
-            GCodePath(travel_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_factor_1),
-            GCodePath(extrusion_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_factor_1)
+            GCodePath(extrusion_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_1),
+            GCodePath(travel_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_1),
+            GCodePath(extrusion_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_1),
+            GCodePath(travel_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_1),
+            GCodePath(extrusion_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_1)
         });
         lines[0].points = {Point(0, 0), Point(1000, 0)};
         lines[1].points = {Point(1000, 0), Point(1000, 400)};
         lines[2].points = {Point(1000, 400), Point(0, 400)};
         lines[3].points = {Point(0, 400), Point(0, 800)};
         lines[4].points = {Point(0, 800), Point(1000, 800)};
+
+        constexpr Ratio flow_12 = 1.2_r;
+        constexpr Ratio flow_08 = 0.8_r;
+        constexpr Ratio flow_04 = 0.4_r;
+        decreasing_flow.assign({
+            GCodePath(extrusion_config, mesh_id, SpaceFillType::Lines, flow_12, no_spiralize, speed_1),
+            GCodePath(travel_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_1),
+            GCodePath(extrusion_config, mesh_id, SpaceFillType::Lines, flow_08, no_spiralize, speed_1),
+            GCodePath(travel_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_1),
+            GCodePath(extrusion_config, mesh_id, SpaceFillType::Lines, flow_04, no_spiralize, speed_1)
+        });
+        decreasing_flow[0].points = {Point(0, 0), Point(1000, 0)};
+        decreasing_flow[1].points = {Point(1000, 0), Point(1000, 400)};
+        decreasing_flow[2].points = {Point(1000, 400), Point(0, 400)};
+        decreasing_flow[3].points = {Point(0, 400), Point(0, 800)};
+        decreasing_flow[4].points = {Point(0, 800), Point(1000, 800)};
+
+        constexpr Ratio speed_12 = 1.2_r;
+        constexpr Ratio speed_08 = 0.8_r;
+        constexpr Ratio speed_04 = 0.4_r;
+        decreasing_speed.assign({
+            GCodePath(extrusion_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_12),
+            GCodePath(travel_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_1),
+            GCodePath(extrusion_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_08),
+            GCodePath(travel_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_1),
+            GCodePath(extrusion_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_04)
+        });
+        decreasing_speed[0].points = {Point(0, 0), Point(1000, 0)};
+        decreasing_speed[1].points = {Point(1000, 0), Point(1000, 400)};
+        decreasing_speed[2].points = {Point(1000, 400), Point(0, 400)};
+        decreasing_speed[3].points = {Point(0, 400), Point(0, 800)};
+        decreasing_speed[4].points = {Point(0, 800), Point(1000, 800)};
     }
 };
 
@@ -133,7 +183,9 @@ TEST_P(ExtruderPlanPathsParameterizedTest, BackPressureCompensationZeroIsUncompe
 
 INSTANTIATE_TEST_CASE_P(ExtruderPlanTestInstantiation, ExtruderPlanPathsParameterizedTest, testing::Values(
         ExtruderPlanTestPathCollection().square,
-        ExtruderPlanTestPathCollection().lines
+        ExtruderPlanTestPathCollection().lines,
+        ExtruderPlanTestPathCollection().decreasing_flow,
+        ExtruderPlanTestPathCollection().decreasing_speed
 ));
 
 }
