88114d5d063517df0502e221fb217c6dce10abb4
Ghostkeeper <rubend@tutanota.com>
2021-08-06 16:32:00 +0200

    Add lines paths to test with
    A bit of a different one since it's got:
    * Multiple paths in the vector.
    * Travel moves in between.
    
    Contributes to issue CURA-7279.

--- tests/ExtruderPlanTest.cpp
+++ tests/ExtruderPlanTest.cpp
@@ -23,17 +23,30 @@ public:
     std::vector<GCodePath> square;
 
     /*!
+     * Three lines side by side, with two travel moves in between.
+     */
+    std::vector<GCodePath> lines;
+
+    /*!
      * An extruder plan that can be used as a victim for testing.
      */
     ExtruderPlan extruder_plan;
 
     /*!
-     * Configuration to print all paths with in the fixture.
+     * Configuration to print extruded paths with in the fixture.
      *
      * This config is referred to via pointer, so changing it will immediately
-     * change it for all paths.
+     * change it for all extruded paths.
      */
-    GCodePathConfig path_config;
+    GCodePathConfig extrusion_config;
+
+    /*!
+     * Configuration to print travel paths with in the fixture.
+     *
+     * This config is referred to via pointer, so changing it will immediately
+     * change it for all travel paths.
+     */
+    GCodePathConfig travel_config;
 
     ExtruderPlanTest() :
         extruder_plan(
@@ -45,22 +58,26 @@ public:
             FanSpeedLayerTimeSettings(),
             RetractionConfig()
         ),
-        path_config(
+        extrusion_config(
             PrintFeatureType::OuterWall,
             /*line_width=*/400,
-            100,//extruder_plan.layer_thickness,
+            extruder_plan.layer_thickness,
             /*flow=*/1.0_r,
             GCodePathConfig::SpeedDerivatives(50, 1000, 10)
+        ),
+        travel_config(
+            PrintFeatureType::MoveCombing,
+            /*line_width=*/0,
+            extruder_plan.layer_thickness,
+            /*flow=*/0.0_r,
+            GCodePathConfig::SpeedDerivatives(120, 5000, 30)
         )
     {
-        square.push_back(GCodePath(
-            path_config,
-            /*mesh_id=*/"test_mesh",
-            SpaceFillType::PolyLines,
-            /*flow=*/1.0_r,
-            /*spiralize=*/false,
-            /*speed_factor=*/1.0_r
-        ));
+        const std::string mesh_id = "test_mesh";
+        constexpr Ratio flow_1 = 1.0_r;
+        constexpr bool no_spiralize = false;
+        constexpr Ratio speed_factor_1 = 1.0_r;
+        square.assign({GCodePath(extrusion_config, mesh_id, SpaceFillType::PolyLines, flow_1, no_spiralize, speed_factor_1)});
         square.back().points = {
             Point(0, 0),
             Point(1000, 0),
@@ -68,6 +85,19 @@ public:
             Point(0, 1000),
             Point(0, 0)
         };
+
+        lines.assign({
+            GCodePath(extrusion_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_factor_1),
+            GCodePath(travel_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_factor_1),
+            GCodePath(extrusion_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_factor_1),
+            GCodePath(travel_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_factor_1),
+            GCodePath(extrusion_config, mesh_id, SpaceFillType::Lines, flow_1, no_spiralize, speed_factor_1)
+        });
+        lines[0].points = {Point(0, 0), Point(1000, 0)};
+        lines[1].points = {Point(1000, 0), Point(1000, 400)};
+        lines[2].points = {Point(1000, 400), Point(0, 400)};
+        lines[3].points = {Point(0, 400), Point(0, 800)};
+        lines[4].points = {Point(0, 800), Point(1000, 800)};
     }
 };
 
@@ -77,19 +107,22 @@ public:
  */
 TEST_F(ExtruderPlanTest, BackPressureCompensationZeroIsUncompensated)
 {
-    extruder_plan.paths = square;
-    std::vector<Ratio> original_flows;
-    for(const GCodePath& path : extruder_plan.paths)
+    for(std::vector<GCodePath> path : {square, lines})
     {
-        original_flows.push_back(path.flow);
-    }
+        extruder_plan.paths = path;
+        std::vector<Ratio> original_flows;
+        for(const GCodePath& path : extruder_plan.paths)
+        {
+            original_flows.push_back(path.flow);
+        }
 
-    extruder_plan.applyBackPressureCompensation(0.0_r);
+        extruder_plan.applyBackPressureCompensation(0.0_r);
 
-    ASSERT_EQ(extruder_plan.paths.size(), original_flows.size()) << "Number of paths may not have changed.";
-    for(size_t i = 0; i < extruder_plan.paths.size(); ++i)
-    {
-        EXPECT_EQ(original_flows[i], extruder_plan.paths[i].flow) << "The flow rate did not change, because the back pressure compensation ratio was 0.";
+        ASSERT_EQ(extruder_plan.paths.size(), original_flows.size()) << "Number of paths may not have changed.";
+        for(size_t i = 0; i < extruder_plan.paths.size(); ++i)
+        {
+            EXPECT_EQ(original_flows[i], extruder_plan.paths[i].flow) << "The flow rate did not change, because the back pressure compensation ratio was 0.";
+        }
     }
 }
 
