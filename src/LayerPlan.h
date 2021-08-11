ec35e592f4469ac3f527330fe18402e3dff281b0
Jaime van Kessel <nallath@gmail.com>
2021-08-11 11:06:46 +0200

    Clarify documentation & fix some spelling mistakes
    CURA-7279

--- src/LayerPlan.h
+++ src/LayerPlan.h
@@ -171,12 +171,12 @@ public:
 
     /*!
      * Apply back-pressure compensation to this path.
-     * Since the total (filament) pressure in a feeder-system is not only dependant on the pressure that exists between the nozzle and the
-     * feed-mechanism (which should be near-constant on our setup), but _also_ between the nozzle and the last-printed layer. This last
-     * type is called 'back-pressure'. In this function, properties of the path-outflow are adjusted so that the back-pressure is
-     * compensated for. This is conjenctured to be especially important if the printer has a Bowden-tube style setup.
+     * Since the total (filament) pressure in a feeder-system is not only dependent on the pressure that exists between the nozzle and the
+     * feed-mechanism (which should be near-constant on a bowden style setup), but _also_ between the nozzle and the last-printed layer.
+     * This last type is called 'back-pressure'. In this function, properties of the path-outflow are adjusted so that the back-pressure is
+     * compensated for. This is conjectured to be especially important if the printer has a Bowden-tube style setup.
      *
-     * \param The ammount of back-pressure compensation in mm^3/s. 'Applying' a value of 0 is a no-op.
+     * \param The amount of back-pressure compensation in mm^3/s. 'Applying' a value of 0 is a no-op.
      */
     void applyBackPressureCompensation(const double back_pressure_compensation);
 
@@ -767,10 +767,10 @@ public:
 
     /*!
      * Apply back-pressure compensation to this layer-plan.
-     * Since the total (filament) pressure in a feeder-system is not only dependant on the pressure that exists between the nozzle and the
-     * feed-mechanism (which should be near-constant on our setup), but _also_ between the nozzle and the last-printed layer. This last
+     * Since the total (filament) pressure in a feeder-system is not only dependent on the pressure that exists between the nozzle and the
+     * feed-mechanism (which should be near-constant on a bowden style setup), but _also_ between the nozzle and the last-printed layer. This last
      * type is called 'back-pressure'. In this function, properties of the outflow (of the paths for each extruder) are adjusted so that
-     * the back-pressure is compensated for. This is conjenctured to be especially important if the printer has a Bowden-tube style setup.
+     * the back-pressure is compensated for. This is conjectured to be especially important if the printer has a Bowden-tube style setup.
      */
     void applyBackPressureCompensation();
 };
