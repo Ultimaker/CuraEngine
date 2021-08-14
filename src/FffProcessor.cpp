9e8b4bafaafed1004b193c5ef92b6f580dc0e071
Jaime van Kessel <nallath@gmail.com>
2021-08-14 19:43:27 +0200

    Remove unused code

--- src/FffProcessor.cpp
+++ src/FffProcessor.cpp
@@ -1,9 +1,7 @@
-//Copyright (c) 2018 Ultimaker B.V.
+//Copyright (c) 2021 Ultimaker B.V.
 //CuraEngine is released under the terms of the AGPLv3 or higher.
 
 #include "FffProcessor.h"
-#include "Application.h" //To send the layer data through the communication channel.
-#include "communication/Communication.h" //To send the layer data through the communication channel.
 
 namespace cura 
 {
--- src/FffProcessor.h
+++ src/FffProcessor.h
@@ -1,4 +1,4 @@
-//Copyright (c) 2018 Ultimaker B.V.
+//Copyright (c) 2021 Ultimaker B.V.
 //CuraEngine is released under the terms of the AGPLv3 or higher.
 
 #ifndef FFF_PROCESSOR_H
@@ -9,7 +9,6 @@
 #include "utils/gettime.h"
 #include "utils/NoCopy.h"
 
-#define SHOW_ALL_SETTINGS true
 
 namespace cura {
 
