3b34b69277a16910cdab1b2b961635fd34b63b68
Ghostkeeper <rubend@tutanota.com>
2021-10-25 18:26:50 +0200

    Make number of layers depend on slicing tolerance setting
    This is how much space we make for layers. The actual number of generated layers can still be different. I've seen some cases where the actual number of layers was clearly wrong.
    
    Contributes to issue CURA-8614.

--- src/utils/math.h
+++ src/utils/math.h
@@ -1,4 +1,6 @@
-/** Copyright (C) 2016 Ultimaker - Released under terms of the AGPLv3 License */
+//Copyright (c) 2021 Ultimaker B.V.
+//CuraEngine is released under the terms of the AGPLv3 or higher.
+
 #ifndef UTILS_MATH_H
 #define UTILS_MATH_H
 
@@ -21,6 +23,14 @@ inline unsigned int round_divide_signed(int dividend, int divisor) //!< Return d
 {
     const int abs_div = std::abs(divisor);
     return (dividend * divisor > 0 ? 1 : -1) * ((std::abs(dividend) + abs_div / 2) / abs_div);
+}
+inline unsigned int ceil_divide_signed(int dividend, int divisor) //!< Return dividend divided by divisor rounded up towards positive infinity.
+{
+    return (dividend / divisor) + (dividend * divisor > 0 ? 1 : 0);
+}
+inline unsigned int floor_divide_signed(int dividend, int divisor) //!< Return dividend divided by divisor rounded down towards negative infinity.
+{
+    return (dividend / divisor) + (dividend * divisor > 0 ? 0 : -1);
 }
 inline unsigned int round_divide(unsigned int dividend, unsigned int divisor) //!< Return dividend divided by divisor rounded to the nearest integer
 {
