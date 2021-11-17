0a561014646dea2817cf50a52a4ecfc0d1660b49
Remco Burema <r.burema@ultimaker.com>
2021-11-17 15:50:14 +0100

    Update documentation.
    part of CURA-8702

--- src/infill/LightningTreeNode.h
+++ src/infill/LightningTreeNode.h
@@ -57,7 +57,7 @@ public:
     const Point& getLocation() const;
 
     /*!
-     * Get the point the furthest from the root.
+     * Get the point the furthest from the root, measured along the branches of the path to that node.
      * \return The furthest position, which may be the (local) root itself if the tree has no nodes.
      */
     Point getFurthestLocation() const;
