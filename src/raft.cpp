7c7eb9950c5e5e81b969549ea208f7096793f294
Remco Burema <r.burema@ultimaker.com>
2021-12-10 15:31:37 +0100

    Initial work on shrinkage plate.
    Hardcoded a.t.m. A shrinkage plate is _mostly_ an adhesion method that's just a 'convex-ifief' raft, but care needs to be taken that the shrinkage is the same or very similar in every direction. For materials that have massive shrinkage.
    
    part of CURA-8081

--- src/raft.cpp
+++ src/raft.cpp
@@ -39,6 +39,13 @@ void Raft::generate(SliceDataStorage& storage)
     }
     const coord_t smoothing = settings.get<coord_t>("raft_smoothing");
     storage.raftOutline = storage.raftOutline.offset(smoothing, ClipperLib::jtRound).offset(-smoothing, ClipperLib::jtRound); // remove small holes and smooth inward corners
+
+    constexpr bool shrinkage_plate = true;
+    if (shrinkage_plate)
+    {
+        storage.raftOutline = storage.raftOutline.approxConvexHull();
+        storage.raftOutline.makeConvex();
+    }
 }
 
 coord_t Raft::getTotalThickness()
