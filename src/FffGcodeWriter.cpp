4f8eb1cd10ebf70fcefc429b47accffaf72c677e
Tim Kuipers <t.kuipers@ultimaker.com>
2022-03-02 20:57:32 +0100

    clarify variable naming

--- src/FffGcodeWriter.cpp
+++ src/FffGcodeWriter.cpp
@@ -2689,7 +2689,7 @@ bool FffGcodeWriter::processSupportInfill(const SliceDataStorage& storage, Layer
             const coord_t support_line_width = default_support_line_width * (combine_idx + 1);
 
             Polygons support_polygons;
-            VariableWidthPaths wall_toolpaths_here;
+            VariableWidthPaths gap_filler_toolpaths; // In case of a zigzag infill pattern the Infill comp generates gap fillers for the initial offset from the outline to where the zigzag connectors will lie
             Polygons support_lines;
             const size_t max_density_idx = part.infill_area_per_combine_per_density.size() - 1;
             for(size_t density_idx = max_density_idx; (density_idx + 1) > 0; --density_idx)
@@ -2718,7 +2718,7 @@ bool FffGcodeWriter::processSupportInfill(const SliceDataStorage& storage, Layer
                                    max_resolution, max_deviation,
                                    wall_count, infill_origin, support_connect_zigzags,
                                    use_endpieces, skip_some_zags, zag_skip_count, pocket_size);
-                infill_comp.generate(wall_toolpaths_here, support_polygons, support_lines, infill_extruder.settings, storage.support.cross_fill_provider);
+                infill_comp.generate(gap_filler_toolpaths, support_polygons, support_lines, infill_extruder.settings, storage.support.cross_fill_provider);
             }
 
             setExtruder_addPrime(storage, gcode_layer, extruder_nr); // only switch extruder if we're sure we're going to switch
