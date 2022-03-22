43b14dc4fd2be04f79130c03387b47c0d7ebaa11
Remco Burema <r.burema@ultimaker.com>
2022-03-22 11:49:56 +0100

    Assume bead-count is correct and leading for Center compute.
    The bead count should already have been correctly calculated beforehand, and the rest of the program may assume so.
    
    part of CURA-9027

--- src/BeadingStrategy/CenterDeviationBeadingStrategy.cpp
+++ src/BeadingStrategy/CenterDeviationBeadingStrategy.cpp
@@ -20,9 +20,6 @@ CenterDeviationBeadingStrategy::CenterDeviationBeadingStrategy
 
 CenterDeviationBeadingStrategy::Beading CenterDeviationBeadingStrategy::compute(coord_t thickness, coord_t bead_count) const
 {
-    const coord_t minimum_line_width_split = optimal_width * wall_split_middle_threshold;
-    const coord_t minimum_line_width_add = optimal_width * wall_add_middle_threshold;
-
     Beading ret;
 
     ret.total_thickness = thickness;
@@ -30,27 +27,25 @@ CenterDeviationBeadingStrategy::Beading CenterDeviationBeadingStrategy::compute(
     {
         // Set the bead widths
         ret.bead_widths = std::vector<coord_t>(static_cast<size_t>(bead_count), optimal_width);
-        const coord_t optimal_thickness = getOptimalThickness(bead_count);
-        const coord_t diff_thickness = thickness - optimal_thickness; //Amount of deviation. Either spread out over the middle 2 lines, or concentrated in the center line.
-        const size_t center_bead_idx = ret.bead_widths.size() / 2;
-        if (bead_count % 2 == 0) // Even lines
+        coord_t leftover_thickness = ret.total_thickness;
+        for (size_t bead_index = 0; bead_index <= bead_count / 2; ++bead_index)
         {
-            const coord_t inner_bead_widths = optimal_width + diff_thickness / 2;
-            if (inner_bead_widths < minimum_line_width_add)
+            const size_t opposite_bead_index = bead_count - (1 + bead_index);
+            switch (opposite_bead_index - bead_index)
             {
-                return compute(thickness, bead_count - 1);
+            case 0: // Single bead in the middle:
+                ret.bead_widths[bead_index] = leftover_thickness;
+                break;
+            case 1: // Two beads in the middle:
+                ret.bead_widths[bead_index] = leftover_thickness / 2;
+                ret.bead_widths[opposite_bead_index] = leftover_thickness / 2;
+                break;
+            default: // Beads on the outside.
+                ret.bead_widths[bead_index] = optimal_width;
+                ret.bead_widths[opposite_bead_index] = optimal_width;
+                break;
             }
-            ret.bead_widths[center_bead_idx - 1] = inner_bead_widths;
-            ret.bead_widths[center_bead_idx] = inner_bead_widths;
-        }
-        else // Uneven lines
-        {
-            const coord_t inner_bead_widths = optimal_width + diff_thickness;
-            if (inner_bead_widths < minimum_line_width_split)
-            {
-                return compute(thickness, bead_count - 1);
-            }
-            ret.bead_widths[center_bead_idx] = inner_bead_widths;
+            leftover_thickness -= ret.bead_widths[bead_index] + ret.bead_widths[opposite_bead_index]; // Incorrect when there is a last single middle line, but the loop will exit anyway then.
         }
 
         // Set the center line location of the bead toolpaths.
@@ -83,10 +78,10 @@ coord_t CenterDeviationBeadingStrategy::getTransitionThickness(coord_t lower_bea
 
 coord_t CenterDeviationBeadingStrategy::getOptimalBeadCount(coord_t thickness) const
 {
-    const coord_t naive_count = thickness / optimal_width; // How many lines we can fit in for sure.
+    const coord_t naive_count = thickness / optimal_width; // How many lines we can fit in for sure. (Note difference with Distributed.)
     const coord_t remainder = thickness - naive_count * optimal_width; // Space left after fitting that many lines.
     const coord_t minimum_line_width = optimal_width * (naive_count % 2 == 1 ? wall_split_middle_threshold : wall_add_middle_threshold);
-    return naive_count + (remainder > minimum_line_width); // If there's enough space, fit an extra one.
+    return naive_count + (remainder >= minimum_line_width); // If there's enough space, fit an extra one.
 }
 
 } // namespace cura
