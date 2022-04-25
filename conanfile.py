160d9c8eebfde60cdbf309b362a0f2e409490db0
Jelle Spijker <j.spijker@ultimaker.com>
2022-04-25 08:44:05 +0200

    Added build and package methods
    The previous conanfile.py was the bare minimum for a consumer package
    if CuraEngine is to serve as a consuming package as well it should
    contain the options and package.
    
    Still under construction
    
    CURA-9177 Fix CI/CD for Cura, Uranium and CuraEngine

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,7 @@
 from conan import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMakeDeps
-
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
+from conan.tools import files
+from conans import tools
 
 required_conan_version = ">=1.46.2"
 
@@ -11,24 +12,85 @@ class CuraBuildEnvironemtConan(ConanFile):
     topics = ("conan", "python", "pypi", "pip")
     settings = "os", "compiler", "build_type", "arch"
     build_policy = "missing"
+    exports = "LICENSE*"
+    options = {
+        "enable_arcus": [True, False],
+        "enable_openmp": [True, False],
+        "enable_testing": [True, False]
+    }
+    default_options = {
+        "enable_arcus": True,
+        "enable_openmp": True,
+        "enable_testing": False
+    }
+    scm = {
+        "type": "git",
+        "subfolder": ".",
+        "url": "auto",
+        "revision": "auto"
+    }
+
+    def config_options(self):
+        if self.settings.os == "Macos":
+            self.options.enable_openmp = False
 
     def configure(self):
         self.options["boost"].header_only = True
+        self.options["clipper"].shared = True
+        self.options["protobuf"].shared = True
+        if self.options.enable_arcus:
+            self.options["arcus"].shared = True
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, 17)
+
+    def build_requirements(self):
+        self.tool_requires("ninja/[>=1.10.0]")
+        self.tool_requires("cmake/[>=3.23.0]")
+        if self.options.enable_arcus:
+            self.tool_requires("protobuf/3.17.1")
+        if self.options.enable_testing:
+            self.test_requires("gtest/[>=1.10.0]")
 
     def requirements(self):
         self.requires("protobuf/3.17.1")
         self.requires("clipper/6.4.2")
         self.requires("boost/1.78.0")
-        self.requires("gtest/1.8.1")
         self.requires("rapidjson/1.1.0")
         self.requires("stb/20200203")
-        self.requires("arcus/latest@ultimaker/stable")
+        if self.options.enable_arcus:
+            self.requires("protobuf/3.17.1")
+            self.requires("arcus/latest@ultimaker/stable")
+        if self.options.enable_openmp:
+            self.requires("llvm-openmp/12.0.1")
 
     def generate(self):
         cmake = CMakeDeps(self)
+        if self.options.enable_arcus:
+            if len(cmake.build_context_activated) == 0:
+                cmake.build_context_activated = ["protobuf"]
+            else:
+                cmake.build_context_activated.append("protobuf")
+            cmake.build_context_suffix = {"protobuf": "_BUILD"}
+            if len(cmake.build_context_activated) == 0:
+                cmake.build_context_build_modules = ["protobuf"]
+            else:
+                cmake.build_context_build_modules.append("protobuf")
+
+        if self.options.enable_testing:
+            if len(cmake.build_context_build_modules) == 0:
+                cmake.build_context_activated = ["gtest"]
+            else:
+                cmake.build_context_activated.append("gtest")
         cmake.generate()
 
-        tc = CMakeToolchain(self)
+        tc = CMakeToolchain(self, generator = "Ninja")
+
+        tc.variables["ALLOW_IN_SOURCE_BUILD"] = True
+        tc.variables["ENABLE_ARCUS"] = self.options.enable_arcus
+        tc.variables["BUILD_TESTS"] = self.options.enable_testing
+        tc.variables["ENABLE_OPENMP"] = self.options.enable_openmp
 
         # Don't use Visual Studio as the CMAKE_GENERATOR
         if self.settings.compiler == "Visual Studio":
@@ -36,3 +98,15 @@ class CuraBuildEnvironemtConan(ConanFile):
             tc.blocks["generic_system"].values["toolset"] = None
 
         tc.generate()
+
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        packager = files.AutoPackager(self)
+        packager.run()
