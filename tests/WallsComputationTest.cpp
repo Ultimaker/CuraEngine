4409a95f0232ea51e43681f73a6eb57bc6908952
Remco Burema <r.burema@ultimaker.com>
2022-05-27 14:56:16 +0200

    Wouldn't run on windows, since /tmp is not there.
    done as part of CURA-9338

--- tests/WallsComputationTest.cpp
+++ tests/WallsComputationTest.cpp
@@ -10,7 +10,6 @@
 #include "../src/WallsComputation.h" //Unit under test.
 #include "../src/InsetOrderOptimizer.h" //Unit also under test.
 
-#define WALLS_COMPUTATION_TEST_SVG_OUTPUT
 #ifdef WALLS_COMPUTATION_TEST_SVG_OUTPUT
 #include "../src/utils/polygon.h"
 #include <cstdlib>
