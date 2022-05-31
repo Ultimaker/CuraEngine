f2b2f4bcdff911fa3bb35062c7efc157fa2892e4
Ghostkeeper <rubend@tutanota.com>
2022-05-31 14:17:26 +0200

    Fix testing on result of simplification
    Otherwise we'll test on the original.
    
    Contributes to issue CURA-8636.

--- tests/utils/SimplifyTest.cpp
+++ tests/utils/SimplifyTest.cpp
@@ -29,6 +29,12 @@ public:
     }
 };
 
+/*!
+ * Test the maximum resolution being held.
+ *
+ * If the deviation is practically unlimited, there should be no line segments
+ * smaller than the maximum resolution in the result.
+ */
 TEST_F(SimplifyTest, CircleMaxResolution)
 {
     Polygon circle;
@@ -36,19 +42,19 @@ TEST_F(SimplifyTest, CircleMaxResolution)
     constexpr double segment_length = max_resolution - 10;
     constexpr double tau = 6.283185307179586476925286766559; //2 * pi.
     constexpr double increment = segment_length / radius; //Segments of 990 units.
-    for (double angle = 0; angle < tau; angle += increment)
+    for(double angle = 0; angle < tau; angle += increment)
     {
         circle.add(Point(std::cos(angle) * radius, std::sin(angle) * radius));
     }
 
     simplifier.max_deviation = 999999; //For this test, the maximum deviation should not be an issue.
     constexpr bool is_closed = true;
-    simplifier.polygon(circle);
+    circle = simplifier.polygon(circle);
     //With segments of 990, we need to remove exactly half of the vertices to meet the requirement that all segments are >=1000.
     constexpr coord_t minimum_segment_length = max_resolution;
     constexpr coord_t maximum_segment_length = segment_length * 2 + 20; //+20 for some error margin due to rounding.
 
-    for (size_t point_index = 1; point_index + 1 < circle.size(); point_index++) //Don't check the last vertex. Due to odd-numbered vertices it has to be shorter than the minimum.
+    for(size_t point_index = 1; point_index + 1 < circle.size(); point_index++) //Don't check the last vertex. Due to odd-numbered vertices it has to be shorter than the minimum.
     {
         coord_t segment_length = vSize(circle[point_index % circle.size()] - circle[point_index - 1]);
         ASSERT_GE(segment_length, minimum_segment_length) << "Segment " << (point_index - 1) << " - " << point_index << " is too short!";
