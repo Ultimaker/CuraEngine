a636b5507cee1a501e7edc1f9095c208b35f95ca
Ghostkeeper <rubend@tutanota.com>
2022-05-31 14:33:22 +0200

    Add test for maximum deviation in a circle
    Contributes to issue CURA-8636.

--- tests/utils/SimplifyTest.cpp
+++ tests/utils/SimplifyTest.cpp
@@ -4,6 +4,7 @@
 #include <gtest/gtest.h>
 
 #include "../../src/utils/Simplify.h" //The unit under test.
+#include "../../src/utils/polygonUtils.h" //Helper functions for testing deviation.
 
 namespace cura
 {
@@ -21,11 +22,23 @@ public:
      */
     Simplify simplifier;
 
+    //Some polygons to run tests on.
+    Polygon circle; //High resolution circle.
+
     SimplifyTest() : simplifier(max_resolution, max_deviation, max_area_deviation) {}
 
     void SetUp()
     {
         simplifier = Simplify(max_resolution, max_deviation, max_area_deviation); //Reset in case the test wants to change a parameter.
+
+        constexpr coord_t radius = 100000;
+        constexpr double segment_length = max_resolution - 10;
+        constexpr double tau = 6.283185307179586476925286766559; //2 * pi.
+        constexpr double increment = segment_length / radius; //Segments of 990 units.
+        for(double angle = 0; angle < tau; angle += increment)
+        {
+            circle.add(Point(std::cos(angle) * radius, std::sin(angle) * radius));
+        }
     }
 };
 
@@ -37,24 +50,43 @@ public:
  */
 TEST_F(SimplifyTest, CircleMaxResolution)
 {
-    Polygon circle;
-    constexpr coord_t radius = 100000;
-    constexpr double segment_length = max_resolution - 10;
-    constexpr double tau = 6.283185307179586476925286766559; //2 * pi.
-    constexpr double increment = segment_length / radius; //Segments of 990 units.
-    for(double angle = 0; angle < tau; angle += increment)
-    {
-        circle.add(Point(std::cos(angle) * radius, std::sin(angle) * radius));
-    }
-
     simplifier.max_deviation = 999999; //For this test, the maximum deviation should not be an issue.
-    constexpr bool is_closed = true;
     circle = simplifier.polygon(circle);
 
     for(size_t point_index = 1; point_index + 1 < circle.size(); point_index++) //Don't check the last vertex. Due to odd-numbered vertices it has to be shorter than the minimum.
     {
         coord_t segment_length = vSize(circle[point_index % circle.size()] - circle[point_index - 1]);
         EXPECT_GE(segment_length, max_resolution) << "Segment " << (point_index - 1) << " - " << point_index << " is too short! May not be less than the maximum resolution.";
+    }
+}
+
+/*!
+ * Test the maximum deviation being held.
+ *
+ * The deviation may not exceed the maximum deviation. If the maximum resolution
+ * is high, all line segments should get considered for simplification. It
+ * should then approach the maximum deviation regularly.
+ */
+TEST_F(SimplifyTest, CircleMaxDeviation)
+{
+    simplifier.max_resolution = 999999; //For this test, the maximum resolution should not be an issue.
+    Polygon simplified = simplifier.polygon(circle);
+
+    //Check on each vertex if it didn't deviate too much.
+    for(Point v : circle)
+    {
+        Point moved_point = v;
+        PolygonUtils::moveInside(simplified, moved_point);
+        const coord_t deviation = vSize(moved_point - v);
+        EXPECT_LE(deviation, simplifier.max_deviation);
+    }
+    //Also check the other way around, since the longest distance may also be on a vertex of the new polygon.
+    for(Point v : simplified)
+    {
+        Point moved_point = v;
+        PolygonUtils::moveInside(circle, moved_point);
+        const coord_t deviation = vSize(moved_point - v);
+        EXPECT_LE(deviation, simplifier.max_deviation);
     }
 }
 
