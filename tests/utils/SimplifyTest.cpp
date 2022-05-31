f176c38f1e506d6f345f662987e55736530659c4
Ghostkeeper <rubend@tutanota.com>
2022-05-31 14:19:25 +0200

    Don't test for segments being too long
    It's allowed to remove more, as long as it doesn't deviate too much. And in this case the deviation allowed is practically infinite.
    
    Contributes to issue CURA-8636.

--- tests/utils/SimplifyTest.cpp
+++ tests/utils/SimplifyTest.cpp
@@ -50,15 +50,11 @@ TEST_F(SimplifyTest, CircleMaxResolution)
     simplifier.max_deviation = 999999; //For this test, the maximum deviation should not be an issue.
     constexpr bool is_closed = true;
     circle = simplifier.polygon(circle);
-    //With segments of 990, we need to remove exactly half of the vertices to meet the requirement that all segments are >=1000.
-    constexpr coord_t minimum_segment_length = max_resolution;
-    constexpr coord_t maximum_segment_length = segment_length * 2 + 20; //+20 for some error margin due to rounding.
 
     for(size_t point_index = 1; point_index + 1 < circle.size(); point_index++) //Don't check the last vertex. Due to odd-numbered vertices it has to be shorter than the minimum.
     {
         coord_t segment_length = vSize(circle[point_index % circle.size()] - circle[point_index - 1]);
-        ASSERT_GE(segment_length, minimum_segment_length) << "Segment " << (point_index - 1) << " - " << point_index << " is too short!";
-        ASSERT_LE(segment_length, maximum_segment_length) << "Segment " << (point_index - 1) << " - " << point_index << " is too long!";
+        EXPECT_GE(segment_length, max_resolution) << "Segment " << (point_index - 1) << " - " << point_index << " is too short! May not be less than the maximum resolution.";
     }
 }
 
