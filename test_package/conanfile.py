6471c270ecfdcd3183b9a35c66952ae0226551f2
jspijker <j.spijker@ultimaker.com>
2022-08-06 14:05:06 +0200

    test_package now actually test the package
    - conandata.yml isn't necesary in this context
    - we are not building a library but a executable, so no need to build one ourself.
      - Also relative import in the CMakeLists.txt defeat the purpose of a test_package
         since we want to actually test the outcome of the Conan package. Therefor a
         test_package should be seen as standalone from a code prespective.
    - Test CuraEngine executable using the path in the user_info.
    
    Contribute to CURA-8830

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,45 +1,34 @@
+#  Copyright (c) 2022 Ultimaker B.V.
+#  CuraEngine is released under the terms of the AGPLv3 or higher
+
 from conan import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
 from conan.tools.env import VirtualRunEnv
 from conans import tools
+from conans.errors import ConanException
+from io import StringIO
 
 
 class CuraEngineTestConan(ConanFile):
-    build_policy = "missing"
     settings = "os", "compiler", "build_type", "arch"
-
-    python_requires = "umbase/0.1.5@ultimaker/testing"
-    python_requires_extend = "umbase.UMBaseConanfile"
-
-    def requirements(self):
-        for req in self._um_data()["requirements"]:
-            self.requires(req)
+    generators = "VirtualRunEnv"
 
     def generate(self):
-        cmake = CMakeDeps(self)
-        cmake.generate()
-
         venv = VirtualRunEnv(self)
         venv.generate()
 
-        tc = CMakeToolchain(self, generator = "Ninja")
-        if self.settings.compiler == "Visual Studio":
-            tc.blocks["generic_system"].values["generator_platform"] = None
-            tc.blocks["generic_system"].values["toolset"] = None
-        tc.generate()
-
     def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
+        pass
 
     def imports(self):
-        self.copy("*.lib", dst=".", src="@bindirs")
-        #self.copy("*.pb.h", dst=".", src="@bindirs")  # Needs to be in if test_package is extended to arcus use, but that would also defy the minimalist 'only test if it can build and run' that 'test_package' is supposed to have.
-        if self.settings.os == "Windows" and not tools.cross_building(self, skip_x64_x86 = True):
-            self.copy("*.dll", dst=".", src="@bindirs")
+        self.copy("*.lib", dst = ".", src = "@bindirs")
+        if self.settings.os == "Windows" and not tools.cross_building(self):
+            self.copy("*.dll", dst = self.build_folder, src = "@bindirs")
+            self.copy("*.dylib", dst = self.build_folder, src = "@bindirs")
 
     def test(self):
         if not tools.cross_building(self):
-            (prefix, postfix) = ("", ".exe") if self.settings.os == "Windows" else ("./", "")
-            self.run(f"{prefix}PackageTest{postfix}", env = "conanrun")
+            test_buf = StringIO()
+            self.run(f"{self.deps_user_info['curaengine'].curaengine}", env = "conanrun", output=test_buf)
+            out_val = test_buf.getvalue()
+            if f"Cura_SteamEngine version" not in out_val:
+                raise ConanException("CuraEngine wasn't build correctly!")
