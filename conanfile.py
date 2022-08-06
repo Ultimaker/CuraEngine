fcbb1a5bae6e1c4e6e79396d6ec5e3e506047ad4
jspijker <j.spijker@ultimaker.com>
2022-08-06 14:01:07 +0200

    Cleaned up Conan file
    - Remove option OpenMP when building on MacOS
    - use cmake default layout as all our projects.
    - add Path to cura engine bin as user_info such that other projects can easily use it
    
    Contribute to CURA-8830

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,12 @@
-import os
+#  Copyright (c) 2022 Ultimaker B.V.
+#  CuraEngine is released under the terms of the AGPLv3 or higher
 
+import os
 from conan import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
-from conan.tools import files
-from conans import tools
 from conan.errors import ConanInvalidConfiguration
-from conans.errors import ConanException
+from conan.tools import files
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
+from conans import tools
 
 required_conan_version = ">=1.47.0"
 
@@ -41,17 +42,9 @@ class CuraEngineConan(ConanFile):
         "revision": "auto"
     }
 
-    _cmake = None
-
-    @property
-    def cmake(self):
-        if self._cmake is None:
-            self._cmake = CMake(self)
-        return self._cmake
-
     def config_options(self):
         if self.settings.os == "Macos":
-            self.options.enable_openmp = False
+            del self.options.enable_openmp
 
     def configure(self):
         self.options["boost"].header_only = True
@@ -83,19 +76,15 @@ class CuraEngineConan(ConanFile):
                 self.requires(req)
 
     def generate(self):
-        cmake = CMakeDeps(self)
-
-        if self.options.enable_testing:
-            cmake.build_context_activated = ["gtest"]
-        cmake.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
         tc = CMakeToolchain(self)
-
         tc.variables["CURA_ENGINE_VERSION"] = self.version
         tc.variables["ENABLE_ARCUS"] = self.options.enable_arcus
-        tc.variables["BUILD_TESTING"] = self.options.enable_testing
-        tc.variables["ENABLE_OPENMP"] = self.options.enable_openmp
-        tc.variables["ALLOW_IN_SOURCE_BUILD"] = True
+        tc.variables["ENABLE_TESTING"] = self.options.enable_testing
+        if self.settings.os != "Macos":
+            tc.variables["ENABLE_OPENMP"] = self.options.enable_openmp
 
         # Don't use Visual Studio as the CMAKE_GENERATOR
         if self.settings.compiler == "Visual Studio":
@@ -105,29 +94,10 @@ class CuraEngineConan(ConanFile):
         tc.generate()
 
     def layout(self):
-        # TODO: Use the cmake_layout provided by Conan, that requires restructuring the headers and sources,
-        # or if we decided against a cmake_layout, we should at least make things uniform across our projects
-        # and create the layout by defining a function in the UMBaseConanfile.
-        # https://docs.conan.io/en/latest/reference/conanfile/tools/layout.html
-        self.folders.source = "."
-        try:
-            build_type = str(self.settings.build_type)
-        except ConanException:
-            raise ConanException("'build_type' setting not defined, it is necessary")
+        cmake_layout(self)
 
-        self.folders.build = f"cmake-build-{build_type.lower()}"
-        self.folders.generators = os.path.join(self.folders.build, "conan")
-
-        self.cpp.source.includedirs = ["include", "src"]
-
-        self.cpp.build.libdirs = ["."]
-        self.cpp.build.bindirs = ["."]
-
-        self.cpp.build.libs = ["_CuraEngine"]
-
-        self.cpp.package.includedirs = ["include"]
-        self.cpp.package.libdirs = ["lib"]
-        self.cpp.package.bindirs = ["bin"]
+        self.cpp.build.includedirs = ["."]  # To package the generated headers
+        self.cpp.package.libs = ["_CuraEngine"]
 
     def imports(self):
         self.copy("*.dll", dst=self.build_folder, src="@bindirs")
@@ -138,22 +108,22 @@ class CuraEngineConan(ConanFile):
             self.copy("*.dylib", dst=dest, src="@bindirs")
 
     def build(self):
-        cmake = self.cmake
+        cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def test(self):
         if not tools.cross_building(self) and self.options.enable_testing:
-            self.cmake.test()
-
-            import os
             for program in [f for f in os.listdir("./tests/.") if f.replace(".exe", "").endswith("Test")]:
                 prefix_path = "" if self.settings.os == "Windows" else "./"
                 self.run(f"{prefix_path}tests/{program}", env = "conanrun")
 
     def package(self):
         packager = files.AutoPackager(self)
-        packager.patterns.bin = ["CuraEngine.exe", "CuraEngine"]
         packager.run()
         self.copy("CuraEngine", src = self.build_folder, dst = "bin")
         self.copy("CuraEngine.exe", src = self.build_folder, dst = "bin")
+
+    def package_info(self):
+        ext = ".exe" if self.settings.os == "Windows" else ""
+        self.user_info.curaengine = os.path.join(self.package_folder, "bin", f"CuraEngine{ext}")
