2a7cf70a33771bb10b427b5e036e4d51105d5611
jspijker <j.spijker@ultimaker.com>
2022-09-06 14:17:22 +0200

    Use standardprojectsettings

--- conanfile.py
+++ conanfile.py
@@ -79,7 +79,7 @@ class CuraEngineConan(ConanFile):
                 self.test_requires(req)
 
     def requirements(self):
-        self.requires("umbase/[>=0.1.7]@ultimaker/stable")
+        self.requires("standardprojectsettings/[>=0.1.0]@ultimaker/stable")
         for req in self._um_data()["requirements"]:
             self.requires(req)
         if self.options.enable_arcus:
