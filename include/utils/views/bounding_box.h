9b2c40712efc13d2dcb021b82a98dfed7806320f
jspijker <j.spijker@ultimaker.com>
2022-11-01 10:13:55 +0100

    Use the convert view to return a bounding_box view
    Contribute to CURA-9775

--- include/utils/views/bounding_box.h
+++ include/utils/views/bounding_box.h
@@ -5,30 +5,36 @@
 #define UTILS_VIEWS_BOUNDING_BOX_H
 
 #include "utils/AABB.h"
+#include "utils/views/convert.h"
 
 #include <type_traits>
 
-#include <range/v3/view/transform.hpp>
-#include <range/v3/view/view.hpp>
 
 namespace cura::views
 {
 /* # cura::views::bounding_box
- * The bounding_box view converts an input range of `T` into an output range of `U` by calling the Projection on every element of the input range.
+ * The convert view converts an input range of `T` into an output range of `U` by calling the Projection on every element of the input range.
+ * and convert them to `V`
  *
  * ## Syntax
  * ```cpp
- * auto output_range = input_range | ranges::views::bounding_box(&ExtrusionLine::toPolygon);
+ * std::vector<ExtrusionLine*> input_range { ... };
+ * auto output_range = input_range | ranges::views::convert<Polygons>(&ExtrusionLine::toPolygon);
+ * ```
+ * or
+ * ```cpp
+ * std::vector<Polygon> input_range { ... };
+ * auto output_range = input_range | ranges::views::convert<Polygons>(ranges::identity{});
  * ```
  *
  * ## Parameters
- * \param proj Projection used to obtain the polygon(s), defaults to `std::identity`
+ * \param proj Projection used to obtain the elements
  *
- * \returns a transformed view with the elements of type `AABB`
+ * \returns a transformed view with elements of type `AABB`
  */
-constexpr auto bounding_box(auto&& proj = ranges::identity{})
+constexpr auto bounding_box(auto&& proj)
 {
-    return ranges::make_view_closure(ranges::views::transform([proj](auto item) { return AABB{ std::invoke(proj, item) }; }));
+    return convert<AABB>(std::forward<decltype(proj)>(proj));
 }
 } // namespace cura::views
 
