25b3732da72dd3b6775c9e87e7c61b6f8f4cac3e
c.lamboo <casperlamboo@gmail.com>
2023-02-09 16:06:04 +0100

    Don't generate support towers if the tip would be (partially) inside the xy-disallowed areas
    Contributes to CURA-10220

--- src/support.cpp
+++ src/support.cpp
@@ -1044,10 +1044,12 @@ void AreaSupport::generateSupportAreasForMesh(SliceDataStorage& storage,
                     constexpr size_t tower_top_layer_count = 6; // number of layers after which to conclude that a tiny support area needs a tower
                     if (layer_idx < layer_count - tower_top_layer_count && layer_idx >= tower_top_layer_count + bottom_empty_layer_count)
                     {
+                        const Polygons& disallowed_area = xy_disallowed_per_layer[layer_idx];
                         const Polygons& layer_below = storage.getLayerOutlines(layer_idx - tower_top_layer_count - bottom_empty_layer_count, no_support, no_prime_tower);
                         const Point middle = AABB(poly).getMiddle();
                         const bool has_model_below = layer_below.inside(middle);
-                        if (! has_model_below)
+                        const bool is_inside_disallowed_area = !disallowed_area.intersection(poly).empty();
+                        if (! has_model_below && ! is_inside_disallowed_area)
                         {
                             Polygons tiny_tower_here;
                             tiny_tower_here.add(poly);
