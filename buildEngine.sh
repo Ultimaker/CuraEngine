0ad10debcbb30ddd8a7f78b835ff675514253d54
thecampbells384 <thecampbells384@gmail.com>
2023-02-13 18:05:25 -0800

    changed the build and run scripts to give release and debug options

--- buildEngine.sh
+++ buildEngine.sh
@@ -3,7 +3,18 @@ root_dir=$(dirname $(realpath -- "$BASH_SOURCE"))
 
 cd $root_dir
 
-echo $(pwd)
-rm -r build | echo "no build dir to remove, installing..."
-conan install . --build=missing --update
-conan build .
+if [ $1 = debug ]; then
+    rm -r build | echo "no build dir to remove, installing..."
+    echo "building debug version..."
+    conan install . --build=missing --update -s build_type=Debug
+    cmake --preset debug
+    cmake --build --preset debug
+    source build/generators/conanrun.sh
+elif [ $1 = release ]; then
+    rm -r build | echo "no build dir to remove, installing..."
+    echo "building release version..."
+    conan install . --build=missing --update
+    conan build .
+else
+    echo "invalid option. options are 'debug' or 'release'"
+fi
--- runEngine.sh
+++ runEngine.sh
@@ -3,7 +3,9 @@ root_dir=$(dirname $(realpath -- "$BASH_SOURCE"))
 
 cd $root_dir
 
-if [ -f "./build/Release/CuraEngine" ]; then
+if [ -f "./build/Debug/CuraEngine" ]; then
+    ./build/Debug/CuraEngine connect 127.0.0.1:49674
+elif [ -f "./build/Release/CuraEngine" ]; then
     ./build/Release/CuraEngine connect 127.0.0.1:49674
 else
     echo "you must build using buildEngine.sh first"
