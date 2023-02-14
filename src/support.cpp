faf19b8d04fdf41a93719cb4b738ffb9d7210af7
Rijk van Manen <r.vanmanen@ultimaker.com>
2023-02-14 16:38:35 +0100

    do join after horizontal expansion
    Otherwise areas that are previously expanded, but now joined are expanded again.
    
    CURA-102220

--- src/support.cpp
+++ src/support.cpp
@@ -961,21 +961,6 @@ void AreaSupport::generateSupportAreasForMesh(SliceDataStorage& storage,
     {
         Polygons layer_this = mesh.full_overhang_areas[layer_idx + layer_z_distance_top];
 
-        // join different parts
-        const coord_t join_distance = infill_settings.get<coord_t>("support_join_distance");
-        if (join_distance > 0)
-        {
-            Polygons joining_areas = layer_this
-                                         .offset(join_distance, ClipperLib::jtRound)
-                                         .offset(-join_distance, ClipperLib::jtRound)
-                                         .difference(layer_this);
-            // To make sure the joining_areas have sufficient width the joining_areas are expanded with 2 line widths.
-            // Typically, the joining area is close to the model, so half of the offset will be removed by the model.
-            // The remaining half will be 2 line widths wide, allowing for a wall going back and forth.
-            joining_areas = joining_areas.offset(support_line_width * 2);
-            layer_this = layer_this.unionPolygons(joining_areas);
-        }
-
         if (extension_offset && ! is_support_mesh_place_holder)
         {
             // To avoid that the support is folding around the model, the support horizontal expansion should not cause
@@ -992,6 +977,21 @@ void AreaSupport::generateSupportAreasForMesh(SliceDataStorage& storage,
                 model_outline = model_outline.offset(offset_per_step);
                 layer_this = layer_this.difference(model_outline);
             }
+        }
+
+        // join different parts
+        const coord_t join_distance = infill_settings.get<coord_t>("support_join_distance");
+        if (join_distance > 0)
+        {
+            Polygons joining_areas = layer_this
+                                         .offset(join_distance, ClipperLib::jtRound)
+                                         .offset(-join_distance, ClipperLib::jtRound)
+                                         .difference(layer_this);
+            // To make sure the joining_areas have sufficient width the joining_areas are expanded with 2 line widths.
+            // Typically, the joining area is close to the model, so half of the offset will be removed by the model.
+            // The remaining half will be 2 line widths wide, allowing for a wall going back and forth.
+            joining_areas = joining_areas.offset(support_line_width * 2);
+            layer_this = layer_this.unionPolygons(joining_areas);
         }
 
         if (use_towers && ! is_support_mesh_place_holder)
