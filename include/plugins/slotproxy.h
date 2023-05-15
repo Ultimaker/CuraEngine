474027de2091f4dd3f4e0e476cf3c575334950c5
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-05-15 13:54:50 +0200

    Refactor SlotProxy to use gRPC
    - Remove Arcus::Socket and Arcus::SocketListener related code
    - Add includes for agrpc/grpc_context.hpp, boost/asio/awaitable.hpp, agrpc/asio_grpc.hpp, boost/asio/co_spawn.hpp, boost/asio/detached.hpp, grpcpp/client_context.h, grpcpp/create_channel.h
    - Add includes for plugin.grpc.pb.h and plugin.pb.h
    - Update SlotProxy constructor to use gRPC
    
    [CURA-10475]

--- include/plugins/slotproxy.h
+++ include/plugins/slotproxy.h
@@ -4,74 +4,26 @@
 #ifndef CURAENGINE_INCLUDE_PLUGINS_SLOTPROXY_H
 #define CURAENGINE_INCLUDE_PLUGINS_SLOTPROXY_H
 
+#include <agrpc/grpc_context.hpp>
+#include <boost/asio/awaitable.hpp>
 #include <functional>
 #include <memory>
 
-#include <Arcus/Socket.h>
-#include <Arcus/SocketListener.h>
+#include <agrpc/asio_grpc.hpp>
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/detached.hpp>
+#include <grpcpp/client_context.h>
+#include <grpcpp/create_channel.h>
 #include <range/v3/utility/semiregular_box.hpp>
-#include <semver.hpp>
 #include <spdlog/spdlog.h>
 
+#include "plugin.grpc.pb.h"
+#include "plugin.pb.h"
 #include "plugins/types.h"
 #include "plugins/validator.h"
 
-#include "plugin.pb.h"
-
 namespace cura::plugins
 {
-
-namespace detail
-{
-template<class Validator, class Receiver>
-class PluginListener : public Arcus::SocketListener
-{
-public:
-    using validator_t = Validator;
-    using receive_t = Receiver;
-
-    bool msgReceived() noexcept
-    {
-        bool msg_received = msg_received_;
-        msg_received_ = false;
-        return msg_received;
-    }
-
-private:
-    std::shared_ptr<validator_t> validator_{};
-    receive_t receiver_{};
-    bool msg_received_{ false };
-
-public:
-    explicit PluginListener(std::shared_ptr<validator_t> validator) noexcept : validator_{ validator_ }
-    {
-    }
-
-    void stateChanged(Arcus::SocketState) override{};
-
-    void messageReceived() override
-    {
-//        auto message = getSocket()->takeNextMessage();
-//
-//        if (message->GetTypeName() == "cura.plugins.proto.Plugin_ret")
-//        {
-//            auto* msg = dynamic_cast<proto::Plugin_ret*>(message.get());
-//            cura::plugins::proto::Plugin_ret mm {};
-//            auto x = converter_(mm);
-////            validator_->version = semver::from_string(version);
-////            validator_->plugin_hash = plugin_hash;
-//            spdlog::info("Plugin version: {}", validator_->version.to_string());
-//        }
-//        else if (message->GetTypeName() == receive_t::default_instance().GetTypeName())
-//        {
-//            // unblock plugin
-//        }
-    };
-
-    void error(const Arcus::Error& error) override{};
-};
-
-} // namespace detail
 
 template<plugins::SlotID Slot, class Validator, class Receiver, class Sender>
 class SlotProxy
@@ -81,69 +33,41 @@ public:
     using receive_t = Receiver;
     using send_t = Sender;
     using validator_t = Validator;
-    using listener_t = detail::PluginListener<validator_t, receive_t>;
 
     static inline constexpr plugins::SlotID slot_id{ Slot };
 
-    //std::shared_ptr<validator_t> validator;
-
 private:
-//    std::unique_ptr<Arcus::Socket> socket_;
-//    listener_t* listener_; // FIXME: in Arcus use smart_ptr for listeners otherwise we need to add a deconstructor in this class and that forces us to define the big 6
-//    receiver_t receiver_{};
-//    sender_t sender_{};
+    //    receiver_t receiver_{};
+    //    sender_t sender_{};
 
 public:
     SlotProxy(const std::string& ip, int port)
-//        validator{ std::make_shared<validator_t>() },
-//        socket_{ std::make_unique<Arcus::Socket>() }
-//        listener_{ new listener_t(validator) }
     {
-        // Add the listener
-//        socket_->addListener(listener_);
-//
-//        // Register all receiving message types
-//        socket_->registerMessageType(&plugins::proto::PluginRequest::default_instance());
-//        socket_->registerMessageType(&receive_t::default_instance());
-//
-//        // Connect to the plugin
-//        spdlog::info("Connecting to plugin at {}:{}", ip, port);
-//        socket_->connect(ip, port);
-//        while (socket_->getState() == Arcus::SocketState::Connecting || socket_->getState() == Arcus::SocketState::Opening)
-//        {
-//            std::this_thread::sleep_for(std::chrono::milliseconds{ 100 }); // TODO: Fix magic number
-//            // TODO: Add timeout??? Is this blocking even necessary?
-//        }
+        grpc::Status status;
+        proto::Plugin::Stub plugin_stub(grpc::CreateChannel(ip + ":" + std::to_string(port), grpc::InsecureChannelCredentials()));
+        agrpc::GrpcContext grpc_context;
 
-//        converters::simplify_converter_fn<send_t, receive_t > convvvvv{};
-
-//        SlotID slotId { SlotID::SIMPLIFY };
-//        auto x = convvvvv(slotId);
-//
-//        proto::PluginRequest args{};
-//        args.set_id(slot_id);
-
-//        socket_->sendMessage(std::make_shared<proto::PluginRequest>(args));
-//        if (listener_->msgReceived())
-//        {
-//            auto message = socket_->takeNextMessage();
-//            if (message->GetTypeName() == "cura.plugins.proto.Plugin_ret")
-//            {
-////                auto* msg = dynamic_cast<proto::Plugin_ret*>(message.get());
-////                validator->version = semver::from_string(msg->version());
-////                validator->plugin_hash = msg->plugin_hash();
-////                spdlog::info("Plugin version: {}", validator->version.to_string());
-//            }
-//        }
-        //
-        // No need to wait for the response, since the listener will set the version
+        boost::asio::co_spawn(
+            grpc_context,
+            [&]() -> boost::asio::awaitable<void>
+            {
+                using RPC = agrpc::RPC<&proto::Plugin::Stub::PrepareAsyncIdentify>;
+                grpc::ClientContext client_context{};
+                proto::PluginRequest request{};
+                request.set_id(slot_id);
+                proto::PluginResponse response{};
+                status = co_await RPC::request(grpc_context, plugin_stub, client_context, request, response, boost::asio::use_awaitable);
+                spdlog::info("Received response from plugin: {}", response.DebugString());
+            },
+            boost::asio::detached);
+        grpc_context.run();
     }
 
     auto operator()(auto&&... args)
     {
-        if (true)//validator && socket_->getState() == Arcus::SocketState::Connected)
+        if (true) // validator && socket_->getState() == Arcus::SocketState::Connected)
         {
-//            socket_->sendMessage(converter_(std::forward<decltype(args)>(args)...));
+            //            socket_->sendMessage(converter_(std::forward<decltype(args)>(args)...));
             // TODO: Block until message is received
             // TODO: Convert return message to actual return value
             return 1; // FIXME: This is not correct
