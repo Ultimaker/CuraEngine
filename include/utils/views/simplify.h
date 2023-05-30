2f66c52274bfc544b4d25cbaa125a50bd944d387
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-05-30 10:04:16 +0200

    fix simplified to give back correct value
    This breaks the pipe operation
    
    [CURA-10500]

--- include/utils/views/simplify.h
+++ include/utils/views/simplify.h
@@ -5,14 +5,20 @@
 #define UTILS_GEOMETRY_SIMPLIFY_H
 
 #include <concepts>
+#include <range/v3/functional/bind_back.hpp>
+#include <range/v3/functional/pipeable.hpp>
 #include <range/v3/view/drop.hpp>
+#include <range/v3/view/subrange.hpp>
 #include <type_traits>
 
 #include <boost/geometry/algorithms/simplify.hpp>
+#include <range/v3/all.hpp>
 #include <range/v3/range/concepts.hpp>
 #include <range/v3/range/operations.hpp>
 #include <range/v3/view/join.hpp>
 #include <range/v3/view/transform.hpp>
+#include <spdlog/spdlog.h>
+#include <utility>
 
 #include "utils/types/boost_tags.h"
 #include "utils/types/geometry.h"
@@ -25,20 +31,25 @@ namespace details
 struct simplify_base_fn
 {
     template<ranges::viewable_range Rng>
-    requires utils::closed_path<std::remove_cvref_t<Rng>> || utils::open_path<std::remove_cvref_t<Rng>> || utils::filled_path<std::remove_cvref_t<Rng>>
-    constexpr auto operator()(Rng&& rng, const std::integral auto max_deviation) const
+    requires utils::closed_path<std::remove_cvref_t<Rng>> || utils::open_path<std::remove_cvref_t<Rng>> || utils::filled_path<std::remove_cvref_t<Rng>> constexpr auto operator()(Rng&& rng, const std::integral auto max_deviation) const
     {
-        using point_t = std::remove_cvref_t<decltype(*ranges::begin(rng))>;
-        boost::geometry::model::ring<point_t> simplified;
-        boost::geometry::simplify(rng, simplified, max_deviation);
-        return simplified | ranges::views::all;
+        return impl_(std::forward<Rng>(rng), max_deviation);
     }
 
     template<ranges::viewable_range Rng>
-    requires utils::ranged_path<std::remove_cvref_t<Rng>>
-    constexpr auto operator()(Rng&& rng, const std::integral auto max_deviation) const
+    requires utils::ranged_path<std::remove_cvref_t<Rng>> constexpr auto operator()(Rng&& rng, const std::integral auto max_deviation) const
     {
-        return rng | ranges::views::transform([this, max_deviation](auto&& sub_rng) { return operator()(sub_rng, max_deviation); }) | ranges::views::all;
+        return rng | ranges::views::transform([this, max_deviation](auto&& sub_rng) { return impl_(std::forward<decltype(sub_rng)>(sub_rng), max_deviation); }) | ranges::views::all;
+    }
+
+private:
+    template<ranges::viewable_range Rng>
+    requires utils::closed_path<std::remove_cvref_t<Rng>> || utils::open_path<std::remove_cvref_t<Rng>> || utils::filled_path<std::remove_cvref_t<Rng>> constexpr auto impl_(Rng&& rng, const std::integral auto max_deviation) const
+    {
+        using rng_t = std::remove_cvref_t<Rng>;
+        rng_t simplified;
+        boost::geometry::simplify(rng, simplified, max_deviation);
+        return simplified;
     }
 };
 
@@ -55,7 +66,6 @@ struct simplify_fn : simplify_base_fn
 } // namespace details
 
 RANGES_INLINE_VARIABLE(details::simplify_fn, simplify);
-
 
 } // namespace cura::views
 
