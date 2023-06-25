46d414b4cec597b64c3cb8752815ee78b867d908
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-06-25 13:35:23 +0200

    Allow uniform coordinate query with std::get
    This would allow for getting a x,y,z coordinate for a IntPoint, or 3D point,
    described as a struct or vector/array/tuple etc. Will be expanded to accommodate
    Arachne types.
    
    ```
    struct Point{ int X; int Y; }
    Point p_struct{ .X = 1, .Y = 2. };
    
    std::array<int, 2> p_array { 1, 2 };
    
    auto x_struct = std::get<"X">(p_struct);
    auto x_array = std::get<"X">(p_array);
    ```
    
    [CURA-10724]

---
+++ include/utils/types/get.h
@@ -0,0 +1,69 @@
+// Copyright (c) 2023 UltiMaker
+// CuraEngine is released under the terms of the AGPLv3 or higher
+
+#ifndef UTILS_TYPES_GET_H
+#define UTILS_TYPES_GET_H
+
+#include <utils/types/char_range_literal.h>
+#include <utils/types/geometry.h>
+
+namespace std
+{
+template<size_t N>
+constexpr auto& get(cura::utils::point2d_named auto& point) noexcept
+{
+    static_assert(N < 2, "Index out of bounds");
+    if constexpr (N == 0)
+    {
+        return point.X;
+    }
+    return point.Y;
+}
+
+template<cura::utils::CharRangeLiteral C>
+constexpr auto& get(cura::utils::point2d auto& point) noexcept
+{
+    constexpr std::string_view idx = C.value;
+    static_assert(idx.starts_with("X") || idx.starts_with("x") || idx.starts_with("Y") || idx.starts_with("y"), "Index out of bounds");
+    if constexpr (idx.starts_with("X") || idx.starts_with("x"))
+    {
+        return std::get<0>(point);
+    }
+    return std::get<1>(point);
+}
+
+template<size_t N>
+constexpr auto& get(cura::utils::point3d_named auto& point) noexcept
+{
+    static_assert(N < 3, "Index out of bounds");
+    if constexpr (N == 0)
+    {
+        return point.x;
+    }
+    else if constexpr (N == 1)
+    {
+        return point.y;
+    }
+    return point.z;
+}
+
+template<cura::utils::CharRangeLiteral C>
+constexpr auto& get(cura::utils::point3d auto& point) noexcept
+{
+    static_assert(C.value == "X" || C.value == "x" || C.value == "Y" || C.value == "y" || C.value == "Z" || C.value == "z", "Index out of bounds");
+    constexpr std::string_view idx = C.value;
+    if constexpr (idx.starts_with("X") || idx.starts_with("x"))
+    {
+        return std::get<0>(point);
+    }
+    if constexpr (idx.starts_with("Y") || idx.starts_with("y"))
+    {
+        return std::get<1>(point);
+    }
+    return std::get<2>(point);
+}
+
+} // namespace std
+
+
+#endif // UTILS_TYPES_GET_H
