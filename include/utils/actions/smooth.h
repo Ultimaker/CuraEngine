4e1c55f5db1d18bdf4854e7dd483c397401608e9
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-07-01 09:57:19 +0200

    Refactor smoothing function, remove unused parameter and improve readability
    The smoothing function in `smooth.h` was refactored to improve code
    readability and maintainability. The 'smooth_distance' parameter was
    removed as it was not being utilized. The core logic of the function
    remains unchanged, and several variable names were updated to better
    reflect their context and function. Introduced a clearer, more accurate
    method to calculate the cosine of the angle deviation which is also
    generally optimized for smaller values of the `fluid_angle`. This is a
    more computational efficient approach since for 'Curved' interpolations,
    the `fluid_angle` is usually small which solves the primary problem.
    
    Contributes to CURA-10724

--- include/utils/actions/smooth.h
+++ include/utils/actions/smooth.h
@@ -25,19 +25,15 @@ namespace cura::actions
 
 struct smooth_fn
 {
-    constexpr auto operator()(const std::integral auto max_resolution, const std::integral auto smooth_distance, const std::floating_point auto fluid_angle) const
+    constexpr auto operator()(const std::integral auto max_resolution, const std::floating_point auto fluid_angle) const
     {
-        return ranges::make_action_closure(ranges::bind_back(smooth_fn{}, max_resolution, smooth_distance, fluid_angle));
+        return ranges::make_action_closure(ranges::bind_back(smooth_fn{}, max_resolution, fluid_angle));
     }
 
     template<class Rng>
-    requires ranges::forward_range<Rng> && ranges::sized_range<Rng> && ranges::erasable_range<Rng, ranges::iterator_t<Rng>, ranges::sentinel_t<Rng>> && (utils::point2d<ranges::range_value_t<Rng>> || utils::junctions<Rng>)
-    constexpr auto operator()(Rng&& rng, const std::integral auto max_resolution, const std::integral auto smooth_distance, const std::floating_point auto fluid_angle) const
+        requires ranges::forward_range<Rng>&& ranges::sized_range<Rng>&& ranges::erasable_range<Rng, ranges::iterator_t<Rng>, ranges::sentinel_t<Rng>>
+        && (utils::point2d<ranges::range_value_t<Rng>> || utils::junctions<Rng>)constexpr auto operator()(Rng&& rng, const std::integral auto max_resolution, const std::floating_point auto fluid_angle) const
     {
-        if (smooth_distance == 0)
-        {
-            return static_cast<Rng&&>(rng);
-        }
         const auto size = ranges::distance(rng) - 1; // For closed Path, if open then subtract 0
         if (size < 3)
         {
@@ -47,60 +43,75 @@ struct smooth_fn
         using point_type = std::remove_cvref_t<decltype(*ranges::begin(rng))>;
         std::set<point_type*> to_remove;
 
-        // Create a range of pointers to the points in the path, using the sliding view doesn't work because of the changing size of the path, which happens
-        // when points are filtered out.
-        auto tmp = rng;
-        auto ref_view = ranges::views::concat(tmp, tmp | ranges::views::take(2)) | ranges::views::addressof | ranges::to_vector;
-        auto windows = ref_view | ranges::views::filter([&to_remove](auto point) { return ! to_remove.contains(point); });
+        auto tmp = rng; // We don't want to shift the points of the ingoing range, therefor we create a temporary copy
+        auto ref_view = ranges::views::concat(tmp, tmp) | ranges::views::addressof; // Concate twice to make sure we have enough points to shift the window, even after filtering (note: the cylce view doesn't have and end, which makes it harder to determine when to stop)
+        auto windows = ref_view | ranges::views::filter([&to_remove](auto point) { return ! to_remove.contains(point); });  // Filter out the points that are marked for removal
 
         // Smooth the path, by moving over three segments at a time. If the middle segment is shorter than the max resolution, then we try to shifting those points outwards.
         // The previous and next segment should have a remaining length of at least the smooth distance, otherwise the point is not shifted, but deleted.
-        const auto shift_smooth_distance = smooth_distance * 2;
-
         for (auto windows_it = ranges::begin(windows); windows_it != ranges::end(windows); ++windows_it)
         {
-            auto p0 = *windows_it;
-            auto p1 = *std::next(windows_it, 1);
-            auto p2 = *std::next(windows_it, 2);
-            auto p3 = *std::next(windows_it, 3);
+            auto A = *windows_it;
+            auto B = *std::next(windows_it, 1);
+            auto C = *std::next(windows_it, 2);
+            auto D = *std::next(windows_it, 3);
 
-            const auto p1p2_distance = std::hypot(std::get<"X">(*p2) - std::get<"X">(*p1), std::get<"Y">(*p2) - std::get<"Y">(*p1));
-            if (p1p2_distance < max_resolution && ! withinDeviation(p0, p1, p2, p3, fluid_angle))
+            const auto BC_magnitude = std::hypot(std::get<"X">(*C) - std::get<"X">(*B), std::get<"Y">(*C) - std::get<"Y">(*B));
+            if (BC_magnitude < max_resolution && ! withinDeviation(A, B, C, D, fluid_angle))
             {
-                const auto p0p1_distance = std::hypot(std::get<"X">(*p1) - std::get<"X">(*p0), std::get<"Y">(*p1) - std::get<"Y">(*p0));
-                const bool shift_p1 = p0p1_distance > shift_smooth_distance;
-                if (shift_p1)
+                const auto AB_magnitude = std::hypot(std::get<"X">(*B) - std::get<"X">(*A), std::get<"Y">(*B) - std::get<"Y">(*A));
+                if (AB_magnitude > max_resolution)
                 {
-                    shiftPointTowards(p1, p0, p0p1_distance, smooth_distance);
+                    shiftPointTowards(B, A, AB_magnitude, max_resolution / 2);
                 }
                 else if (size - to_remove.size() > 2) // Only remove if there are more than 2 points left for open-paths, or 3 for closed
                 {
-                    to_remove.insert(p1);
+                    to_remove.insert(B);
                 }
-                const auto p2p3_distance = std::hypot(std::get<"X">(*p3) - std::get<"X">(*p2), std::get<"Y">(*p3) - std::get<"Y">(*p2));
-                const bool shift_p2 = p2p3_distance > shift_smooth_distance;
-                if (shift_p2)
+                const auto CD_magnitude = std::hypot(std::get<"X">(*D) - std::get<"X">(*C), std::get<"Y">(*D) - std::get<"Y">(*C));
+                if (CD_magnitude > max_resolution)
                 {
-                    shiftPointTowards(p2, p3, p2p3_distance, smooth_distance);
+                    shiftPointTowards(C, D, CD_magnitude, max_resolution / 2);
                 }
                 else if (size - to_remove.size() > 2) // Only remove if there are more than 2 points left for open-paths, or 3 for closed
                 {
-                    to_remove.insert(p2);
+                    to_remove.insert(C);
                 }
             }
-            if (p2 == ranges::front(windows))
+            if (B == ranges::front(ref_view) || B == ranges::back(ref_view))
             {
                 break;
             }
         }
 
-        return static_cast<Rng&&>(ranges::actions::remove_if(tmp, [&to_remove](auto point){ return to_remove.contains(&point); }));
+        return static_cast<Rng&&>(ranges::actions::remove_if(tmp, [&to_remove](auto point) { return to_remove.contains(&point); }));
     }
 
 private:
+    template<utils::point2d Vector>
+    constexpr double cosAngle(Vector* AB, Vector* BC) const noexcept
+    {
+        const auto dot = dotProduct(AB, BC);
+        const auto AB_mag = std::hypot(std::get<"X">(*AB), std::get<"Y">(*AB));
+        const auto BC_mag = std::hypot(std::get<"X">(*BC), std::get<"Y">(*BC));
+        if (AB_mag == 0 || BC_mag == 0)
+        {
+            return 0.0;
+        }
+        return dot / (AB_mag * BC_mag);
+    }
+
     template<class Point>
-    requires utils::point2d<Point> || utils::junction<Point>
-    constexpr void shiftPointTowards(Point* point, Point* target, const std::floating_point auto p0p1_distance, const std::integral auto smooth_distance) const noexcept {
+        requires utils::point2d<Point> || utils::junction<Point> constexpr auto cosAngle(Point* A, Point* B, Point* C) const noexcept
+    {
+        auto AB = std::make_tuple(std::get<"X">(*B) - std::get<"X">(*A), std::get<"Y">(*B) - std::get<"Y">(*A));
+        auto BC = std::make_tuple(std::get<"X">(*C) - std::get<"X">(*B), std::get<"Y">(*C) - std::get<"Y">(*B));
+        return cosAngle(&AB, &BC);
+    }
+
+    template<class Point>
+        requires utils::point2d<Point> || utils::junction<Point> constexpr void shiftPointTowards(Point* point, Point* target, const std::floating_point auto p0p1_distance, const std::integral auto smooth_distance) const noexcept
+    {
         using coord_type = std::remove_cvref_t<decltype(std::get<"X">(*point))>;
         const auto shift_distance = smooth_distance / p0p1_distance;
         const auto shift_distance_x = static_cast<coord_type>((std::get<"X">(*target) - std::get<"X">(*point)) * shift_distance);
@@ -118,42 +129,21 @@ private:
     }
 
     template<class Vector>
-    requires utils::point2d<Vector> || utils::junction<Vector>
-    constexpr auto dotProduct(Vector* point_0, Vector* point_1) const noexcept
+        requires utils::point2d<Vector> || utils::junction<Vector> constexpr auto dotProduct(Vector* point_0, Vector* point_1) const noexcept
     {
         return std::get<"X">(*point_0) * std::get<"X">(*point_1) + std::get<"Y">(*point_0) * std::get<"Y">(*point_1);
     }
 
-    template<utils::point2d Vector>
-    constexpr auto angleBetweenVectors(Vector* vec0, Vector* vec1) const -> decltype(dotProduct(vec0, vec1))
+    template<class Point>
+        requires utils::point2d<Point> || utils::junction<Point> constexpr auto withinDeviation(Point* A, Point* B, Point* C, Point* D, const std::floating_point auto fluid_angle) const
     {
-        const auto dot = dotProduct(vec0, vec1);
-        const auto vec0_mag = std::hypot(std::get<"X">(*vec0), std::get<"Y">(*vec0));
-        const auto vec1_mag = std::hypot(std::get<"X">(*vec1), std::get<"Y">(*vec1));
-        if (vec0_mag == 0 || vec1_mag == 0)
-        {
-            constexpr auto perpendicular_angle = 90.0;
-            return perpendicular_angle;
-        }
-        const auto cos_angle = dot / (vec0_mag * vec1_mag);
-        const auto angle_rad = std::acos(cos_angle);
-        constexpr auto rad_to_degree_factor = 180.0 / std::numbers::pi;
-        return angle_rad * rad_to_degree_factor;
-    }
-
-    template<class Vector>
-    requires utils::point2d<Vector> || utils::junction<Vector>
-    constexpr auto withinDeviation(Vector* p0, Vector* p1, Vector* p2, Vector* p3, const std::floating_point auto fluid_angle) const
-    {
-        struct Point
-        {
-            int64_t X;
-            int64_t Y;
-        };
-        Point ab{ std::get<"X">(*p1) - std::get<"X">(*p0), std::get<"Y">(*p1) - std::get<"Y">(*p0) };
-        Point bc{ std::get<"X">(*p2) - std::get<"X">(*p1), std::get<"Y">(*p2) - std::get<"Y">(*p1) };
-        Point cd{ std::get<"X">(*p3) - std::get<"X">(*p2), std::get<"Y">(*p3) - std::get<"Y">(*p2) };
-        return std::abs(angleBetweenVectors(&ab, &bc) - angleBetweenVectors(&ab, &cd)) < fluid_angle;
+        // This comparison might be slightly off because the cosine function is not linear, especially for larger angles. The range of the cosine function is from -1 to 1 for the
+        // input range from -pi to pi, so for small angles, the difference in the cosine of the angles can be approximately equal to the difference in the angles
+        // (measured in radians). In order words, the comparison std::abs(cosAngle(A, B, C) - cosAngle(A, B, D)) < std::cos(fluid_angle) is an approximation that is most accurate
+        // for small fluid angles. For fluid angles near or exceeding pi/2 radians (90 degrees), the right-hand side of the inequality can even become negative, making the
+        // condition always false. Since the fluid angle is usually small, this is not a problem in practice.
+        const auto abs_cos_angle = std::abs(cosAngle(A, B, C) - cosAngle(A, B, D));
+        return abs_cos_angle < std::cos(fluid_angle);
     }
 };
 
