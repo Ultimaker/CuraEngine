6027c543e00bfaa07b41503ce85674b925b9883f
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-07-01 09:55:38 +0200

    Update comment syntax in geometry.h
    In this commit, the syntax for the comments in the geometry.h file has
    been updated. The "\\" syntax was replaced by "@" as the latter is more
    standard for Doxygen usage. This change allows for better parsing and
    generation of documentation by Doxygen, and aligns with common practices
    in the software industry.
    
    Contributes to CURA-10724

--- include/utils/types/geometry.h
+++ include/utils/types/geometry.h
@@ -1,8 +1,8 @@
 // Copyright (c) 2023 UltiMaker
 // CuraEngine is released under the terms of the AGPLv3 or higher
 
-/*! \file geometry.h
- *  \brief Defines geometric concepts like Point2D, Point3D, Segment etc.
+/*! @file geometry.h
+ *  @brief Defines geometric concepts like Point2D, Point3D, Segment etc.
  */
 
 #ifndef UTILS_TYPES_GEOMETRY_H
@@ -17,16 +17,16 @@
 #include <range/v3/range/operations.hpp>
 
 /*!
- * \namespace cura::utils
- * \brief Namspace for Cura utils
+ * @namespace cura::utils
+ * @brief Namspace for Cura utils
  */
 namespace cura::utils
 {
 
 /*!
- * \concept point2d_tuple
- * \brief Checks whether T is a 2D point represented as a tuple with two integral elements
- * \tparam T The type to check
+ * @concept point2d_tuple
+ * @brief Checks whether T is a 2D point represented as a tuple with two integral elements
+ * @tparam T The type to check
  */
 template<typename T>
 concept point2d_tuple = requires(T t)
@@ -36,9 +36,9 @@ concept point2d_tuple = requires(T t)
 };
 
 /*!
- * \concept point2d_ranged
- * \brief Checks whether T is a 2D point represented as a range with two integral elements
- * \tparam T The type to check
+ * @concept point2d_ranged
+ * @brief Checks whether T is a 2D point represented as a range with two integral elements
+ * @tparam T The type to check
  */
 template<class T>
 concept point2d_ranged = ranges::range<T> && std::integral<typename T::value_type> && requires(T point)
@@ -48,9 +48,9 @@ concept point2d_ranged = ranges::range<T> && std::integral<typename T::value_typ
 
 
 /*!
- * \concept point2d_named
- * \brief Checks whether T is a 2D point represented as an object with X and Y integral fields
- * \tparam T The type to check
+ * @concept point2d_named
+ * @brief Checks whether T is a 2D point represented as an object with X and Y integral fields
+ * @tparam T The type to check
  */
 template<class T>
 concept point2d_named = requires(T point)
@@ -60,17 +60,17 @@ concept point2d_named = requires(T point)
 };
 
 /*!
- * \concept point2d
- * \brief Checks whether T is a 2D point. A type satisfying any of point2d_tuple, point2d_ranged, or point2d_named
- * \tparam T The type to check
+ * @concept point2d
+ * @brief Checks whether T is a 2D point. A type satisfying any of point2d_tuple, point2d_ranged, or point2d_named
+ * @tparam T The type to check
  */
 template<class T>
 concept point2d = point2d_named<T> || point2d_ranged<T> || point2d_tuple<T>;
 
 /*!
- * \concept point3d_tuple
- * \brief Checks whether T is a 3D point represented as a tuple with three integral elements
- * \tparam T The type to check
+ * @concept point3d_tuple
+ * @brief Checks whether T is a 3D point represented as a tuple with three integral elements
+ * @tparam T The type to check
  */
 template<typename T>
 concept point3d_tuple = requires(T t)
@@ -80,9 +80,9 @@ concept point3d_tuple = requires(T t)
 };
 
 /*!
- * \concept point3d_ranged
- * \brief Checks whether T is a 3D point represented as a range with three integral elements
- * \tparam T The type to check
+ * @concept point3d_ranged
+ * @brief Checks whether T is a 3D point represented as a range with three integral elements
+ * @tparam T The type to check
  */
 template<class T>
 concept point3d_ranged = ranges::range<T> && std::integral<typename T::value_type> && requires(T point)
@@ -91,9 +91,9 @@ concept point3d_ranged = ranges::range<T> && std::integral<typename T::value_typ
 };
 
 /*!
- * \concept point3d_named
- * \brief Checks whether T is a 3D point represented as an object with X, Y and Z integral fields
- * \tparam T The type to check
+ * @concept point3d_named
+ * @brief Checks whether T is a 3D point represented as an object with X, Y and Z integral fields
+ * @tparam T The type to check
  */
 template<class T>
 concept point3d_named = requires(T point)
@@ -104,33 +104,33 @@ concept point3d_named = requires(T point)
 };
 
 /*!
- * \concept point3d
- * \brief Checks whether T is a 3D point. A type satisfying any of point3d_tuple, point3d_ranged, or point3d_named
- * \tparam T The type to check
+ * @concept point3d
+ * @brief Checks whether T is a 3D point. A type satisfying any of point3d_tuple, point3d_ranged, or point3d_named
+ * @tparam T The type to check
  */
 template<class T>
 concept point3d = point3d_named<T> || point3d_ranged<T> || point3d_tuple<T>;
 
 /*!
- * \concept point_named
- * \brief Checks whether T is a point represented as an object with named fields
- * \tparam T The type to check
+ * @concept point_named
+ * @brief Checks whether T is a point represented as an object with named fields
+ * @tparam T The type to check
  */
 template<class T>
 concept point_named = point2d_named<T> || point3d_named<T>;
 
 /*!
- * \concept point
- * \brief Checks whether T is a 2D or 3D point
- * \tparam T The type to check
+ * @concept point
+ * @brief Checks whether T is a 2D or 3D point
+ * @tparam T The type to check
  */
 template<class T>
 concept point = point2d<T> || point3d<T>;
 
 /*!
- * \concept segment
- * \brief Checks whether T represents a segment as a pair of 2D or 3D points.
- * \tparam T The type to check
+ * @concept segment
+ * @brief Checks whether T represents a segment as a pair of 2D or 3D points.
+ * @tparam T The type to check
  */
 template<class T>
 concept segment = requires(T segment)
@@ -140,9 +140,9 @@ concept segment = requires(T segment)
 };
 
 /*!
- * \concept segment_range
- * \brief Checks whether T is a range of valid segments
- * \tparam T The type to check
+ * @concept segment_range
+ * @brief Checks whether T is a range of valid segments
+ * @tparam T The type to check
  */
 template<class T>
 concept segment_range = ranges::range<T> && requires(T segment_range)
