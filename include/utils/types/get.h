db25559363747b4ac40582ba07ca65e962146807
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-07-01 09:53:31 +0200

    Add functionality to access 2D and 3D point coordinates by index or character
    This commit addresses the need of accessing point coordinates more
    flexibly. It introduces functions to access the coordinates of 2D and 3D
    points either by coordinate index (e.g. 0 for X, 1 for Y in a 2D point)
    or by character (e.g. 'x' or 'X' for X-coordinate). It makes handling
    points more intuitive and comes in handy when the specific type of point
    (2D or 3D) is not of interest. Additionally, accessing coordinates of a
    junction by index or character is also implemented, further enhancing
    the ease of use.
    
    Contributes to CURA-10724

--- include/utils/types/get.h
+++ include/utils/types/get.h
@@ -10,6 +10,14 @@
 
 namespace std
 {
+
+/**
+ * @brief Get a coordinate of a 2d point by index.
+ *
+ * @tparam N The index of the coordinate to get (0 for X, 1 for Y).
+ * @param point The point to get the coordinate from.
+ * @return auto& A reference to the requested coordinate.
+ */
 template<size_t N>
 constexpr auto& get(cura::utils::point2d_named auto& point) noexcept
 {
@@ -21,6 +29,13 @@ constexpr auto& get(cura::utils::point2d_named auto& point) noexcept
     return point.Y;
 }
 
+/**
+ * @brief Get a coordinate of a 2d point by character.
+ *
+ * @tparam C The character of the coordinate to get ('x' or 'X' for X-coordinate, 'y' or 'Y' for Y-coordinate).
+ * @param point The point to get the coordinate from.
+ * @return auto& A reference to the requested coordinate.
+ */
 template<cura::utils::CharRangeLiteral C>
 constexpr auto& get(cura::utils::point2d auto& point) noexcept
 {
@@ -33,6 +48,32 @@ constexpr auto& get(cura::utils::point2d auto& point) noexcept
     return std::get<1>(point);
 }
 
+/**
+ * @brief Get a coordinate of a 2d point by character.
+ *
+ * @tparam C The character of the coordinate to get ('x' or 'X' for X-coordinate, 'y' or 'Y' for Y-coordinate).
+ * @param point The point to get the coordinate from.
+ * @return auto& A reference to the requested coordinate.
+ */
+template<cura::utils::CharRangeLiteral C>
+constexpr const auto& get(const cura::utils::point2d auto& point) noexcept
+{
+    constexpr std::string_view idx = C.value;
+    static_assert(idx.starts_with("X") || idx.starts_with("x") || idx.starts_with("Y") || idx.starts_with("y"), "Index out of bounds");
+    if constexpr (idx.starts_with("X") || idx.starts_with("x"))
+    {
+        return std::get<0>(point);
+    }
+    return std::get<1>(point);
+}
+
+/**
+ * @brief Get a coordinate of a 3d point by index.
+ *
+ * @tparam N The index of the coordinate to get (0 for X, 1 for Y, 2 for Z).
+ * @param point The point to get the coordinate from.
+ * @return auto& A reference to the requested coordinate.
+ */
 template<size_t N>
 constexpr auto& get(cura::utils::point3d_named auto& point) noexcept
 {
@@ -48,6 +89,13 @@ constexpr auto& get(cura::utils::point3d_named auto& point) noexcept
     return point.z;
 }
 
+/**
+ * @brief Get a coordinate of a 3d point by character.
+ *
+ * @tparam C The character of the coordinate to get ('x' or 'X' for X-coordinate, 'y' or 'Y' for Y-coordinate, 'z' or 'Z' for Z-coordinate).
+ * @param point The point to get the coordinate from.
+ * @return auto& A reference to the requested coordinate.
+ */
 template<cura::utils::CharRangeLiteral C>
 constexpr auto& get(cura::utils::point3d auto& point) noexcept
 {
@@ -64,12 +112,26 @@ constexpr auto& get(cura::utils::point3d auto& point) noexcept
     return std::get<2>(point);
 }
 
+/**
+ * @brief Get a coordinate of a junction by index.
+ *
+ * @tparam N The index of the coordinate to get.
+ * @param junction The junction to get the coordinate from.
+ * @return auto& A reference to the requested coordinate.
+ */
 template<size_t N>
 constexpr auto& get(cura::utils::junction auto& junction) noexcept
 {
     return get<N>(junction.p);
 }
 
+/**
+ * @brief Get a coordinate of a junction by character.
+ *
+ * @tparam C The character of the coordinate to get.
+ * @param junction The junction to get the coordinate from.
+ * @return auto& A reference to the requested coordinate.
+ */
 template<cura::utils::CharRangeLiteral C>
 constexpr auto& get(cura::utils::junction auto& junction) noexcept
 {
