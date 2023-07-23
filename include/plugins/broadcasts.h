e058c19bb031bf6ab85bd3467790f2c6d14a601d
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-07-23 11:13:10 +0200

    Use SlotID to determine broadcast channel
    Move type_trait is_broadcast_channel out of
    generic types to broadcast.h, since this
    is very plugin specific and otherwise
    the slot_id.pb.h will gets pulled in in a
    lot of irrelevant locations.
    
    CURA-10805

--- include/plugins/broadcasts.h
+++ include/plugins/broadcasts.h
@@ -4,25 +4,35 @@
 #ifndef PLUGINS_BROADCAST_H
 #define PLUGINS_BROADCAST_H
 
+#include "cura/plugins/v0/slot_id.pb.h"
 #include "plugins/converters.h"
-#include "utils/types/char_range_literal.h"
-#include "utils/types/generic.h"
 
 #include <agrpc/asio_grpc.hpp>
+
+#include <type_traits>
 
 namespace cura::plugins::details
 {
 
-template<utils::CharRangeLiteral BroadcastChannel>
-requires utils::is_broadcast_channel_v<BroadcastChannel, "BroadcastSettings">
+template<plugins::v0::SlotID T1, plugins::v0::SlotID T2>
+struct is_broadcast_channel
+{
+    inline static constexpr bool value{ T1 == T2 };
+};
+
+template<plugins::v0::SlotID T1, plugins::v0::SlotID T2>
+inline constexpr bool is_broadcast_channel_v = is_broadcast_channel<T1, T2>::value;
+
+template<v0::SlotID S>
+requires is_broadcast_channel_v<S, v0::SlotID::SETTINGS_BROADCAST>
 constexpr auto broadcast_message_factory(auto&&... args)
 {
     return broadcast_settings_request{}(std::forward<decltype(args)>(args)...);
 };
 
 
-template<class Stub, utils::CharRangeLiteral BroadcastChannel>
-requires utils::is_broadcast_channel_v<BroadcastChannel, "BroadcastSettings">
+template<class Stub, v0::SlotID S>
+requires is_broadcast_channel_v<S, v0::SlotID::SETTINGS_BROADCAST>
 constexpr auto broadcast_factory()
 {
     return agrpc::RPC<&Stub::PrepareAsyncBroadcastSettings>{};
