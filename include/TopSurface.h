6c375d0ab3dd8e19f9fea2608d06e6b7d468b3d2
rburema <rburema@users.noreply.github.com>
2023-08-08 17:49:39 +0000

    Applied clang-format.

--- include/TopSurface.h
+++ include/TopSurface.h
@@ -1,5 +1,5 @@
-//Copyright (c) 2018 Ultimaker B.V.
-//CuraEngine is released under the terms of the AGPLv3 or higher.
+// Copyright (c) 2018 Ultimaker B.V.
+// CuraEngine is released under the terms of the AGPLv3 or higher.
 
 #ifndef TOPSURFACE_H
 #define TOPSURFACE_H
@@ -60,6 +60,6 @@ public:
     Polygons areas;
 };
 
-}
+} // namespace cura
 
 #endif /* TOPSURFACE_H */
