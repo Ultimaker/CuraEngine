f23c3d2e99fd9d75b03fa64dce8415ff6310a677
Remco Burema <r.burema@ultimaker.com>
2023-08-11 15:55:24 +0200

    .

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -1,5 +1,5 @@
 
-// Copyright (c) 2022 Ultimaker B.V.
+// Copyright (c) 2023 UltiMaker
 // CuraEngine is released under the terms of the AGPLv3 or higher
 #include "infill_benchmark.h"
 #include "wall_benchmark.h"
