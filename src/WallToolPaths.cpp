ae003e7a45c02ee7bc21eebb1c3ed8de125b2032
Casper Lamboo <c.lamboo@ultimaker.com>
2023-08-11 13:22:36 +0200

    Temporarily disable smooth function

--- src/WallToolPaths.cpp
+++ src/WallToolPaths.cpp
@@ -91,7 +91,7 @@ const std::vector<VariableWidthLines>& WallToolPaths::generate()
     scripta::log("prepared_outline_0", prepared_outline, section_type, layer_idx);
     prepared_outline.removeSmallAreas(small_area_length * small_area_length, false);
     prepared_outline = Simplify(settings).polygon(prepared_outline);
-    if (settings.get<bool>("meshfix_fluid_motion_enabled") && section_type != SectionType::SUPPORT)
+    if (false && settings.get<bool>("meshfix_fluid_motion_enabled") && section_type != SectionType::SUPPORT)
     {
         // No need to smooth support walls
         auto smoother = actions::smooth(settings);
