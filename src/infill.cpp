a77182eca1aa6c0d727aeb43d6bf4a758c2b8c10
Remco Burema <r.burema@ultimaker.com>
2023-09-01 16:12:31 +0200

    Large wasn't large enough.
    The smaller value caused rounding errors still. Since coord_t should always be 64-bit, even if people compile it for 32-bit systems (which we don't officially support, but still...), it's safe to go to the 32-bit max. This will solve most or all of the remaining issues with the infill-line-crossing-resolve. (It's a bit more important than the previous commit in that regard.)
    
    part of CURA-10410 second edition

--- src/infill.cpp
+++ src/infill.cpp
@@ -800,7 +800,7 @@ void Infill::resolveIntersection(const coord_t at_distance, const Point& interse
     bend_b.emplace(0, 0);
 
     // Find a bisector of the intersection; specifically, the one that crosses the connection & offset it by 1/2 distance to each side.
-    constexpr auto large_enough_vec_len = 0xFFFF;
+    constexpr auto large_enough_vec_len = 0xFFFFFFFF;
     const auto bisect = LinearAlg2D::getBisectorVector(intersect, connect_start, connect_end, large_enough_vec_len);
     const auto offset = ((at_distance / 2) * Point(-bisect.Y, bisect.X)) / large_enough_vec_len;
     const auto q = intersect + offset;
