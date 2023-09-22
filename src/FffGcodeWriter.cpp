22824f9d44fdea70471b0f64c4a3b95dd06b5d83
Erwan MATHIEU <erwan.mathieu@ultimaker.com>
2023-09-22 09:23:23 +0200

    Fixed missing prime tower hull on some layers
    CURA-10993

--- src/FffGcodeWriter.cpp
+++ src/FffGcodeWriter.cpp
@@ -1346,7 +1346,7 @@ std::vector<size_t> FffGcodeWriter::getUsedExtrudersOnLayerExcludingStartingExtr
     std::vector<bool> extruder_is_used_on_this_layer = storage.getExtrudersUsed(layer_nr);
 
     // The outermost prime tower extruder is always used if there is a prime tower, apart on layers with negative index (e.g. for the raft)
-    if (mesh_group_settings.get<bool>("prime_tower_enable") && layer_nr >= 0 && layer_nr <= storage.max_print_height_second_to_last_extruder)
+    if (mesh_group_settings.get<bool>("prime_tower_enable") && /*layer_nr >= 0 &&*/ layer_nr <= storage.max_print_height_second_to_last_extruder)
     {
         extruder_is_used_on_this_layer[storage.primeTower.extruder_order[0]] = true;
     }
