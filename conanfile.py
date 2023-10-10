6c20ccd6a9538411fcb220c4813ab8ade3be5207
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-10-10 10:40:52 +0200

    set version to 5.5.0-beta.2

--- conanfile.py
+++ conanfile.py
@@ -40,7 +40,7 @@ class CuraEngineConan(ConanFile):
 
     def set_version(self):
         if not self.version:
-            self.version = "5.5.0-beta.1"
+            self.version = "5.5.0-beta.2"
 
     def export_sources(self):
         copy(self, "CMakeLists.txt", self.recipe_folder, self.export_sources_folder)
