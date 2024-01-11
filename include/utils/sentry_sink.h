556cf235a6c7431ff2874b2915c6c63ff2e81bd7
Jelle Spijker <jelle.spijker@ultimaker.com>
2024-01-11 17:39:43 +0100

    Remove timestamp
    Corrupted the data
    
    Contribute to CURA-11482

--- include/utils/sentry_sink.h
+++ include/utils/sentry_sink.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 UltiMaker
+// Copyright (c) 2024 UltiMaker
 // CuraEngine is released under the terms of the AGPLv3 or higher
 
 #ifndef CURAENGINE_INCLUDE_UTILS_SENTRY_SINK_H
@@ -18,11 +18,6 @@ protected:
         sentry_value_t crumb = sentry_value_new_breadcrumb("debug", msg.payload.data());
         sentry_value_set_by_key(crumb, "category", sentry_value_new_string(msg.logger_name.data()));
         sentry_value_set_by_key(crumb, "level", sentry_value_new_string(spdlog::level::to_string_view(msg.level).data()));
-
-        auto duration = msg.time.time_since_epoch();
-        auto timestamp = std::chrono::duration_cast<std::chrono::seconds>(duration).count();
-        sentry_value_set_by_key(crumb, "timestamp", sentry_value_new_int32(timestamp));
-
         sentry_add_breadcrumb(crumb);
     }
     void flush_() override
