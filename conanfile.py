75885abb542479017276bd9668a587aac6537c35
Jelle Spijker <jelle.spijker@ultimaker.com>
2024-02-20 08:46:41 +0100

    Create `d.ts` file for CuraEngine
    Contributes to NP5 and NP-43

--- conanfile.py
+++ conanfile.py
@@ -219,6 +219,7 @@ class CuraEngineConan(ConanFile):
         else:
             ext = ""
         copy(self, f"CuraEngine{ext}", src=self.build_folder, dst=path.join(self.package_folder, "bin"))
+        copy(self, f"*.d.ts", src=self.build_folder, dst=path.join(self.package_folder, "bin"))
         copy(self, f"_CuraEngine.*", src=self.build_folder, dst=path.join(self.package_folder, "lib"))
         copy(self, "LICENSE*", src=self.source_folder, dst=path.join(self.package_folder, "license"))
 
