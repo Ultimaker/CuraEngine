a3f5fbf5322aba4bee7401a5252d9ae465b16306
Saumya Jain <saumya.jain@ultimaker.com>
2024-02-22 16:48:03 +0100

    adding an extra travel move at the beginning to machine_start_pos
    CURA-11444

--- src/FffGcodeWriter.cpp
+++ src/FffGcodeWriter.cpp
@@ -140,6 +140,12 @@ void FffGcodeWriter::writeGCode(SliceDataStorage& storage, TimeKeeper& time_keep
             mesh_order_per_extruder.push_back(calculateMeshOrder(storage, extruder_nr));
         }
     }
+
+    // Setting first travel move of the first extruder to the machine start position
+    const auto extruder_settings = Application::getInstance().current_slice_->scene.extruders[start_extruder_nr].settings_;
+    Point3LL p(extruder_settings.get<coord_t>("machine_extruder_start_pos_x"), extruder_settings.get<coord_t>("machine_extruder_start_pos_y"), gcode.getPositionZ());
+    gcode.writeTravel(p, Application::getInstance().current_slice_->scene.extruders[gcode.getExtruderNr()].settings_.get<Velocity>("speed_travel"));;
+
     calculateExtruderOrderPerLayer(storage);
     calculatePrimeLayerPerExtruder(storage);
 
