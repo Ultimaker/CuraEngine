6b5975031b9f583e4b27134ceaa57f01401446e1
c.lamboo <casperlamboo@gmail.com>
2024-02-22 22:40:56 +0100

    skip first

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 
-for file in `ls ./NightlyTestModels/*.stl -r`;
+for file in `ls ./NightlyTestModels/*.stl | tail -n 3`;
     echo $file
 #          do
 #              ( time ./build/Release/CuraEngine slice --force-read-parent --force-read-nondefault -v -p -j ../ultimaker/Cura/resources/definitions/ultimaker_s3.def.json -l $file -o ../`basename $file .stl`.gcode ) 2> ../`basename $file .stl`.time
