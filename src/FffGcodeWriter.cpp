f5ac2b902d3e7395505eb8be0284ee029d23f0a0
casperlamboo <casperlamboo@users.noreply.github.com>
2024-02-23 05:43:44 +0000

    Applied clang-format.

--- src/FffGcodeWriter.cpp
+++ src/FffGcodeWriter.cpp
@@ -1602,7 +1602,6 @@ std::vector<ExtruderUse>
 
         switch (method)
         {
-
         case PrimeTowerMethod::NORMAL:
             if (extruder_is_used_on_this_layer[extruder_nr] && extruder_nr != last_extruder)
             {
