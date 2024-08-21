ee99fda4ce0a72753e906ae99fd7a3125be0d973
saumyaj3 <saumyaj3@users.noreply.github.com>
2024-08-21 12:30:44 +0000

    Applied clang-format.

--- src/communication/EmscriptenCommunication.cpp
+++ src/communication/EmscriptenCommunication.cpp
@@ -39,8 +39,9 @@ EmscriptenCommunication::EmscriptenCommunication(const std::vector<std::string>&
     }
 }
 
-void EmscriptenCommunication::sendGCodePrefix(const std::string& prefix) const {
-    emscripten_run_script(fmt::format("globalThis[\"{}\"]({})", gcode_prefix_handler_ , prefix).c_str());
+void EmscriptenCommunication::sendGCodePrefix(const std::string& prefix) const
+{
+    emscripten_run_script(fmt::format("globalThis[\"{}\"]({})", gcode_prefix_handler_, prefix).c_str());
 }
 
 void EmscriptenCommunication::sendProgress(double progress) const
