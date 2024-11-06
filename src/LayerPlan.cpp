1456b393affe18aa6978b9d5ac406fa9a4cfcb57
Erwan MATHIEU <erwan.mathieu@ultimaker.com>
2024-11-06 19:20:22 +0100

    Fix combing sometimes being processed over outer surfaces
    CURA-12234
    Instead of computing the combing surfaces based on outer surfaces infill, use the full outer surfaces so that the outer walls are not considered for combing.

--- src/LayerPlan.cpp
+++ src/LayerPlan.cpp
@@ -210,37 +210,33 @@ Shape LayerPlan::computeCombBoundary(const CombBoundary boundary_type)
                 const CombingMode combing_mode = mesh.settings.get<CombingMode>("retraction_combing");
                 for (const SliceLayerPart& part : layer.parts)
                 {
-                    if (combing_mode == CombingMode::ALL) // Add the increased outline offset (skin, infill and part of the inner walls)
+                    if (combing_mode == CombingMode::INFILL)
                     {
-                        comb_boundary.push_back(part.outline.offset(offset));
+                        comb_boundary = part.infill_area;
                     }
-                    else if (combing_mode == CombingMode::NO_SKIN) // Add the increased outline offset, subtract skin (infill and part of the inner walls)
+                    else
                     {
-                        comb_boundary.push_back(part.outline.offset(offset).difference(part.inner_area.difference(part.infill_area)));
-                    }
-                    else if (combing_mode == CombingMode::NO_OUTER_SURFACES)
-                    {
-                        Shape top_and_bottom_most_fill;
-                        for (const SliceLayerPart& outer_surface_part : layer.parts)
+                        comb_boundary = part.outline.offset(offset);
+
+                        if (combing_mode == CombingMode::NO_SKIN) // Add the increased outline offset, subtract skin (infill and part of the inner walls)
                         {
-                            for (const SkinPart& skin_part : outer_surface_part.skin_parts)
+                            comb_boundary = comb_boundary.difference(part.inner_area.difference(part.infill_area));
+                        }
+                        else if (combing_mode == CombingMode::NO_OUTER_SURFACES)
+                        {
+                            for (const SliceLayerPart& outer_surface_part : layer.parts)
                             {
-                                top_and_bottom_most_fill.push_back(skin_part.top_most_surface_fill);
-                                top_and_bottom_most_fill.push_back(skin_part.bottom_most_surface_fill);
+                                comb_boundary = comb_boundary.difference(outer_surface_part.top_most_surface);
+                                comb_boundary = comb_boundary.difference(outer_surface_part.bottom_most_surface);
                             }
                         }
-
-                        comb_boundary.push_back(part.outline.offset(offset).difference(top_and_bottom_most_fill));
-                    }
-                    else if (combing_mode == CombingMode::INFILL) // Add the infill (infill only)
-                    {
-                        comb_boundary.push_back(part.infill_area);
                     }
                 }
             }
             break;
         }
     }
+
     return comb_boundary;
 }
 
@@ -1295,7 +1291,7 @@ std::vector<LayerPlan::PathCoasting>
 
         for (const auto& reversed_chunk : paths | ranges::views::enumerate | ranges::views::reverse
                                               | ranges::views::chunk_by(
-                                                  [](const auto&path_a, const auto&path_b)
+                                                  [](const auto& path_a, const auto& path_b)
                                                   {
                                                       return (! std::get<1>(path_a).isTravelPath()) || std::get<1>(path_b).isTravelPath();
                                                   }))
