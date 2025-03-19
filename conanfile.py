7ce90b2b2fb48f5862951652adc34c055f3311a9
Remco Burema <r.burema@ultimaker.com>
2025-03-19 10:20:17 +0100

    Version (fmt) supposedly known to work with current spdlog on emscripten/clang.
    CURA-12470

--- conanfile.py
+++ conanfile.py
@@ -134,7 +134,7 @@ class CuraEngineConan(ConanFile):
         self.requires("rapidjson/cci.20230929")
         self.requires("stb/cci.20230920")
         self.requires("spdlog/1.14.1")
-        self.requires("fmt/10.2.1")
+        self.requires("fmt/11.0.2")
         self.requires("range-v3/0.12.0")
         self.requires("zlib/1.3.1")
         self.requires("mapbox-wagyu/0.5.0@ultimaker/stable")
