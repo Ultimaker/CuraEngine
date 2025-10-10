9bb4078e6cdcfd7746728799dddf8d0bcf834627
Erwan MATHIEU <erwan.mathieu@ultimaker.com>
2025-10-10 10:52:41 +0200

    Apply roofing extension differently
    CURA-12446

--- src/skin.cpp
+++ src/skin.cpp
@@ -333,6 +333,7 @@ void SkinInfillAreaComputation::generateSkinRoofingFlooringFill(SliceLayerPart& 
         const size_t roofing_layer_count = std::min(mesh_.settings.get<size_t>("roofing_layer_count"), mesh_.settings.get<size_t>("top_layers"));
         const size_t flooring_layer_count = std::min(mesh_.settings.get<size_t>("flooring_layer_count"), mesh_.settings.get<size_t>("bottom_layers"));
         const coord_t skin_overlap = mesh_.settings.get<coord_t>("skin_overlap_mm");
+        const coord_t roofing_extension = mesh_.settings.get<coord_t>("roofing_extension");
 
         const Shape filled_area_above = generateFilledAreaAbove(part, roofing_layer_count);
         const std::optional<Shape> filled_area_below = generateFilledAreaBelow(part, flooring_layer_count);
@@ -350,6 +351,12 @@ void SkinInfillAreaComputation::generateSkinRoofingFlooringFill(SliceLayerPart& 
             // skin_part.flooring_fill = Shape();
             skin_part.skin_fill = skin_part.outline.intersection(filled_area_above);
         }
+
+        // Allow roof areas to grow inside the skin by roofing_extension amount
+        constexpr coord_t epsilon = 5; // Roofing and skin edges overlap, so make sure they properly merge
+        Shape roof_and_skin = skin_part.roofing_fill.offset(epsilon).unionPolygons(skin_part.skin_fill);
+        Shape extended_roof = skin_part.roofing_fill.offset(roofing_extension);
+        skin_part.roofing_fill = roof_and_skin.intersection(extended_roof);
 
         // We remove offsets areas from roofing and flooring anywhere they overlap with skin_fill.
         // Otherwise, adjacent skin_fill and roofing/flooring would have doubled offset areas. Since they both offset into each other.
