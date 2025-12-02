c6dbe16ec33685103b6b74b47e6a69c4586543ce
HellAholic <alireza.doustdar@gmail.com>
2025-12-02 11:27:33 +0100

    Remove unused _min_cppstd property and update options
    Deleted the debug property from CuraEngineConan. Added logic to disable tbbproxy for ARM64 Windows platforms in the requirements configuration.

--- conanfile.py
+++ conanfile.py
@@ -43,9 +43,6 @@ class CuraEngineConan(ConanFile):
         "with_cura_resources": False,
     }
 
-    @property
-    def _min_cppstd(self):
-        return 20
 
     @property
     def _compilers_minimum_version(self):
@@ -97,6 +94,9 @@ class CuraEngineConan(ConanFile):
             self.options["protobuf"].shared = False
         if self.options.enable_arcus:
             self.options["arcus"].shared = True
+        # ARM64 Windows: Disable tbbproxy (not available on ARM64 platforms)
+        if self.settings.os == "Windows" and self.settings.arch == "armv8":
+            self.options["onetbb"].tbbproxy = False
         # Force all libraries to be static for Emscripten builds
         if self.settings.os == "Emscripten":
             self.options["*"].shared = False
