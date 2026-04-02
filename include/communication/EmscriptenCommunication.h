7a401a9dd66988d5065e87da7f3a09962287d44b
Erwan MATHIEU <wawanbreton@gmail.com>
2026-04-02 19:27:31 +0200

    Fix EmscriptenCommunication
    CURA-13049

--- include/communication/EmscriptenCommunication.h
+++ include/communication/EmscriptenCommunication.h
@@ -28,7 +28,7 @@ private:
      * \brief Creates a message containing slice information.
      * \return A string containing the slice information message.
      */
-    [[nodiscard]] static std::string createSliceInfoMessage();
+    [[nodiscard]] static std::string createSliceInfoMessage(const std::vector<cura::Duration>& time_estimates, const PrintInformation& print_information);
     [[nodiscard]] static std::string createEngineInfoMessage();
 
 
@@ -50,15 +50,11 @@ public:
      */
     void sendGCodePrefix(const std::string& prefix) const override;
 
-    /**
-     * \brief Indicate that we're beginning to send g-code.
+    /*
+     * \brief Send an estimate of how long the print would take and how much
+     * material it would use.
      */
-    void beginGCode() override;
-
-    /**
-     * \brief Initiates the slicing of the next item.
-     */
-    void sliceNext() override;
+    void sendPrintInformation(const std::vector<cura::Duration>& time_estimates, const PrintInformation& print_information, const size_t initial_extruder_nr) const override;
 
     bool isSequential() const override
     {
