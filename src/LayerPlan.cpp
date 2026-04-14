b5cdc0a0ccfa0330f0907d60dac9fcad561b02a4
wawanbreton <601114+wawanbreton@users.noreply.github.com>
2026-04-14 06:30:21 +0000

    Apply clang-format

--- src/LayerPlan.cpp
+++ src/LayerPlan.cpp
@@ -66,18 +66,17 @@ GCodePath* LayerPlan::getLatestPathWithConfig(
     {
         return &paths.back();
     }
-    paths.emplace_back(
-        GCodePath{
-            .z_offset = z_offset,
-            .config = config,
-            .mesh = current_mesh_,
-            .space_fill_type = space_fill_type,
-            .flow = flow,
-            .width_factor = width_factor,
-            .spiralize = spiralize,
-            .speed_factor = speed_factor,
-            .travel_to_z = travel_to_z,
-        });
+    paths.emplace_back(GCodePath{
+        .z_offset = z_offset,
+        .config = config,
+        .mesh = current_mesh_,
+        .space_fill_type = space_fill_type,
+        .flow = flow,
+        .width_factor = width_factor,
+        .spiralize = spiralize,
+        .speed_factor = speed_factor,
+        .travel_to_z = travel_to_z,
+    });
 
     GCodePath* ret = &paths.back();
     return ret;
