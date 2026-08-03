d420aecc4cea202e6e578c9508dee084f409e0b8
Casper Lamboo <c.lamboo@ultimaker.com>
2026-08-03 10:04:07 +0200

    Use updated copyright notice

--- include/BeadingStrategy/InnerWallInsetBeadingStrategy.h
+++ include/BeadingStrategy/InnerWallInsetBeadingStrategy.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Ultimaker B.V.
+// Copyright (c) 2026 UltiMaker
 // CuraEngine is released under the terms of the AGPLv3 or higher.
 
 #ifndef INNER_WALL_INSET_BEADING_STRATEGY_H
