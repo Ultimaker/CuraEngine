7b4155441de4fa97af54b2861752b491aa695c9c
Remco Burema <r.burema@ultimaker.com>
2026-09-16 10:39:29 +0200

    Give a bonus whenever skin-bridge is aligned with wall-bridge when finding the angle.
    The score calculation otherwise can get quite confused, and the 'best angle' can be one degree off alignment with the walls. This does a sort of 'soft snap' by giving a bonus if the skin bridging lines are aligned with the outer walls. This only makes sense if there even is some sort of 'dominant wall alignment', so first check if there's at least two walls in opposite directions long enough.
    
    CURA-13334

--- src/bridge/bridge.cpp
+++ src/bridge/bridge.cpp
@@ -53,6 +53,44 @@ std::vector<coord_t> shapeLineIntersections(const coord_t line_y, const Transfor
     return intersections;
 }
 
+double getWallAlignmentBonus(const coord_t line_y, const TransformedShape& transformed_skin_area, const coord_t bridge_len)
+{
+    // Find the 'longest' segments (in forwards and backwards directions) w.r.t. the current direction.
+    const TransformedSegment* skin_forward_longest{ nullptr };
+    const TransformedSegment* skin_backward_longest{ nullptr };
+    for (const auto& segment : transformed_skin_area.getSegments())
+    {
+        const auto vec_x = segment.getEnd().X - segment.getStart().X;
+        const TransformedSegment*& skin_longest = vec_x > 0 ? skin_forward_longest : skin_backward_longest;
+        if (skin_longest == nullptr || std::abs(vec_x) > std::abs(skin_longest->getEnd().X - skin_longest->getStart().X))
+        {
+            skin_longest = &segment;
+        }
+    }
+
+    // Encourage the lines to be aligned with the longest walls.
+    const auto half_bridge_len2 = (bridge_len / 2) << 1;
+    const AngleRadians small_angle{ AngleDegrees{ 0.5 } };
+    int wall_alignment_count = 0;
+    for (const TransformedSegment* skin_longest : { skin_forward_longest, skin_backward_longest })
+    {
+        const auto vec = skin_longest->getEnd() - skin_longest->getStart();
+        if (vSize2(vec) < half_bridge_len2)
+        {
+            // Don't align with smaller walls, or walls that are too far away.
+            continue;
+        }
+        AngleRadians angle = std::abs(std::atan2(vec.Y, vec.X));
+        angle = std::min(angle, AngleRadians{ TAU / 2 } - angle);
+        if (angle <= small_angle)
+        {
+            ++wall_alignment_count;
+        }
+    }
+
+    return wall_alignment_count >= 2 ? 0.5 : 0.0;
+}
+
 /*!
  * Evaluates a potential bridging line to see if it can actually bridge between two supported regions
  * @param line_y The Y coordinate of the horizontal line
@@ -74,6 +112,9 @@ coord_t evaluateBridgeLine(const coord_t line_y, const TransformedShape& transfo
         return 0;
     }
     ranges::stable_sort(skin_outline_intersections);
+
+    const coord_t bridge_len = std::abs(skin_outline_intersections.back() - skin_outline_intersections.front());
+    const double wall_alignment_bonus = getWallAlignmentBonus(line_y, transformed_skin_area, bridge_len);
 
     // Calculate intersections with supported regions to see which segments are anchored
     std::vector<coord_t> supported_regions_intersections = shapeLineIntersections(line_y, transformed_supported_area);
@@ -155,7 +196,7 @@ coord_t evaluateBridgeLine(const coord_t line_y, const TransformedShape& transfo
 
         const bool leaving_skin = next_intersection_is_skin_area && ! next_inside_skin_area;
         const bool reaching_supported = next_intersection_is_supported_area && next_inside_supported_area;
-        double add_segment_score_weight = 0.0;
+        double add_segment_score_weight = wall_alignment_bonus;
 
         switch (bridge_status)
         {
@@ -208,7 +249,8 @@ coord_t evaluateBridgeLine(const coord_t line_y, const TransformedShape& transfo
  * @param supported_regions The supported regions areas
  * @param line_width The bridging line width
  * @param angle The current angle to be tested
- * @return The global bridging score for this angle */
+ * @return The global bridging score for this angle
+ */
 coord_t evaluateBridgeLines(const Shape& skin_outline, const Shape& supported_regions, const coord_t line_width, const AngleDegrees& angle)
 {
     // Transform the skin outline and supported regions according to the angle to speedup intersections calculations
